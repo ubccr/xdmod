02673754c4fd45cf7f176eac76e0631082c816fe
Ryan Rathsam <ryanrath@buffalo.edu>
2016-11-03 11:56:34 -0400

    - Adding timestamp values for module_versions
    - Added because the ETL ManageTables action doesn't support defaults
      that are functions.

--- configuration/etl/etl_sql.d/acls/module_versions.sql
+++ configuration/etl/etl_sql.d/acls/module_versions.sql
@@ -1,9 +1,11 @@
-INSERT INTO ${DESTINATION_SCHEMA}.module_versions(module_id, version_major, version_minor, version_micro, version_patch)
+INSERT INTO ${DESTINATION_SCHEMA}.module_versions(module_id, version_major, version_minor, version_micro, version_patch, created_on, last_modified_on)
 SELECT
 m.module_id,
 6,
 5,
 0,
-''
+'',
+now(),
+now()
 FROM modules m
 WHERE m.name = 'xdmod';
