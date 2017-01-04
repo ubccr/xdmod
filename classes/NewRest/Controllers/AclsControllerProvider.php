978a5302381f9b5856feb1fae9fbdf1bd83384cf
Ryan Rathsam <ryanrath@buffalo.edu>
2017-01-04 09:06:21 -0500

    Populated Remaining Acl Routes
    - Filled out the rest of the Acl routes with basic implementations.
      Note: authorization still needs to be handled but will be added in a
      later commit as not all of the data is in place and adding it now
      would hamper testing.

--- classes/NewRest/Controllers/AclsControllerProvider.php
+++ classes/NewRest/Controllers/AclsControllerProvider.php
@@ -4,6 +4,7 @@ use Silex\Application;
 use Silex\ControllerCollection;
 
 use Symfony\Component\HttpFoundation\Request;
+use User\Acl;
 use User\Acls;
 
 class AclsControllerProvider extends BaseControllerProvider
@@ -51,23 +52,86 @@ class AclsControllerProvider extends BaseControllerProvider
 
     public function createAcl(Request $request, Application $app)
     {
-        return $app->json(array('success' => false));
+        $moduleId = self::getIntParam($request, Acl::MODULE_ID, true);
+        $aclTypeId = self::getIntParam($request, Acl::ACL_TYPE_ID, true);
+        $name = self::getStringParam($request, Acl::NAME, true);
+        $display = self::getStringParam($request, Acl::DISPLAY, false, $name);
+        $enabled = self::getBooleanParam($request, Acl::ENABLED, false);
+
+        $acl = Acls::createAcl(
+            new Acl(
+                array(
+                    Acl::MODULE_ID => $moduleId,
+                    Acl::ACL_TYPE_ID => $aclTypeId,
+                    Acl::NAME => $name,
+                    Acl::DISPLAY => $display,
+                    Acl::ENABLED => $enabled
+                )
+            )
+        );
+
+        $success = isset($acl);
+        $status = true == $success ? 200 : 500;
+        return $app->json(array(
+            'success' => $success,
+            'results' => $acl
+        ), $status);
     }
 
     public function getAcl(Request $request, Application $app, $id)
     {
-        return $app->json(array('success' => false));
+        $acl = Acls::getAcl($id);
+        $success = isset($acl);
+        $status = true == $success ? 200 : 404;
 
+        return $app->json(array(
+            'success' => $success,
+            'results' => $acl
+        ),$status);
     }
 
     public function updateAcl(Request $request, Application $app, $id)
     {
-        return $app->json(array('success' => false));
+        $moduleId = self::getIntParam($request, Acl::MODULE_ID, true);
+        $aclTypeId = self::getIntParam($request, Acl::ACL_TYPE_ID, true);
+        $name = self::getStringParam($request, Acl::NAME, true);
+        $display = self::getStringParam($request, Acl::DISPLAY, false, $name);
+        $enabled = self::getBooleanParam($request, Acl::ENABLED, false);
+
+        $success = Acls::updateAcl(
+            new Acl(
+                array(
+                    ACL::ACL_ID => $id,
+                    Acl::MODULE_ID => $moduleId,
+                    Acl::ACL_TYPE_ID => $aclTypeId,
+                    Acl::NAME => $name,
+                    Acl::DISPLAY => $display,
+                    Acl::ENABLED => $enabled
+                )
+            )
+        );
+        $status = true == $success ? 200 : 500;
+
+        return $app->json(array(
+            'success' => $success,
+            'result' => $success
+        ), $status);
     }
 
     public function deleteAcl(Request $request, Application $app, $id)
     {
-        return $app->json(array('success' => false));
+        $success = Acls::deleteAcl(
+            new Acl(
+                array(
+                    Acl::ACL_ID => $id
+                )
+            )
+        );
+        $status = true == $success ? 200 : 500;
+        return $app->json(array(
+            'success' => $success,
+            'results' => $success
+        ), $status);
     }
 
     public function listUserAcls(Request $request, Application $app)
