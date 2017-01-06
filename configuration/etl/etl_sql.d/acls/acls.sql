73d59ab3d518e58e9589a71b123be5bffc55fe25
Ryan Rathsam <ryanrath@buffalo.edu>
2017-01-06 11:39:27 -0500

    Added the public acl
    - Added the public acl as an actual acl as opposed to something that
      just lives in code.

--- configuration/etl/etl_sql.d/acls/acls.sql
+++ configuration/etl/etl_sql.d/acls/acls.sql
@@ -1,80 +1,108 @@
--- 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-  m.module_id,
-  (SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'flag') as acl_type_id,
-  'mgr' as name,
-  'Manager' as display,
-  true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+--
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'flag') AS acl_type_id,
+        'mgr'                    AS name,
+        'Manager'                AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-m.module_id,
-(SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'flag') as acl_type_id,
-'dev' as name,
-'Developer' as display,
-true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'flag') AS acl_type_id,
+        'dev'                    AS name,
+        'Developer'              AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-m.module_id,
-(SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'data') as acl_type_id,
-'usr' as name,
-'User' as display,
-true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'data') AS acl_type_id,
+        'usr'                    AS name,
+        'User'                   AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-m.module_id,
-(SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'data') as acl_type_id,
-'po' as name,
-'Program Officer' as display,
-true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'data') AS acl_type_id,
+        'po'                     AS name,
+        'Program Officer'        AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-m.module_id,
-(SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'data') as acl_type_id,
-'cs' as name,
-'Center Staff' as display,
-true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'data') AS acl_type_id,
+        'cs'                     AS name,
+        'Center Staff'           AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-m.module_id,
-(SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'feature') as acl_type_id,
-'cd' as name,
-'Center Director' as display,
-true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'feature') AS acl_type_id,
+        'cd'                        AS name,
+        'Center Director'           AS display,
+        TRUE                        AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-m.module_id,
-(SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'flag') as acl_type_id,
-'pi' as name,
-'Principal Investigator' as display,
-true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'flag') AS acl_type_id,
+        'pi'                     AS name,
+        'Principal Investigator' AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
 
-INSERT INTO ${DESTINATION_SCHEMA}.acls(module_id, acl_type_id, name, display, enabled)
-SELECT
-m.module_id,
-(SELECT at.acl_type_id FROM ${DESTINATION_SCHEMA}.acl_types at WHERE at.name = 'flag') as acl_type_id,
-'cc' as name,
-'Campus Champion' as display,
-true as enabled
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'flag') AS acl_type_id,
+        'cc'                     AS name,
+        'Campus Champion'        AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
+
+INSERT INTO ${DESTINATION_SCHEMA}.acls (module_id, acl_type_id, name, display, enabled)
+    SELECT
+        m.module_id,
+        (SELECT at.acl_type_id
+         FROM ${DESTINATION_SCHEMA}.acl_types at
+         WHERE at.name = 'flag') AS acl_type_id,
+        'pub'                    AS name,
+        'Public'                 AS display,
+        TRUE                     AS enabled
+    FROM ${DESTINATION_SCHEMA}.modules m
+    WHERE m.name = 'xdmod';
