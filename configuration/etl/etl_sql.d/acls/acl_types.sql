21c63d584770b8e529454dc1fb03c7df7c5970e3
Ryan Rathsam <ryanrath@buffalo.edu>
2017-01-19 11:33:15 -0500

    Updated acl_types sql and added group_bys

--- configuration/etl/etl_sql.d/acls/acl_types.sql
+++ configuration/etl/etl_sql.d/acls/acl_types.sql
@@ -1,47 +1,44 @@
-DELETE FROM ${DESTINATION_SCHEMA}.acl_types
-WHERE name = 'data' and display = 'Data' AND module_id IN (
-    SELECT
-        m.module_id
-    FROM ${DESTINATION_SCHEMA}.modules m
-    WHERE m.name = 'xdmod'
-);
+INSERT INTO ${DESTINATION_SCHEMA}.acl_types(module_id, name, display)
+SELECT inc.*
+FROM (
+     SELECT
+      m.module_id,
+      'data' as name,
+      'Data' as display
+      FROM ${DESTINATION_SCHEMA}.modules m
+      WHERE m.name = 'xdmod') inc
+LEFT JOIN ${DESTINATION_SCHEMA}.acl_types cur
+     ON  cur.module_id = inc.module_id
+     AND cur.name      = inc.name
+     AND cur.display   = inc.display
+WHERE cur.acl_type_id IS NULL;
 
 INSERT INTO ${DESTINATION_SCHEMA}.acl_types(module_id, name, display)
-SELECT
-m.module_id,
-'data' as name,
-'Data' as display
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
--- //
-DELETE FROM ${DESTINATION_SCHEMA}.acl_types
-WHERE name = 'feature' and display = 'Feature' AND module_id IN (
-    SELECT
-        m.module_id
-    FROM ${DESTINATION_SCHEMA}.modules m
-    WHERE m.name = 'xdmod'
-);
+SELECT inc.*
+FROM (
+     SELECT
+      m.module_id,
+      'feature' as name,
+      'Feature' as display
+     FROM ${DESTINATION_SCHEMA}.modules m
+     WHERE m.name = 'xdmod') inc
+LEFT JOIN ${DESTINATION_SCHEMA}.acl_types cur
+     ON  cur.module_id = inc.module_id
+     AND cur.name      = inc.name
+     AND cur.display   = inc.display
+     WHERE cur.acl_type_id IS NULL;
 
 INSERT INTO ${DESTINATION_SCHEMA}.acl_types(module_id, name, display)
-SELECT
-m.module_id,
-'feature' as name,
-'Feature' as display
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
--- //
-DELETE FROM ${DESTINATION_SCHEMA}.acl_types
-WHERE name = 'flag' and display = 'Flag' AND module_id IN (
-    SELECT
-        m.module_id
-    FROM ${DESTINATION_SCHEMA}.modules m
-    WHERE m.name = 'xdmod'
-);
-
-INSERT INTO ${DESTINATION_SCHEMA}.acl_types(module_id, name, display)
-SELECT
-m.module_id,
-'flag' as name,
-'Flag' as display
-FROM ${DESTINATION_SCHEMA}.modules m
-WHERE m.name = 'xdmod';
+SELECT inc.*
+FROM (
+     SELECT
+      m.module_id,
+      'flag' as name,
+      'Flag' as display
+     FROM ${DESTINATION_SCHEMA}.modules m
+     WHERE m.name = 'xdmod') inc
+LEFT JOIN ${DESTINATION_SCHEMA}.acl_types cur
+     ON  cur.module_id = inc.module_id
+     AND cur.name      = inc.name
+     AND cur.display   = inc.display
+     WHERE cur.acl_type_id IS NULL;
