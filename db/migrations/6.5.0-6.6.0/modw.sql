182fb9ffdbe20548aa46087cbc8ce44e9cf895c9
Steve Gallo <smgallo@buffalo.edu>
2017-02-08 14:45:40 -0500

    Add schema migration file

---
+++ db/migrations/6.5.0-6.6.0/modw.sql
@@ -0,0 +1,12 @@
+use modw;
+
+LOCK TABLES `peopleonaccount` WRITE;
+ALTER TABLE `peopleonaccount` DISABLE KEYS;
+
+ALTER TABLE `peopleonaccount` DROP COLUMN `min_activity_time`;
+ALTER TABLE `peopleonaccount` DROP COLUMN `min_activity_time_ts`;
+ALTER TABLE `peopleonaccount` DROP KEY `aggregation_index`;
+ALTER TABLE `peopleonaccount` ADD KEY `aggregation_index` (`resource_id`,`account_id`,`person_id`,`start_time_ts`,`end_time_ts`,`allocationstate_id`);
+
+ALTER TABLE `peopleonaccount` ENABLE KEYS;
+UNLOCK TABLES;
