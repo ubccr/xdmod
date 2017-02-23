75f94a1c61ed39b5078e1a5073d8299df060fa24
Ryan Rathsam <ryanrath@buffalo.edu>
2017-02-23 11:16:13 -0500

    Modified the return format of ui/tabs
    - updated the return format of ui/tabs to be in line with what get_tabs.php
      returns.

--- classes/NewRest/Controllers/UIControllerProvider.php
+++ classes/NewRest/Controllers/UIControllerProvider.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Silex\Application;
 use Silex\ControllerCollection;
 use Tabs;
+use Tab;
 
 class UIControllerProvider extends BaseControllerProvider
 {
@@ -26,6 +27,21 @@ class UIControllerProvider extends BaseControllerProvider
         $user = $request->get(BaseControllerProvider::_USER);
 
         $tabs = Tabs::getTabsForUser($user);
+
+        // Ensure that the results are returned in the format that is expected.
+        $tabs = array_reduce($tabs, function($carry, Tab $item) {
+            $carry []= array(
+                'isDefault' => $item->getIsDefault(),
+                'javascriptClass' => $item->getJavascriptClass(),
+                'javascriptReference' => $item->getJavascriptReference(),
+                'pos' => $item->getPosition(),
+                'tab' => $item->getName(),
+                'title' => $item->getDisplay(),
+                'tooltip' => $item->getTooltip(),
+                'userManualSectionName' => $item->getUserManualSectionName()
+            );
+            return $carry;
+        }, array());
 
         $success = isset($tabs);
 
