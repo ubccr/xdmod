b26b362f80ba061b0a09262c0adce8fa13424f2e
Ryan Rathsam <ryanrath@buffalo.edu>
2017-03-22 16:01:22 -0400

    Added a Statistics Controller
    - Added a couple of REST end points for statistics.

---
+++ classes/NewRest/Controllers/StatisticsControllerProvider.php
@@ -0,0 +1,56 @@
+<?php namespace NewRest\Controllers;
+
+use Silex\Application;
+use Silex\ControllerCollection;
+
+use \Statistics;
+use \Statistic;
+
+class StatisticsControllerProvider extends BaseControllerProvider
+{
+
+    /**
+     * This function is responsible for the setting up of any routes that this
+     * ControllerProvider is going to be managing. It *must* be overridden by
+     * a child class.
+     *
+     * @param Application $app
+     * @param ControllerCollection $controller
+     * @return null
+     */
+    public function setupRoutes(Application $app, ControllerCollection $controller)
+    {
+        $prefix = $this->prefix;
+        $class = get_class($this);
+
+        $controller
+            ->get("$prefix", "$class::getStatistics");
+        $controller
+            ->get("$prefix/permitted", "$class::getPermittedStatistics");
+    }
+
+    public function getStatistics(Request $request, Application $app)
+    {
+        $statistics = Statistics::listStatistics();
+        $success = isset($statistics) && count($statistics) > 0;
+
+        return $app->json(array(
+            'success' => $success,
+            'data' => $statistics
+        ));
+    }
+
+    public function getPermittedStatistics(Request $request, Application $app)
+    {
+        $user = $this->getUserFromRequest($request);
+        $realmName = $this->getStringParam($request, 'realm', true);
+        $groupByName = $this->getStringParam($request, 'group_by', true);
+
+        $statistics = Statistics::listPermittedStatistics($user, $realmName, $groupByName);
+        $success = isset($statistics) && count($statistics) > 0;
+        return $app->json(array(
+            'success' => $success,
+            'data' => $statistics
+        ));
+    }
+}
