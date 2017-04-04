e6eee4e844884a7d186f5735bf97fd65e096a11b
Ryan Rathsam <ryanrath@buffalo.edu>
2017-04-04 09:15:09 -0400

    Reformat to use spacing consistent with current guidelines
    - was previously using 3 spaces, should be using 4 now.

--- html/controllers/role_manager/downgrade_member.php
+++ html/controllers/role_manager/downgrade_member.php
@@ -1,40 +1,38 @@
 <?php
 
-   \xd_security\assertParameterSet('member_id', RESTRICTION_UID);
-   
-	// -----------------------------
-	
-	$member = XDUser::getUserByID($_POST['member_id']);
-	
-	if ($member == NULL){
-      \xd_response\presentError('user_does_not_exist');
-	}			
-	
-	// -----------------------------
+\xd_security\assertParameterSet('member_id', RESTRICTION_UID);
 
-   try {
-   
-      $active_user = \xd_security\getLoggedInUser();
-      
-      $member_organizations = $member->getOrganizationCollection();
-            
-      if (!in_array($active_user->getActiveOrganization(), $member_organizations)) {
-         \xd_response\presentError('center_mismatch_between_member_and_director');
-      }
+// -----------------------------
 
-      $active_user->getActiveRole()->downgradeStaffMember($member);
-      $returnData['success'] = true; 
-      
-   }
-   catch (SessionExpiredException $see) {
-      // TODO: Refactor generic catch block below to handle specific exceptions,
-      //       which would allow this block to be removed.
-      throw $see;
-   }
-   catch (\Exception $e){
-      \xd_response\presentError($e->getMessage());
-   }
-       
-   echo json_encode($returnData);
-   	
+$member = XDUser::getUserByID($_POST['member_id']);
+
+if ($member == NULL) {
+    \xd_response\presentError('user_does_not_exist');
+}
+
+// -----------------------------
+
+try {
+
+    $active_user = \xd_security\getLoggedInUser();
+
+    $member_organizations = $member->getOrganizationCollection();
+
+    if (!in_array($active_user->getActiveOrganization(), $member_organizations)) {
+        \xd_response\presentError('center_mismatch_between_member_and_director');
+    }
+
+    $active_user->getActiveRole()->downgradeStaffMember($member);
+    $returnData['success'] = true;
+
+} catch (SessionExpiredException $see) {
+    // TODO: Refactor generic catch block below to handle specific exceptions,
+    //       which would allow this block to be removed.
+    throw $see;
+} catch (\Exception $e) {
+    \xd_response\presentError($e->getMessage());
+}
+
+echo json_encode($returnData);
+
 ?>
