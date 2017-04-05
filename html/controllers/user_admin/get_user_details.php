a68a9787bad86e1e98aee4db4d2f6401b7ce8355
Ryan Rathsam <ryanrath@buffalo.edu>
2017-04-05 13:24:24 -0400

    No more primary roles ( and this is not being used )
    - removed the 'primary_role' information returned in get_user_details
      as it is no longer being used and we no longer have primary roles.

--- html/controllers/user_admin/get_user_details.php
+++ html/controllers/user_admin/get_user_details.php
@@ -25,7 +25,6 @@ use User\Acls;
 	$userDetails['time_updated'] = $selected_user->getUpdateTimestamp();
 	$userDetails['time_last_logged_in'] = $selected_user->getLastLoginTimestamp();
 
-	$userDetails['primary_role'] = $selected_user->getPrimaryRole()->getIdentifier();
 	$userDetails['email_address'] = $selected_user->getEmailAddress();
 
 	if ($userDetails['email_address'] == NO_EMAIL_ADDRESS_SET) {
