c1111b2b912efe18589764f10a5954cfd8df0285
Ryan Rathsam <ryanrath@buffalo.edu>
2017-04-06 10:22:57 -0400

    Reverting previous changes.

--- classes/XDUser.php
+++ classes/XDUser.php
@@ -1020,36 +1020,23 @@ SQL;
 
         foreach ($this->_roles as $role) {
             $roleId = $this->_getRoleID($role);
-            if ($roleId !== null) {
-                $this->_pdo->execute(
-                    "INSERT INTO UserRoles VALUES(:id, :roleId, '0', '0')",
-                    array('id' => $this->_id,
-                        'roleId' => $roleId)
-                );
-            }
+            $this->_pdo->execute(
+                "INSERT INTO UserRoles VALUES(:id, :roleId, '0', '0')",
+                array('id' => $this->_id,
+                    'roleId' => $roleId)
+            );
         }
-        $primaryRoleIdentifier = $this->_primary_role->getIdentifier();
-        $primaryRoleId = $this->_getRoleID($primaryRoleIdentifier);
-        if ($primaryRoleId == null) {
-            throw new Exception("Unable to find id for role $primaryRoleIdentifier");
-        }
+        $primaryRoleId = $this->_getRoleID($this->_primary_role->getIdentifier());
         $this->_pdo->execute(
             "UPDATE UserRoles SET is_primary='1' WHERE user_id=:id AND role_id=:roleId",
             array('id' => $this->_id, 'roleId' => $primaryRoleId)
         );
 
         // If the updater (e.g. Manager) has pulled out the (recently) active role for this user, reassign the active role to the primary role.
-        $active_role_id = null;
-        $activeRoleIdentifier = $this->_active_role->getIdentifier();
-        if (in_array($activeRoleIdentifier, $this->_roles)) {
-            $active_role_id = $this->_getRoleID($activeRoleIdentifier);
-            if ($active_role_id == null) {
-                throw new Exception("Unable to retrieve id for role $activeRoleIdentifier");
-            }
-        } else {
-            $active_role_id = $primaryRoleId;
-        }
 
+        $active_role_id = (in_array($this->_active_role->getIdentifier(), $this->_roles)) ?
+            $this->_getRoleID($this->_active_role->getIdentifier()) :
+            $primaryRoleId;
 
 
         $this->_pdo->execute(
