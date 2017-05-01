fb8237662d473b74869f1742059d929c5584b92b
Tom Yearke <tyearke@buffalo.edu>
2017-05-01 11:31:09 -0400

    Use Git to manage xdmod-test-artifacts

--- open_xdmod/modules/xdmod/tests/lib/ETL/Configuration/ConfigurationTest.php
+++ open_xdmod/modules/xdmod/tests/lib/ETL/Configuration/ConfigurationTest.php
@@ -13,8 +13,8 @@ use ETL\Configuration\Configuration;
 
 class ConfigurationTest extends \PHPUnit_Framework_TestCase
 {
-    const TEST_ARTIFACT_INPUT_PATH = "../../../../vendor/ubccr/xdmod-test-artifacts/xdmod/etlv2/configuration/input";
-    const TEST_ARTIFACT_OUTPUT_PATH = "../../../../vendor/ubccr/xdmod-test-artifacts/xdmod/etlv2/configuration/output";
+    const TEST_ARTIFACT_INPUT_PATH = "./artifacts/xdmod-test-artifacts/xdmod/etlv2/configuration/input";
+    const TEST_ARTIFACT_OUTPUT_PATH = "./artifacts/xdmod-test-artifacts/xdmod/etlv2/configuration/output";
 
     /**
      * Test JSON parse errors
