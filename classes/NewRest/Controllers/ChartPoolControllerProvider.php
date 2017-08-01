8a359f922a40d49e648e2f510918342db06f9b73
Ryan Rathsam <ryanrath@buffalo.edu>
2017-08-01 09:32:16 -0400

    Initial Commit
    - Added a ControllerProvider for the 'chart_pool' controller.
      - this provides routes for the operations: add_to_queue, remove_from_queue
    - Added a new rest configuration file that defines the route prefix:
      charts/pools
    - Modified the calling site in Reporting.js to utilize to the new rest end point
    
    Resolving TravisCI errors

---
+++ classes/NewRest/Controllers/ChartPoolControllerProvider.php
@@ -0,0 +1,72 @@
+<?php namespace NewRest\Controllers;
+
+use Silex\Application;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+use XDChartPool;
+
+class ChartPoolControllerProvider extends BaseControllerProvider
+{
+
+    /**
+     * This function is responsible for the setting up of any routes that this
+     * ControllerProvider is going to be managing. It *must* be overridden by
+     * a child class.
+     *
+     * @param Application $app
+     * @param ControllerCollection $controller
+     * @return null
+     */
+    public function setupRoutes(Application $app, ControllerCollection $controller)
+    {
+        $root = $this->prefix;
+        $class = get_class($this);
+        $controller->post("$root/add", "$class::addToQueue");
+        $controller->post("$root/remove", "$class::removeFromQueue");
+    }
+
+    public function addToQueue(Request $request, Application $app)
+    {
+        $result = array();
+
+        $user = $this->getUserFromRequest($request);
+
+        $chartId = $this->getStringParam($request, 'chart_id', true);
+        $chartDrillDetails = $this->getStringParam($request, 'chart_drill_details', true);
+        $chartDateDesc = $this->getStringParam($request, 'chart_date_desc', true);
+
+        $chartTitle = $this->getStringParam($request, 'chart_title', false, 'Untitled Chart');
+
+        $chart_pool = new XDChartPool($user);
+
+        $chart_pool->addChartToQueue($chartId,$chartTitle, $chartDrillDetails, $chartDateDesc);
+
+        $result['success'] = true;
+        $result['action'] = 'add';
+
+        return $app->json($result);
+    }
+
+    public function removeFromQueue(Request $request, Application $app)
+    {
+        $result = array();
+
+        $user = $this->getUserFromRequest($request);
+
+        $chartId = $this->getStringParam($request, 'chart_id', true);
+
+        $chartTitle = $this->getStringParam($request, 'chart_title', false, 'Untitled Chart');
+
+        $chartId = str_replace("title=".$chartTitle, "title=".urlencode($chartTitle), $chartId);
+
+        $chartPool = new XDChartPool($user);
+
+        $chartPool->removeChartFromQueue($chartId);
+
+        $result['success'] = true;
+        $result['action'] = 'remove';
+
+        return $app->json($result);
+    }
+}
+
