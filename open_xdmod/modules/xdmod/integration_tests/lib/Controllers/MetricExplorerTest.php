c0a5139fe7afb9618a429fe1d1f25432f5246783
Joseph White <jpwhite4@buffalo.edu>
2017-09-14 17:52:32 -0400

    Added integration test for the queries endpoint

--- open_xdmod/modules/xdmod/integration_tests/lib/Controllers/MetricExplorerTest.php
+++ open_xdmod/modules/xdmod/integration_tests/lib/Controllers/MetricExplorerTest.php
@@ -104,4 +104,93 @@ class MetricExplorerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($response[1]['content_type'], 'application/json');
         $this->assertEquals($response[1]['http_code'], 401);
     }
+
+    /**
+     * @dataProvider chartDataProvider
+     */
+    public function testChartQueryEndpoint($chartSettings)
+    {
+        $settings = array(
+            'name' => 'Test &lt; <img src="test.gif" onerror="alert()" />',
+            'ts' => microtime(true),
+            'config' => $chartSettings
+        );
+        $this->helper->authenticate('cd');
+        $response = $this->helper->post('rest/v1/metrics/explorer/queries', null, array('data' => json_encode($settings)));
+
+        $this->assertEquals($response[1]['content_type'], 'application/json');
+        $this->assertEquals($response[1]['http_code'], 200);
+
+        $querydata = $response[0];
+        $this->assertArrayHasKey('data', $querydata);
+        $this->assertArrayHasKey('recordid', $querydata['data']);
+        $this->assertArrayHasKey('name', $querydata['data']);
+        $this->assertArrayHasKey('ts', $querydata['data']);
+        $this->assertArrayHasKey('config', $querydata['data']);
+
+        $recordid = $querydata['data']['recordid'];
+
+        $allcharts = $this->helper->get('rest/v1/metrics/explorer/queries');
+        $this->assertTrue($allcharts[0]['success']);
+
+        $seenchart = false;
+        foreach($allcharts[0]['data'] as $chart)
+        {
+            if ($chart['recordid'] == $recordid) {
+                $this->assertEquals("Test &lt; &lt;img src=&quot;test.gif&quot; onerror=&quot;alert()&quot; /&gt;", $chart['name']);
+                $seenchart = true;
+            }
+        }
+        $this->assertTrue($seenchart);
+
+        $justthischart = $this->helper->get('rest/v1/metrics/explorer/queries/' . $recordid);
+        $this->assertTrue($justthischart[0]['success']);
+        $this->assertEquals("Test &lt; &lt;img src=&quot;test.gif&quot; onerror=&quot;alert()&quot; /&gt;", $justthischart[0]['data']['name']);
+
+        $cleanup = $this->helper->delete('rest/v1/metrics/explorer/queries/' . $recordid);
+        $this->assertTrue($cleanup[0]['success']);
+    }
+
+    public function chartDataProvider()
+    {
+        $emptyChart = <<< EOF
+{
+   "featured": false,
+   "trend_line": false,
+   "x_axis": {},
+   "y_axis": {},
+   "legend": {},
+   "defaultDatasetConfig": {  
+      "display_type": "column"
+   },
+   "swap_xy": false,
+   "share_y_axis": false,
+   "hide_tooltip": true,
+   "show_remainder": false,
+   "timeseries": false,
+   "title": "Test",
+   "legend_type": "bottom_center",
+   "font_size": 3,
+   "show_filters": true,
+   "show_warnings": true,
+   "data_series": {  
+      "data": [ ],
+      "total": 0
+   },
+   "aggregation_unit": "Auto",
+   "global_filters": {  
+      "data": [ ],
+      "total": 0
+   },
+   "timeframe_label": "Previous month",
+   "start_date": "2017-08-01",
+   "end_date": "2017-08-31",
+   "start": 0,
+   "limit": 10
+}
+EOF;
+        return array(
+            array($emptyChart)
+        );
+    }
 }
