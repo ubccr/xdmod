a71574dafa7cea8c04519ad67f68f7949e7abb1e
Joseph White <jpwhite4@buffalo.edu>
2017-09-21 15:55:43 -0400

    asdas

--- html/gui/js/report_builder/ChartDateEditor.js
+++ html/gui/js/report_builder/ChartDateEditor.js
@@ -244,7 +244,7 @@ XDMoD.Reporting.ChartDateEditor = Ext.extend(Ext.Window,  {
 
         }
 
-        if (chart_config.type.toLowerCase() == 'user defined') {
+        if (chart_config.type && chart_config.type.toLowerCase() === 'user defined') {
 
            rdoGrpTimeframeMode.items.get(0).setValue(true);
 
