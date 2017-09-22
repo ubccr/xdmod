f5c8bd484aa635818ce76011dc048484cde206f9
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2017-09-22 15:24:11 -0400

    Update ChartDateEditor.js

--- html/gui/js/report_builder/ChartDateEditor.js
+++ html/gui/js/report_builder/ChartDateEditor.js
@@ -422,8 +422,7 @@ XDMoD.Reporting.ChartDateEditor = Ext.extend(Ext.Window,  {
             self.hide(); 
         }
       });//btnCancel
-
-
+       
       // -------------------------------------------------------------------
 
       var btnUpdate = new Ext.Button({
