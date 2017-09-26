b7feb9c79798f8bf0331e7a6a589d0fd212d1445
Ben Plessinger <bpless@buffalo.edu>
2017-09-26 16:12:36 -0400

    Cleanup DW Files before making changes

--- classes/DataWarehouse/Query/Jobs/Statistics/MaxProcessorCountStatistic.php
+++ classes/DataWarehouse/Query/Jobs/Statistics/MaxProcessorCountStatistic.php
@@ -1,25 +1,31 @@
 <?php
 namespace DataWarehouse\Query\Jobs\Statistics;
 
-/* 
-* @author Amin Ghadersohi
-* @date 2011-Feb-07
-*
-* class for calculating the max processor count 
-*/
+/*
+ * @author Amin Ghadersohi
+ * @date 2011-Feb-07
+ *
+ * class for calculating the max processor count
+ */
 
 class MaxProcessorCountStatistic extends \DataWarehouse\Query\Jobs\Statistic
 {
-	public function __construct($query_instance = NULL)
-	{
-		parent::__construct('coalesce(max(jf.processors),0)', 'max_processors', 'Job Size: Max', 'Core Count',0);
-	}
+    public function __construct($query_instance = null)
+    {
+        parent::__construct(
+            'COALESCE(
+                MAX(jf.processors)
+            , 0)',
+            'max_processors',
+            'Job Size: Max',
+            'Core Count',
+            0
+        );
+    }
 
-	public function getInfo()
-	{
-		return 	"The maximum size ".ORGANIZATION_NAME." job in number of cores.<br/>
-		<i>Job Size: </i>The total number of processor cores used by a (parallel) job.";
-	}
+    public function getInfo()
+    {
+        return "The maximum size " . ORGANIZATION_NAME . " job in number of cores.<br/>
+            <i>Job Size: </i>The total number of processor cores used by a (parallel) job.";
+    }
 }
- 
-?>
--- classes/DataWarehouse/Query/Jobs/Statistics/SEMAverageWallHoursStatistic.php
+++ classes/DataWarehouse/Query/Jobs/Statistics/SEMAverageWallHoursStatistic.php
@@ -1,30 +1,58 @@
 <?php
 namespace DataWarehouse\Query\Jobs\Statistics;
 
-/* 
-* @author Amin Ghadersohi
-* @date 2011-Feb-07
-*
-* class for calculating the average wall duration in hours 
-*/
+/*
+ * @author Amin Ghadersohi
+ * @date 2011-Feb-07
+ *
+ * class for calculating the average wall duration in hours
+ */
 
 class SEMAverageWallHoursStatistic extends \DataWarehouse\Query\Jobs\Statistic
 {
-	public function __construct($query_instance = NULL)
-	{
-		$job_count_formula = $query_instance->getQueryType() == 'aggregate'?'job_count':'running_job_count';
-		parent::__construct('coalesce(sqrt((sum(jf.sum_wallduration_squared)/sum(jf.'.$job_count_formula.'))-pow(sum(jf.wallduration)/sum(jf.'.$job_count_formula.'),2))/sqrt(sum(jf.'.$job_count_formula.')),0)/3600.0', 'sem_avg_wallduration_hours', 'Std Dev: Wall Hours: Per Job' , 'Hour',2);
-	}
+    public function __construct($query_instance = null)
+    {
+        $job_count_formula = $query_instance->getQueryType() == 'aggregate' ? 'job_count' : 'running_job_count';
+        parent::__construct(
+            'COALESCE(
+                SQRT(
+                    (
+                        SUM(jf.sum_wallduration_squared)
+                        /
+                        SUM(jf.' . $job_count_formula . ')
+                    )
+                    -
+                    POW(
+                        SUM(jf.wallduration)
+                        /
+                        SUM(jf.' . $job_count_formula . ')
+                        ,
+                        2
+                    )
+                )
+                /
+                SQRT(
+                    SUM(jf.' . $job_count_formula . ')
+                )
+                ,
+                0
+            )
+            /
+            3600.0',
+            'sem_avg_wallduration_hours',
+            'Std Dev: Wall Hours: Per Job',
+            'Hour',
+            2
+        );
+    }
 
-	public function getInfo()
-	{
-		return "The standard error of the average time each ".ORGANIZATION_NAME." job took to execute.<br/>
-		<i>Std Err of the Avg: </i> The standard deviation of the sample mean, estimated by the sample estimate of the population standard deviation (sample standard deviation) divided by the square root of the sample size (assuming statistical independence of the values in the sample).";
-	}
-	public function isVisible()
-	{
-		return false;
-	}
+    public function getInfo()
+    {
+        return "The standard error of the average time each " . ORGANIZATION_NAME . " job took to execute.<br/>
+            <i>Std Err of the Avg: </i> The standard deviation of the sample mean, estimated by the sample estimate of the population standard deviation (sample standard deviation) divided by the square root of the sample size (assuming statistical independence of the values in the sample).";
+    }
+    public function isVisible()
+    {
+        return false;
+    }
 }
- 
-?>
