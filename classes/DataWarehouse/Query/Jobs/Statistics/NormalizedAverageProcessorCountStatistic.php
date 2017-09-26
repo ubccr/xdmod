5cc4d7906db63e16f0d137ca07e68d0d6b087cea
Ben Plessinger <bpless@buffalo.edu>
2017-09-26 16:12:36 -0400

    Cleanup DW Files before making changes

--- classes/DataWarehouse/Query/Jobs/Statistics/NormalizedAverageProcessorCountStatistic.php
+++ classes/DataWarehouse/Query/Jobs/Statistics/NormalizedAverageProcessorCountStatistic.php
@@ -2,23 +2,51 @@
 namespace DataWarehouse\Query\Jobs\Statistics;
 
 /*
-* @author Amin Ghadersohi
-* @date 2011-Feb-07
-*
-* class for calculating the normalized average processor count
-*/
+ * @author Amin Ghadersohi
+ * @date 2011-Feb-07
+ *
+ * class for calculating the normalized average processor count
+ */
 
 class NormalizedAverageProcessorCountStatistic extends \DataWarehouse\Query\Jobs\Statistic
 {
     public function __construct($query_instance = null)
     {
-        parent::__construct('100.0*coalesce(sum(jf.processors*jf.running_job_count)/sum(jf.running_job_count)/(select sum(rrf.processors) from modw.resourcespecs rrf where find_in_set(rrf.resource_id,group_concat(distinct jf.resource_id)) <> 0 and '.$query_instance->getAggregationUnit()->getUnitName().'_end_ts >= rrf.start_date_ts and (rrf.end_date_ts is null or '.$query_instance->getAggregationUnit()->getUnitName().'_end_ts <= rrf.end_date_ts)),0)', 'normalized_avg_processors', 'Job Size: Normalized', '% of Total Cores', 1);
+        parent::__construct(
+            '100.0 *
+                COALESCE(
+                    SUM(jf.processor_count * jf.running_job_count)
+                    /
+                    SUM(jf.running_job_count)
+                    /
+                    (
+                        SELECT
+                            SUM(rrf.processors)
+                        FROM
+                            modw.resourcespecs rrf
+                        WHERE
+                            FIND_IN_SET(
+                                rrf.resource_id,
+                                GROUP_CONCAT(DISTINCT jf.resource_id)
+                            ) <> 0
+                            AND ' . $query_instance->getAggregationUnit()->getUnitName().'_end_ts >= rrf.start_date_ts
+                            AND (
+                                rrf.end_date_ts IS NULL
+                                OR ' . $query_instance->getAggregationUnit()->getUnitName() . '_end_ts <= rrf.end_date_ts
+                            )
+                    )
+                , 0)',
+            'normalized_avg_processors',
+            'Job Size: Normalized',
+            '% of Total Cores',
+            1
+        );
     }
 
     public function getInfo()
     {
-        return  "The percentage average size ".ORGANIZATION_NAME." job over total machine cores.<br>
-        <i>Normalized Job Size: </i>The percentage total number of processor cores used by a (parallel) job over the total number of cores on the machine.";
+        return "The percentage average size " . ORGANIZATION_NAME . " job over total machine cores.<br>
+            <i>Normalized Job Size: </i>The percentage total number of processor cores used by a (parallel) job over the total number of cores on the machine.";
     }
 
     /**
