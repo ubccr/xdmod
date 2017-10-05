6e071bee31e63638b822a0e52f12e5c18447388b
Joseph White <jpwhite4@buffalo.edu>
2017-10-05 14:56:04 -0400

    Impove robustness of report generator autotest

--- open_xdmod/modules/xdmod/automated_tests/test/specs/xdmod/reportGenerator.js
+++ open_xdmod/modules/xdmod/automated_tests/test/specs/xdmod/reportGenerator.js
@@ -359,7 +359,14 @@ describe('Report Generator', function () {
             });
             it('Check available charts', function () {
                 reportGeneratorPage.selectTab();
-                const charts = reportGeneratorPage.getAvailableCharts();
+                var charts;
+                for (let i = 0; i < 100; i++) {
+                    charts = reportGeneratorPage.getAvailableCharts();
+                    if (charts.length === (index + 1)) {
+                        break;
+                    }
+                    browser.pause(20);
+                }
                 expect(charts.length, `${index + 1} chart(s) in the list of available charts`).to.be.equal(index + 1);
 
                 for (let i = 0; i <= index; ++i) {
