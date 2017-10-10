ubccr/xdmod

commit 912660f04ad7e0b76c1c4a3deee4934dcbcc5c39

JSON encode drill down data (#266)

classes/DataWarehouse/Access/Usage.php
@@ -754,24 +754,32 @@ class Usage extends Common
                     // If this is not a trend line series and not a thumbnail,
                     // fill in the drilldown function.
                     if (!$isTrendLineSeries && !$thumbnailRequested) {
-                        $drillDowns = implode(',', $user->getMostPrivilegedRole()->getQueryDescripters(
-                            'tg_usage',
-                            $usageRealm,
-                            $usageGroupBy,
-                            $meRequestMetric->getAlias()->getName()
-                        )->getDrillTargets($meRequestMetric->getAlias()));
+                        $drillDowns = json_encode(
+                            array_map(
+                                function ($drillTarget) {
+                                    return explode('-', $drillTarget, 2);
+                                },
+                                $user->getMostPrivilegedRole()->getQueryDescripters(
+                                    'tg_usage',
+                                    $usageRealm,
+                                    $usageGroupBy,
+                                    $meRequestMetric->getAlias()->getName()
+                                )->getDrillTargets($meRequestMetric->getAlias())
+                            )
+                        );
                         $usageGroupByUnit = $usageGroupByObject->getUnit();
 
                         if ($meRequestIsTimeseries) {
                             $drilldownDetails = $meDataSeries['drilldown'];
                             $drilldownId = $drilldownDetails['id'];
                             $drilldownLabel = json_encode($drilldownDetails['label']);
+                            $groupByNameAndUnit = json_encode(array($usageGroupBy, $usageGroupByUnit));
                             $drilldownFunction = "function(event) {
                                 this.ts = this.x;
                                 XDMoD.Module.Usage.drillChart(
                                     this,
-                                    '$drillDowns',
-                                    '${usageGroupBy}-${usageGroupByUnit}',
+                                    $drillDowns,
+                                    $groupByNameAndUnit,
                                     '$drilldownId',
                                     $drilldownLabel,
                                     'none',
@@ -785,8 +793,8 @@ class Usage extends Common
                                 var label = this.drilldown.label;
                                 XDMoD.Module.Usage.drillChart(
                                     this,
-                                    '$drillDowns',
-                                    '${usageGroupBy}-${usageGroupByUnit}',
+                                    $drillDowns,
+                                    $groupByNameAndUnit,
                                     id,
                                     label,
                                     'none',

classes/DataWarehouse/Query/GroupBy.php
@@ -57,6 +57,8 @@ abstract class GroupBy extends \Common\Identity
 
     /*
     * Constructor
+    *
+    * @param string $name The group by name.  The name must not contain any hyphen-minus characters ("-").
     */
     public function __construct($name, array $statistics = array(), $possible_values_query = NULL)
     { 

html/gui/js/DrillDownMenu.js
@@ -25,11 +25,11 @@ Ext.extend(CCR.xdmod.ui.DrillDownMenu, Ext.menu.Menu, {
     drillDownGroupBys: [],
     initComponent: function () {
         var items = [];
-        if (this.drillDownGroupBys != '') {
-            var groupByDescripter = this.drillDownGroupBys.split(',');
+        if (this.drillDownGroupBys.length !== 0) {
+            var groupByDescripter = this.drillDownGroupBys;
 
             for (var i = 0; i < groupByDescripter.length; i++) {
-                var gbd = groupByDescripter[i].split('-');
+                var gbd = groupByDescripter[i];
                 if (gbd.length == 2 &&
                     (this.node.attributes.parameters == null ||
                         (this.node.attributes.parameters[gbd[0]] == null &&
@@ -52,7 +52,7 @@ Ext.extend(CCR.xdmod.ui.DrillDownMenu, Ext.menu.Menu, {
                     items.push(
                         new Ext.menu.Item({
                             scope: this,
-                            drillDown: groupByDescripter[i],
+                            drillDown: gbd,
                             paramLabel: gbd[1],
                             text: gbd[1],
                             iconCls: 'drill',

html/gui/js/modules/Usage.js
@@ -82,8 +82,6 @@ Ext.apply(XDMoD.Module.Usage, {
             return;
         }
 
-        groupByNameAndUnit = groupByNameAndUnit.split('-');
-
         if (groupByNameAndUnit.length < 2) return;
 
         var groupByName = groupByNameAndUnit[0];
@@ -97,9 +95,6 @@ Ext.apply(XDMoD.Module.Usage, {
         }));
 
         function drillDown(drillDown) {
-
-            var drillDown = drillDown.split('-'); // only one drilldown per chart right now;
-
             var drillDownGroupByName = drillDown[0];
             var drillDownGroupByLabel = drillDown[1];
 

commit 426169a6a67f2f2a26cd0587fb67b2205cb0d6c8

Fix bug introduced by #266 (#291)

classes/DataWarehouse/Access/Usage.php
@@ -768,12 +768,12 @@ class Usage extends Common
                             )
                         );
                         $usageGroupByUnit = $usageGroupByObject->getUnit();
+                        $groupByNameAndUnit = json_encode(array($usageGroupBy, $usageGroupByUnit));
 
                         if ($meRequestIsTimeseries) {
                             $drilldownDetails = $meDataSeries['drilldown'];
                             $drilldownId = $drilldownDetails['id'];
                             $drilldownLabel = json_encode($drilldownDetails['label']);
-                            $groupByNameAndUnit = json_encode(array($usageGroupBy, $usageGroupByUnit));
                             $drilldownFunction = "function(event) {
                                 this.ts = this.x;
                                 XDMoD.Module.Usage.drillChart(