e2f3f4414f43d0325b9d6589b8bcc35d62b5d638
Ben Plessinger <bpless@buffalo.edu>
2017-10-12 09:00:03 -0400

    TRAVIS!!!

--- open_xdmod/modules/xdmod/automated_tests/test/specs/xdmod/loginPage.page.js
+++ open_xdmod/modules/xdmod/automated_tests/test/specs/xdmod/loginPage.page.js
@@ -7,7 +7,7 @@ class LoginPage {
         let password;
         let displayName;
         let role;
-        switch (desiredRole){
+        switch (desiredRole) {
             case 'cd':
             case 'centerdirector':
                 role = 'cd';
@@ -62,10 +62,8 @@ class LoginPage {
                 browser.frame();
             });
             it('Display Logged in Users Name', function () {
-
                 $('#welcome_message').waitForVisible(60000);
                 expect($('#welcome_message').getText()).to.equal(displayName);
-
                 $('#main_tab_panel__about_xdmod').waitForVisible();
             });
         });
