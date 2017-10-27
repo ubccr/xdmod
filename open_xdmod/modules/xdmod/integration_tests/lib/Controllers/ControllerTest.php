e9e6246e3cfc4e2df391f33f74f8a3fdd1849db1
Ryan Rathsam <ryanrath@buffalo.edu>
2017-10-27 13:43:40 -0400

    Debugging Shippable

--- open_xdmod/modules/xdmod/integration_tests/lib/Controllers/ControllerTest.php
+++ open_xdmod/modules/xdmod/integration_tests/lib/Controllers/ControllerTest.php
@@ -145,6 +145,8 @@ JSON;
         // Set the newly ordered users back into the data structure.
         $expectedJson['response'] = $newUsers;
         $diff = $this->arrayRecursiveDiff($expectedJson, $data);
+        echo "\nExpected: " . json_encode($expectedJson) . "\n";
+        echo "Actual : " . json_encode($data) . "\n";
         echo "Diff: " . json_encode($diff) . "\n";
         $success = true;
         array_walk_recursive(
