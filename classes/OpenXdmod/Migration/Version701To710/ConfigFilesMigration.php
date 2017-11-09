9c3db579fb954faac6c9b8faa469332bcdbcdd4c
Ryan Rathsam <ryanrath@buffalo.edu>
2017-11-09 09:14:14 -0500

    Adding a ConfigFilesMigration for datawarehouse.json
    - Added a step to the ConfigFilesMigration that ensures that
      `datawarehouse.json` -> 'Realms' -> 'Jobs' -> 'group_bys' contains an entry
      for 'provider'.

--- classes/OpenXdmod/Migration/Version701To710/ConfigFilesMigration.php
+++ classes/OpenXdmod/Migration/Version701To710/ConfigFilesMigration.php
@@ -5,6 +5,7 @@
 
 namespace OpenXdmod\Migration\Version701To710;
 
+use CCR\Json;
 use OpenXdmod\Migration\ConfigFilesMigration as AbstractConfigFilesMigration;
 
 /**
@@ -22,5 +23,32 @@ class ConfigFilesMigration extends AbstractConfigFilesMigration
 
         // Set new options in portal_settings.ini.
         $this->writePortalSettingsFile();
+
+        // Add the 'provider' group_by if it does not already exist.
+        $this->modifyDatawarehouse();
+    }
+
+    public function modifyDatawarehouse()
+    {
+        $datawarehouseFile = $this->config->getFilePath('datawarehouse');
+        $datawarehouse = Json::loadFile($datawarehouseFile);
+        $realms = isset($datawarehouse['realms']) ? $datawarehouse['realms'] : array();
+        $jobs = isset($realms['Jobs']) ? $realms['Jobs'] : array();
+        $groupBys = isset($jobs['group_bys']) ? $jobs['group_bys'] : array();
+
+        $found = array_filter(
+            $groupBys,
+            function ($groupBy) {
+                return isset($groupBy['name']) && $groupBy['name'] === 'provider';
+            }
+        );
+        if (empty($found)) {
+            $groupBys[] = array(
+                'name' => 'provider',
+                'class' => 'GroupByProvider'
+            );
+            $datawarehouse['realms']['Jobs']['group_bys'] = $groupBys;
+            $this->writeJsonConfigFile('datawarehouse', $datawarehouse);
+        }
     }
 }
