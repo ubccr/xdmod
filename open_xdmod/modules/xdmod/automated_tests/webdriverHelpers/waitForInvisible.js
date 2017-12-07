5b1c351552f403ecc7328b32c667b6f64d5e98e5
Ben <ben@plessinger.us>
2017-12-07 14:25:02 -0500

    update timeouts from 1s to 5s to be more consistient

--- open_xdmod/modules/xdmod/automated_tests/webdriverHelpers/waitForInvisible.js
+++ open_xdmod/modules/xdmod/automated_tests/webdriverHelpers/waitForInvisible.js
@@ -2,12 +2,12 @@
  *  Wait for selector to be invisible
  *
  *  @param {string} selector - element to check
- *  @param {Number} [ms=1000] - Milliseconds to wait for element to be invisible
+ *  @param {Number} [ms=5000] - Milliseconds to wait for element to be invisible
  *
  *  @uses commands/waitForVisible
  */
 
 module.exports = function waitForInvisible(selector, ms) {
-    var timeOut = ms || 1000;
+    var timeOut = ms || 5000;
     return this.waitForVisible(selector, timeOut, true);
 };
