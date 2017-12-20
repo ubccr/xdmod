ba202ca00ad166873160d2e528f436d3372a0ec3
Ryan Rathsam <ryanrath@buffalo.edu>
2017-12-20 15:34:14 -0500

    Data Quality addition
    - Added a sql statement that will ensure that, for all Users whose `person_id`
      does not have a reference in modw.person, set these `person_id` values to -1
      ( the 'unknown' person ).

--- configuration/etl/etl_sql.d/acls/xdmod/update_admin_users.sql
+++ configuration/etl/etl_sql.d/acls/xdmod/update_admin_users.sql
@@ -29,3 +29,12 @@ INSERT INTO user_acls (user_id, acl_id)
         cur.user_id = inc.user_id AND
         cur.acl_id = inc.user_id
   WHERE cur.user_acl_id IS NULL;
+
+-- =============================================================================
+-- PURPOSE: Ensure that any User records that do not have a valid `person_id`
+-- reference to modw.person have their `person_id` updated to -1 ( unknown ).
+-- =============================================================================
+UPDATE Users u LEFT JOIN modw.person p ON p.id = u.person_id
+SET u.person_id = -1
+WHERE
+  p.id IS NULL;
