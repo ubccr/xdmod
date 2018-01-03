3023a0c98a4cc89dca44516d4dbef324c7e955a0
Ben Plessinger <ben@plessinger.us>
2018-01-03 13:44:30 -0500

    add start and end dates to build scripts since our date range is well in the past

--- open_xdmod/modules/xdmod/integration_tests/scripts/bootstrap.sh
+++ open_xdmod/modules/xdmod/integration_tests/scripts/bootstrap.sh
@@ -22,9 +22,9 @@ then
     for resource in $REF_DIR/*.log; do
         xdmod-shredder -r `basename $resource .log` -f slurm -i $resource;
     done
-    xdmod-ingestor
+    xdmod-ingestor --start-date 2016-12-01 --end-date 2017-01-05
     xdmod-import-csv -t names -i $REF_DIR/names.csv
-    xdmod-ingestor
+    xdmod-ingestor --start-date 2016-12-01 --end-date 2017-01-05
     xdmod-build-filter-lists -r Jobs
     php /root/bin/createusers.php
     acl-import
