e563a700aa88c61ef5380895e685b07e5e2ef237
Ryan Rathsam <ryanrath@buffalo.edu>
2018-01-03 12:50:28 -0500

    Keeping control structures consistent
    - Per the previous change of a `switch` to `if-else` updated this section in the
      same manner.

--- html/controllers/user_admin/create_user.php
+++ html/controllers/user_admin/create_user.php
@@ -84,13 +84,10 @@ try {
             $centerConfig = array();
             $count = 0;
             foreach($centers as $center) {
-                switch($count) {
-                    case 0:
-                        $config = array('primary' => 1, 'active' => 1);
-                        break;
-                    default:
-                        $config = array('primary' => 0, 'active' => 0);
-                        break;
+                if ($count === 0) {
+                    $config = array('primary' => 1, 'active' => 1);
+                } else {
+                    $config = array('primary' => 0, 'active' => 0);
                 }
                 $centerConfig[$center] = $config;
                 $count += 1;
