1ae38fbeb835a1a2a055de531247c5d45ceaebf9
Ben Plessinger <ben@plessinger.us>
2018-01-16 13:17:36 -0500

    make the regression test names actually be unique

--- open_xdmod/modules/xdmod/regression_tests/lib/Controllers/UsageExplorerTest.php
+++ open_xdmod/modules/xdmod/regression_tests/lib/Controllers/UsageExplorerTest.php
@@ -165,7 +165,7 @@ class UsageExplorerTest extends \PHPUnit_Framework_TestCase
                 if (file_exists($expectedFile) ) {
                     $testCase[2] = file_get_contents($expectedFile);
                 }
-                $testData[$testName] = $testCase;
+                $testData[$testName. '-' . $k . '-' . (empty($envUserrole) ? 'public' : $envUserrole)] = $testCase;
             }
         }
         if(empty($testData)){
