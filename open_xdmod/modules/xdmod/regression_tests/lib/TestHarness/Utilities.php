1642a38a3963287252c3c4051b8768b86c6fe709
Joseph White <jpwhite4@buffalo.edu>
2018-01-23 16:59:16 -0500

    Add image tests; only run a random subset of tests
    Add tests that check the usage explorer chart image export works
    for a large combination of different chart settings.
    
    A pseudo-random subset of the tests is run by default. Simply
    export REG_TEST_ALL=1 to have all tests run (this should take 15 - 20
    minutes or so).

---
+++ open_xdmod/modules/xdmod/regression_tests/lib/TestHarness/Utilities.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace TestHarness;
+
+class Utilities
+{
+    public static function getCombinations($arrays) {
+        $result = array(array());
+        foreach ($arrays as $property => $property_values) {
+            $tmp = array();
+            foreach ($result as $result_item) {
+                foreach ($property_values as $property_value) {
+                    $tmp[] = array_merge($result_item, array($property => $property_value));
+                }
+            }
+            $result = $tmp;
+        }
+        return $result;
+    }
+}
