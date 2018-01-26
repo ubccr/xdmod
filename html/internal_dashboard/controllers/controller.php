f45bcab1d8eb51d3c0834adb002afc8871288ac7
Ryan Rathsam <ryanrath@buffalo.edu>
2018-01-26 16:23:22 -0500

    Temporary Insanity
    or is it...

--- html/internal_dashboard/controllers/controller.php
+++ html/internal_dashboard/controllers/controller.php
@@ -125,7 +125,7 @@ switch ($operation) {
 
     case 'enum_user_types_and_roles':
 
-        $query = "SELECT id, type, color FROM moddb";
+        $query = "SELECT id, type, color FROM moddb.UserTypes";
 
         $results = $pdo->query($query);
 
