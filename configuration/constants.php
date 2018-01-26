eeeaac84cc0becff175a946b39713eff82a82ffa
Ryan Rathsam <ryanrath@buffalo.edu>
2018-01-26 11:59:20 -0500

    Removing XSEDE_USER_TYPE constant
    - This constant is no longer needed / used.

--- configuration/constants.php
+++ configuration/constants.php
@@ -27,7 +27,6 @@ define('INVALID', '-1p');
 define('AMBIGUOUS', '-2p');
 define('NO_MAPPING', '-3p');
 define('NO_EMAIL_ADDRESS_SET', 'no_email_address_set');
-define('XSEDE_USER_TYPE', 700);
 define('FEDERATED_USER_TYPE', 5);
 define('DEMO_USER_TYPE', 4);
 
