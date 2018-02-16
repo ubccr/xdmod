df027a428ccf3a0a480b635ec3ffdcf9c6f162e5
Ryan Rathsam <ryanrath@buffalo.edu>
2018-02-16 15:18:10 -0500

    Reverting Jobs/GroupBy.php to pre-modified state

--- classes/DataWarehouse/Query/Jobs/GroupBy.php
+++ classes/DataWarehouse/Query/Jobs/GroupBy.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace DataWarehouse\Query\Jobs;
 
 /*
@@ -9,18 +8,12 @@ namespace DataWarehouse\Query\Jobs;
 * class for adding group bys to a jobs query
 *
 */
-use Xdmod\Config;
-
 abstract class GroupBy extends \DataWarehouse\Query\GroupBy
 {
-    public function __construct($name, array $additional_permitted_parameters = array(), $possible_values_query = null)
+    public function __construct($name, array $additional_permitted_parameters = array(), $possible_values_query = NULL)
     {
-        $permitted_paramters = array_unique(
-            array_merge(
-                array_keys(Aggregate::getRegisteredStatistics()),
-                $additional_permitted_parameters
-            )
-        );
+        $permitted_paramters = array_unique(array_merge(
+            array_keys(Aggregate::getRegisteredStatistics()), $additional_permitted_parameters));
 
         parent::__construct($name, $permitted_paramters, $possible_values_query);
     }
@@ -29,19 +22,26 @@ abstract class GroupBy extends \DataWarehouse\Query\GroupBy
     {
         $registerd_group_bys = Aggregate::getRegisteredGroupBys();
         $drill_target_group_bys = array();
-        foreach ($registerd_group_bys as $group_by_name => $group_by_classname) {
-            if ($group_by_name == 'none' || $group_by_name == $this->getName()) {
-                continue;
-            }
+
+        foreach($registerd_group_bys as $group_by_name => $group_by_classname)
+        {
+            if($group_by_name == 'none' || $group_by_name == $this->getName()) continue;
+
+            $group_by_classname = $query_classname::getGroupByClassname($group_by_name);
             $group_by_instance = $query_classname::getGroupBy($group_by_name);
             $permitted_stats = $group_by_instance->getPermittedStatistics();
 
-            if ($group_by_instance->getAvailableOnDrilldown() !== false && array_search($statistic_name, $permitted_stats) !== false) {
-                $drill_target_group_bys[] = $group_by_name . '-' . $group_by_instance->getLabel();
+            if($group_by_instance->getAvailableOnDrilldown() !== false && array_search($statistic_name,$permitted_stats) !== false)
+            {
+                $drill_target_group_bys[] = $group_by_name.'-'.$group_by_instance->getLabel();
             }
         }
 
         sort($drill_target_group_bys);
         return $drill_target_group_bys;
     }
+
+
 }
+
+?>
