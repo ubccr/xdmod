ef8c85fc2e1b1c094c3e0de109540d8e2f594575
Ryan Rathsam <ryanrath@buffalo.edu>
2018-02-20 14:49:15 -0500

    Spacing Updates

--- html/controllers/role_manager/enum_center_staff_members.php
+++ html/controllers/role_manager/enum_center_staff_members.php
@@ -2,30 +2,28 @@
 
 use Models\Services\Centers;
 
-	try {
-	
-   	$activeUser = \xd_security\getLoggedInUser();
+try {
 
-        $members = Centers::getCenterStaffMembers($activeUser);
+    $activeUser = \xd_security\getLoggedInUser();
 
-    	$returnData = array();
-    	
-    	$returnData['success'] = true;
-    	$returnData['count'] = count($members);
-   	$returnData['members'] = $members;
-   	
-   	echo json_encode($returnData);
-	
-	}
-  catch (SessionExpiredException $see) {
+    $members = Centers::getCenterStaffMembers($activeUser);
+
+    $returnData = array();
+
+    $returnData['success'] = true;
+    $returnData['count'] = count($members);
+    $returnData['members'] = $members;
+
+    echo json_encode($returnData);
+
+} catch (SessionExpiredException $see) {
     // TODO: Refactor generic catch block below to handle specific exceptions,
     //       which would allow this block to be removed.
     throw $see;
-  }
-   catch (\Exception $e){
-   
-      \xd_response\presentError($e->getMessage());
-      
-   }	
-	
+} catch (\Exception $e) {
+
+    \xd_response\presentError($e->getMessage());
+
+}
+
 ?>
