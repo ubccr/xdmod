fa8cc26d7abdadd706037c371413aeeeb42998b4
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2018-02-26 11:52:22 -0500

    Added a space.

--- classes/XDUser.php
+++ classes/XDUser.php
@@ -545,7 +545,7 @@ SQL;
 
     public function setPassword($raw_password)
     {
-        if ($this->getUserType() === FEDERATED_USER_TYPE){
+        if ($this->getUserType() === FEDERATED_USER_TYPE) {
             throw new AccessDeniedException("Permission Denied. Only local accounts may have their passwords modified.");
         }
 
