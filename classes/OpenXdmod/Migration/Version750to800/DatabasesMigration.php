eecb21fe7aac265e1e0ae624a84e5ee25f4a3638
Ben Plessinger <ben@plessinger.us>
2018-03-01 14:42:09 -0500

    Version Upgrade

---
+++ classes/OpenXdmod/Migration/Version750to800/DatabasesMigration.php
@@ -0,0 +1,9 @@
+<?php
+namespace OpenXdmod\Migration\Version700To701;
+
+/**
+ * Migrate databases from version 7.5.0 to 8.0.0.
+ */
+class DatabasesMigration extends \OpenXdmod\Migration\Version660To700\DatabasesMigration
+{
+}
