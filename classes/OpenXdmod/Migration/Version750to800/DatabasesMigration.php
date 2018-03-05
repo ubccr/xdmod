305f633a2c71af6e665ec8e9051acc08457b7f72
Ben Plessinger <plessbd@users.noreply.github.com>
2018-03-05 09:38:25 -0500

    Make Upgrades work again (#454)

--- classes/OpenXdmod/Migration/Version750to800/DatabasesMigration.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace OpenXdmod\Migration\Version700To701;
-
-/**
- * Migrate databases from version 7.5.0 to 8.0.0.
- */
-class DatabasesMigration extends \OpenXdmod\Migration\Version660To700\DatabasesMigration
-{
-}
