26367d3fbb813a44a34eabbf90b45e36030aa6b3
Ben Plessinger <ben@plessinger.us>
2018-03-06 07:11:01 -0500

    make databases the same

--- db/migrations/7.5.0-8.0.0/modw_aggregates.sql
+++ db/migrations/7.5.0-8.0.0/modw_aggregates.sql
@@ -7,7 +7,7 @@ ALTER TABLE `jobfact_by_day`
     CHANGE `jobtime_id` `job_time_bucket_id` INT(3)  NOT NULL  COMMENT 'DIMENSION: Job time is bucketing of wall time based on prechosen intervals in the modw.job_times table.',
     CHANGE `nodecount_id` `node_count` INT(8)  NOT NULL  COMMENT 'DIMENSION: Number of nodes each of the jobs used.',
     CHANGE `queue_id` `queue` CHAR(50)  CHARACTER SET latin1  COLLATE latin1_swedish_ci  NOT NULL  DEFAULT ''  COMMENT 'DIMENSION: The queue of the resource on which the jobs ran.',
-    CHANGE `organization_id` `resource_organization_id` INT(11)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
+    CHANGE `organization_id` `resource_organization_id` INT(6)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
 
 ALTER TABLE `jobfact_by_month`
     CHANGE `resource_id` `task_resource_id` INT(5)  NOT NULL  COMMENT 'DIMENSION: The resource on which the jobs ran',
@@ -16,7 +16,7 @@ ALTER TABLE `jobfact_by_month`
     CHANGE `jobtime_id` `job_time_bucket_id` INT(3)  NOT NULL  COMMENT 'DIMENSION: Job time is bucketing of wall time based on prechosen intervals in the modw.job_times table.',
     CHANGE `nodecount_id` `node_count` INT(8)  NOT NULL  COMMENT 'DIMENSION: Number of nodes each of the jobs used.',
     CHANGE `queue_id` `queue` CHAR(50)  CHARACTER SET latin1  COLLATE latin1_swedish_ci  NOT NULL  DEFAULT ''  COMMENT 'DIMENSION: The queue of the resource on which the jobs ran.',
-    CHANGE `organization_id` `resource_organization_id` INT(11)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
+    CHANGE `organization_id` `resource_organization_id` INT(6)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
 
 ALTER TABLE `jobfact_by_quarter`
     CHANGE `resource_id` `task_resource_id` INT(5)  NOT NULL  COMMENT 'DIMENSION: The resource on which the jobs ran',
@@ -25,7 +25,7 @@ ALTER TABLE `jobfact_by_quarter`
     CHANGE `jobtime_id` `job_time_bucket_id` INT(3)  NOT NULL  COMMENT 'DIMENSION: Job time is bucketing of wall time based on prechosen intervals in the modw.job_times table.',
     CHANGE `nodecount_id` `node_count` INT(8)  NOT NULL  COMMENT 'DIMENSION: Number of nodes each of the jobs used.',
     CHANGE `queue_id` `queue` CHAR(50)  CHARACTER SET latin1  COLLATE latin1_swedish_ci  NOT NULL  DEFAULT ''  COMMENT 'DIMENSION: The queue of the resource on which the jobs ran.',
-    CHANGE `organization_id` `resource_organization_id` INT(11)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
+    CHANGE `organization_id` `resource_organization_id` INT(6)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
 
 ALTER TABLE `jobfact_by_year`
     CHANGE `resource_id` `task_resource_id` INT(5)  NOT NULL  COMMENT 'DIMENSION: The resource on which the jobs ran',
@@ -34,4 +34,4 @@ ALTER TABLE `jobfact_by_year`
     CHANGE `jobtime_id` `job_time_bucket_id` INT(3)  NOT NULL  COMMENT 'DIMENSION: Job time is bucketing of wall time based on prechosen intervals in the modw.job_times table.',
     CHANGE `nodecount_id` `node_count` INT(8)  NOT NULL  COMMENT 'DIMENSION: Number of nodes each of the jobs used.',
     CHANGE `queue_id` `queue` CHAR(50)  CHARACTER SET latin1  COLLATE latin1_swedish_ci  NOT NULL  DEFAULT ''  COMMENT 'DIMENSION: The queue of the resource on which the jobs ran.',
-    CHANGE `organization_id` `resource_organization_id` INT(11)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
+    CHANGE `organization_id` `resource_organization_id` INT(6)  NOT NULL  COMMENT 'DIMENSION: The organization of the resource that the jobs ran on.';
