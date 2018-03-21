aa8855162f5e851b12488ab4a1cd3cabac7faa4a
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2018-03-21 16:20:37 -0400

    Added Disk and Memory statistics

--- classes/DataWarehouse/Query/Cloud/Statistics/DiskReservedStatistic.php
+++ classes/DataWarehouse/Query/Cloud/Statistics/DiskReservedStatistic.php
@@ -5,24 +5,24 @@ namespace DataWarehouse\Query\Cloud\Statistics;
 * @author Rudra Chakraborty
 * @date 03-21-2018
 *
-* Sum of cores reserved on active virtual machines
+* Sum of disk space reserved on active virtual machines
 */
-class CoresReservedStatistic extends \DataWarehouse\Query\Cloud\Statistic
+class DiskReservedStatistic extends \DataWarehouse\Query\Cloud\Statistic
 {
     public function __construct($query_instance = null)
     {
         parent::__construct(
-            'COALESCE(SUM(jf.num_cores),0)',
-            'num_cores',
-            'Number of Cores Reserved',
-            'Number of Cores',
+            'COALESCE(SUM(jf.disk_gb),0)',
+            'disk_gb',
+            'Amount of Disk Space Reserved',
+            'Disk Space',
             0
         );
     }
 
     public function getInfo()
     {
-        return 'The total number of cores reserved by active VM instances.<br/>
+        return 'The amount of disk space (in gigabytes) reserved by active VM instances.<br/>
             <i>VM Instance: </i>An individual virtual machine (VM) spun up within a cloud.';
     }
 }
