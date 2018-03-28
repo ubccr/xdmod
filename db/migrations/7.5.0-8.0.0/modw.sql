b337e3690c70e60d52553b287a3527fa62e72cc9
Ben Plessinger <plessbd@users.noreply.github.com>
2018-03-27 20:14:44 -0400

    Schema changes required for federation module (#494)
    * Schema changes required for federation module

--- db/migrations/7.5.0-8.0.0/modw.sql
+++ db/migrations/7.5.0-8.0.0/modw.sql
@@ -1,2 +1,16 @@
 use modw;
 INSERT INTO `schema_version_history` VALUES ('modw', '8.0.0', NOW(), 'upgraded', 'N/A');
+
+ALTER TABLE `modw`.`account`
+ADD COLUMN `account_origin_id` int(10) unsigned NOT NULL DEFAULT 0,
+ADD COLUMN `federation_blade_id` int(11) unsigned NOT NULL DEFAULT 0;
+UPDATE `modw`.`account` SET account_origin_id = id;
+
+ALTER TABLE `modw`.`allocation`
+ADD COLUMN `allocation_origin_id` int(10) unsigned NOT NULL;
+UPDATE `modw`.`allocation` SET  allocation_origin_id = id;
+
+ALTER TABLE `modw`.`person`
+ADD COLUMN `person_origin_id` int(11) NOT NULL DEFAULT 0,
+CHANGE COLUMN `id` `id` int(11) NOT NULL auto_increment;
+UPDATE `modw`.`person` SET person_origin_id = id;
