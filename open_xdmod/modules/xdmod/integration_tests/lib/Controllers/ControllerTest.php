a4c59db10b5cf5ce1291729a4c08d11200c73174
Ryan Rathsam <ryanrath@buffalo.edu>
2018-04-12 10:29:32 -0400

    Updating input for test affected by this PRs changes
    enum_target_addresses was affected by the changes introduced in this PR so the
    input file has been updated to reflect which new output files to utilize during
    testing.

--- open_xdmod/modules/xdmod/integration_tests/lib/Controllers/ControllerTest.php
+++ open_xdmod/modules/xdmod/integration_tests/lib/Controllers/ControllerTest.php
@@ -570,7 +570,7 @@ class ControllerTest extends \PHPUnit_Framework_TestCase
      */
     public function provideEnumTargetAddresses()
     {
-        $data = JSON::loadFile($this->getTestFiles()->getFile('controllers', 'enum_target_addresses', 'input'));
+        $data = JSON::loadFile($this->getTestFiles()->getFile('controllers', 'enum_target_addresses-update_enum_user_types_and_roles', 'input'));
         foreach($data as $key => $test) {
             foreach($test[0]['data'] as $dataKey => $value) {
                 $data[$key][0]['data'][$dataKey] = TestParameterHelper::processParam($value);
