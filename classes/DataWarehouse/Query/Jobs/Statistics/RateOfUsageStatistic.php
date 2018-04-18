ebb92722200fdbb6b4581b9bf78e785cba874072
Ben Plessinger <ben@plessinger.us>
2018-04-18 10:42:36 -0400

    fix column rename

--- classes/DataWarehouse/Query/Jobs/Statistics/RateOfUsageStatistic.php
+++ classes/DataWarehouse/Query/Jobs/Statistics/RateOfUsageStatistic.php
@@ -1,7 +1,7 @@
 <?php
 namespace DataWarehouse\Query\Jobs\Statistics;
 
-/* 
+/*
 * @author Amin Ghadersohi
 * @date 2011-Feb-07
 *
@@ -9,15 +9,14 @@ namespace DataWarehouse\Query\Jobs\Statistics;
 */
 class RateOfUsageStatistic extends \DataWarehouse\Query\Jobs\Statistic
 {
-	public function __construct($query_instance)
-	{
-		$formula = "coalesce(sum(jf.local_charge)/".($query_instance != NULL?$query_instance->getDurationFormula():1).",0)";
-		parent::__construct($formula, 'rate_of_usage', 'Allocation Usage Rate', 'XD SU/Hour',2);  
-	}
+    public function __construct($query_instance)
+    {
+        $formula = "coalesce(sum(jf.local_charge_su)/" . ($query_instance != null ? $query_instance->getDurationFormula():1).",0)";
+        parent::__construct($formula, 'rate_of_usage', 'Allocation Usage Rate', 'XD SU/Hour', 2);
+    }
 
-	public function getInfo()
-	{
-		return 	"The rate of ".ORGANIZATION_NAME." allocation usage in XD SUs per hour.";
-	}
+    public function getInfo()
+    {
+        return "The rate of " . ORGANIZATION_NAME . " allocation usage in XD SUs per hour.";
+    }
 }
-?>
