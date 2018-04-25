d6d5947f47081c7892b892268e2c97423f48c397
Greg Dean <gmdean@buffalo.edu>
2018-04-25 15:28:19 -0400

    Added post ingest sql to update destroy time for volume assets. Moved ingestion to two seperate actions one for ingestion of files and another for moving ingested data to dimension and fact tables

---
+++ configuration/etl/etl_sql.d/cloud_openstack/post_ingest_update.sql
@@ -0,0 +1,30 @@
+-- Update destroy times of volume assets. This updates both volumes that are explicitly
+-- attached to instances and also root volumes which are inferred from compute.instance.create
+-- events.
+
+UPDATE
+	${DESTINATION_SCHEMA}.asset AS a
+LEFT JOIN
+	${DESTINATION_SCHEMA}.openstack_raw_event AS raw
+ON
+  raw.resource_id = a.resource_id AND raw.openstack_resource_id = a.provider_identifier
+SET
+	a.destroy_time_utc = raw.event_time_utc
+WHERE
+	raw.event_type = "volume.delete.end";
+//
+
+UPDATE
+	${DESTINATION_SCHEMA}.asset AS a
+LEFT JOIN
+	${DESTINATION_SCHEMA}.instance AS i
+ON
+	i.resource_id = a.resource_id AND CONCAT('root-vol-', i.provider_identifier) = a.provider_identifier
+LEFT JOIN
+	${DESTINATION_SCHEMA}.openstack_staging_event AS staging
+ON
+	staging.resource_id = i.resource_id AND staging.instance_id = i.instance_id
+SET
+	a.destroy_time_utc = staging.event_time_utc
+WHERE
+	staging.event_type_id = 4;
