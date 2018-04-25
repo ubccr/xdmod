aba18f9c3a21723ac864c58cf1044fde5613124f
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2018-04-25 10:08:23 -0400

    disk reservation stat

---
+++ classes/DataWarehouse/Query/Cloud/Statistics/DiskReservationStatistic.php
@@ -0,0 +1,28 @@
+<?php
+namespace DataWarehouse\Query\Cloud\Statistics;
+
+/*
+* @author Rudra Chakraborty
+* @date 04-17-2018
+*
+* The total core reservation time of virtual machines, in hours
+*/
+class DiskReservationStatistic extends \DataWarehouse\Query\Cloud\Statistic
+{
+    public function __construct($query_instance = null)
+    {
+        parent::__construct(
+            'COALESCE((SUM(jf.disk_reserved) / 3600.0) / jf.wallduration,0)',
+            'disk_reserved',
+            'Average Disk Space Consumption',
+            'Gigabytes',
+            0
+        );
+    }
+
+    public function getInfo()
+    {
+        return 'The average memory reserved by a VM in a given period.<br/>
+            <i>VM Instance: </i>An individual virtual machine (VM) spun up within a cloud.';
+    }
+}
