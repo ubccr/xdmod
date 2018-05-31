4913af6c45401747357bea30e0ab2062fc5890d3
Ben Plessinger <plessbd@users.noreply.github.com>
2018-05-31 14:13:25 -0400

    federation blade -> instance (#563)
    terminology update

--- db/migrations/7.5.0-8.0.0/modw.sql
+++ db/migrations/7.5.0-8.0.0/modw.sql
@@ -3,7 +3,7 @@ INSERT INTO `schema_version_history` VALUES ('modw', '8.0.0', NOW(), 'upgraded',
 
 ALTER TABLE `modw`.`account`
 ADD COLUMN `account_origin_id` int(10) unsigned NOT NULL DEFAULT 0,
-ADD COLUMN `federation_blade_id` int(11) unsigned NOT NULL DEFAULT 0;
+ADD COLUMN `federation_instance_id` int(11) unsigned NOT NULL DEFAULT 0;
 UPDATE `modw`.`account` SET account_origin_id = id;
 
 ALTER TABLE `modw`.`allocation`
