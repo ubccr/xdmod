bd72af91e038077d6a18037c3dac9424d0b0fd55
Jeffrey T. Palmer <jtpalmer@buffalo.edu>
2018-06-21 12:30:12 -0400

    Support 7.5.1 install and upgrade

---
+++ classes/OpenXdmod/Migration/Version750To751/DatabasesMigration.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @author Steven M. Gallo <smgallo@buffalo.edu>
+ */
+namespace OpenXdmod\Migration\Version750To751;
+
+/**
+ * Nothing to do for 7.5.0 -> 7.5.1
+ */
+class DatabasesMigration extends \OpenXdmod\Migration\DatabasesMigration
+{
+}
