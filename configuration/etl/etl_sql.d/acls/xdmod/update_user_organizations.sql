c26b150978f3f06aed87aa8f54d7c05b87929fbe
Ryan Rathsam <ryanrath@buffalo.edu>
2018-07-30 11:16:32 -0400

    Simplified the user organization update script
    Just simplifying the where clause

--- configuration/etl/etl_sql.d/acls/xdmod/update_user_organizations.sql
+++ configuration/etl/etl_sql.d/acls/xdmod/update_user_organizations.sql
@@ -6,5 +6,4 @@
 UPDATE moddb.Users u
   JOIN modw.person p ON p.id = u.person_id
 SET u.organization_id = p.organization_id
-WHERE (u.organization_id IS NULL OR u.organization_id = 0 OR u.organization_id = -1) AND
-      (u.person_id IS NOT NULL AND u.person_id != 0 AND u.person_id != -1 );
+WHERE p.id IS NOT NULL;
