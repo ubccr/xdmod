8b588693032083aeb1f8dce69654398abd3e8f51
Steve Gallo <smgallo@buffalo.edu>
2018-08-03 11:42:06 -0400

    Display all available resource types plus descriptions

--- classes/OpenXdmod/Setup/AddResourceSetup.php
+++ classes/OpenXdmod/Setup/AddResourceSetup.php
@@ -33,6 +33,26 @@ class AddResourceSetup extends SetupItem
      */
     public function handle()
     {
+        $typeOptions = array();
+        $typeDescriptionText = "";
+        $availableTypes = json_decode(file_get_contents(CONFIG_DIR . '/resource_types.json'), true);
+
+        usort(
+            $availableTypes,
+            function ($a, $b) {
+                return strcmp($a['abbrev'], $b['abbrev']);
+            }
+        );
+
+        foreach ( $availableTypes as $type ) {
+            if ( 'UNK' == $type['abbrev'] ) {
+                continue;
+            }
+            // Note that Console::prompt() expects lowercase values for options
+            $typeOptions[] = strtolower($type['abbrev']);
+            $typeDescriptionText .= sprintf("%-10s - %s", $type['abbrev'], $type['description']) . PHP_EOL;
+        }
+
         $this->console->displaySectionHeader('Add A New Resource');
 
         $this->console->displayMessage(<<<"EOT"
@@ -40,13 +60,16 @@ The resource name you enter should match the name used by your resource
 manager.  This is the resource name that you will need to specify during
 the shredding process.  If you are using Slurm this must match the
 cluster name used by Slurm.
+
+Available resource types are:
+$typeDescriptionText
 EOT
         );
         $this->console->displayBlankLine();
 
         $resource = $this->console->prompt('Resource Name:');
         $name     = $this->console->prompt('Formal Name:');
-        $type     = $this->console->prompt('Resource Type:', 'hpc', array('cloud', 'hpc'));
+        $type     = $this->console->prompt('Resource Type:', 'hpc', $typeOptions);
 
         $this->console->displayBlankLine();
         $this->console->displayMessage(<<<"EOT"
