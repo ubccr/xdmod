5a6835876423d302b2ef4bbe1856d8e83ecbfd51
Ryan Rathsam <ryanrath@buffalo.edu>
2018-08-06 13:49:04 -0400

    Style fixes

--- html/controllers/user_admin/enum_institutions.php
+++ html/controllers/user_admin/enum_institutions.php
@@ -1,31 +1,28 @@
 <?php
-	
-	// Operation: user_admin->enum_institutions
-	$xda = new XDAdmin();
-			
-   $name_filter = (isset($_POST['query'])) ? $_POST['query'] : NULL;
 
-	list($institutionCount, $institutions) = $xda->enumerateInstitutions($name_filter);
-			
-   $institutionEntries = array();
-   
-	foreach($institutions as $institution) {
+// Operation: user_admin->enum_institutions
 
-		$institutionEntries[] = array(
-		                    'id' => $institution['id'], 
-		                    'name' => $institution['name']
-		                 );
-		           
-	}//foreach
+$xda = new XDAdmin();
 
-	// -----------------------------
+$name_filter = (isset($_POST['query'])) ? $_POST['query'] : NULL;
 
-	$returnData['status'] = 'success';
-	$returnData['success'] = true;
-	$returnData['total_institution_count'] = $institutionCount;
-	
-	$returnData['institutions'] = $institutionEntries;
-			
-	\xd_controller\returnJSON($returnData);
-			
-?>
+list($institutionCount, $institutions) = $xda->enumerateInstitutions($name_filter);
+
+$institutionEntries = array();
+
+foreach ($institutions as $institution) {
+
+    $institutionEntries[] = array(
+        'id' => $institution['id'],
+        'name' => $institution['name']
+    );
+
+} // foreach
+
+$returnData['status'] = 'success';
+$returnData['success'] = true;
+$returnData['total_institution_count'] = $institutionCount;
+
+$returnData['institutions'] = $institutionEntries;
+
+\xd_controller\returnJSON($returnData);
