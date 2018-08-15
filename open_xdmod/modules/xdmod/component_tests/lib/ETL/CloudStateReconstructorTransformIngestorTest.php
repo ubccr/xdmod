b4c287128296fa29dcfadd21cd3872632bd73ae7
Rudra Chakraborty <chakraborty.rudra.03@gmail.com>
2018-08-15 17:33:01 -0400

    Specify Custom End Dates + Union Bugfixes (#634)
    <!--- Provide a general summary of your changes in the Title above -->
    
    This is #602 over again, wanted to make sure everything worked correctly so I didn't spam you guys with commits.
    
    ## Description
    When ingesting a discrete set of cloud data, we may run into the issue of having a start date for a VM but no clearly defined end. To remediate this issue, we enable specification of a custom default end date as a string in the etl process.
    
    To use, specify the -d option with etl overseer. Some example commands:
    
    `etl_overseer -r RESOURCE -e 'now - 7 days' -p cloud-state-pipeline`
    `etl_overseer -r RESOURCE -e '2018-02-26 + 1 second' -p cloud-state-pipeline`
    `etl_overseer -r RESOURCE -e '+ 14 days' -p cloud-state-pipeline`
    
    ## Motivation and Context
    See above.
    
    ## Tests performed
    Manual verification of data consistency, added component test to validate event reconstruction.
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    - [X] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
    - [ ] My code follows the code style of this project as found in the **CONTRIBUTING** document.
    - [X] I have added tests to cover my changes.
    - [X] All new and existing tests passed.

---
+++ open_xdmod/modules/xdmod/component_tests/lib/ETL/CloudStateReconstructorTransformIngestorTest.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * @package OpenXdmod\ComponentTests
+ * @author Rudra Chakraborty <rudracha@buffalo.edu>
+ */
+
+namespace ComponentTests\ETL;
+
+use ETL\Ingestor\CloudStateReconstructorTransformIngestor;
+use ETL\Ingestor\IngestorOptions;
+use ETL\Configuration\EtlConfiguration;
+
+/**
+ * Test Cloud State FSM
+ */
+
+class CloudStateReconstructorTransformIngestorTest extends \PHPUnit_Framework_TestCase
+{
+    private $valid_event = array(
+        "instance_id" => 2343,
+        "event_time_utc" => "2018-02-06 17:09:01",
+        "event_type_id" => 2,
+        "start_event_id" => -1,
+        "end_time" => -1,
+        "end_event_id" => -1
+    );
+
+    private $valid_end_event = array(
+        "instance_id" => 2343,
+        "event_time_utc" => "2018-02-07 17:09:01",
+        "event_type_id" => 4,
+        "start_event_id" => -1,
+        "end_time" => -1,
+        "end_event_id" => -1
+    );
+
+    private $valid_transform = array(
+        "instance_id" => 2343,
+        "start_time" => "2018-02-06 17:09:01",
+        "start_event_id" => 2,
+        "end_time" => "2018-02-07 17:09:01",
+        "end_event_id" => 4
+    );
+
+    private $invalid_event = array(
+        "instance_id" => -1,
+        "event_time_utc" => "2018-02-06 17:09:01",
+        "event_type_id" => 29,
+        "start_event_id" => -1,
+        "end_time" => -1,
+        "end_event_id" => -1
+    );
+
+    private $zero_event = array(
+        "instance_id" => 0,
+        "event_time_utc" => 0,
+        "event_type_id" => 0,
+        "start_event_id" => 0,
+        "end_time" => 0,
+        "end_event_id" => 0
+    );
+
+    private $options_array = array(
+        "name" => "cloud-state-action",
+        "class" => "CloudStateReconstructorTransformIngestor",
+        "destination" => "modw_test",
+        "definition_file" => ""
+    );
+
+    private $fsm;
+
+    public function __construct()
+    {
+        $configFile = realpath(BASE_DIR . '/tests/artifacts/xdmod-test-artifacts/xdmod/etlv2/configuration/input/xdmod_etl_config_8.0.0.json');
+
+        // This needs to be explicitly defined here so PHP 5.4 doesn't complain
+        $this->options_array["definition_file"] = BASE_DIR . "/tests/artifacts/xdmod-test-artifacts/xdmod/etlv2/configuration/input/etl_action_defs_8.0.0.d/cloud_state.json";
+        
+        $options = new IngestorOptions($this->options_array);
+        $conf = new ETLConfiguration($configFile);
+
+        $this->fsm = new CloudStateReconstructorTransformIngestor($options, $conf);
+    }
+
+    public function testValidTransformation()
+    {
+        $this->fsm->transformHelper($this->valid_event);
+        $event = $this->fsm->transformHelper($this->valid_end_event);
+        
+        $this->assertEquals($this->valid_transform, $event[0]);
+    }
+
+    public function testInvalidTransformation()
+    {
+        $this->assertEquals(array(), $this->fsm->transformHelper($this->invalid_event));
+    }
+
+    public function testZeroTransformation()
+    {
+        $this->assertEquals(array(), $this->fsm->transformHelper($this->zero_event));
+    }
+}
