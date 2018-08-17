33e1c574901fc1d02ad7ce1a61a47fdbe4fa3427
ryanrath <ryanrath@buffalo.edu>
2018-08-17 11:19:39 -0400

    Remove chart / report active role usage (#642)
    * Removing enum[Charts|Reports]UnderOtherRoles
    
    For the following controller operations:
      - `report_builder/enum_available_charts.php`
      - `report_builder/enum_reports.php`
    
    The following values are not used:
      - `has_charts_in_other_roles`
      - `charts_in_other_roles`
      - `has_reports_in_other_roles`
      - `reports_in_other_roles`
    
    This consequently means that the following functions are not used:
      - `XDChartPool::enumChartsUnderOtherRoles`
      - `XDReportManager::enumReportsUnderOtherRoles`
    
    Which means we no longer need to populate / reference the `active_role` column
    in:
      - `moddb.ChartPool`
      - `moddb.Reports`
    
    And that the report_builder test output needs to be updated for:
      - [user]_enum_available_charts
      - [user]_enum_reports
    
    All of this also saves us from having to update / migrate these usages of
    `XDUser::getActiveRole` to an acl equivalent.
    
    * Argument order update
    
    Just swapped the `$actual` / `$expected` arguments so that it's easier to make
    sense of the output when the assertion fails.

--- html/controllers/report_builder/enum_reports.php
+++ html/controllers/report_builder/enum_reports.php
@@ -7,12 +7,7 @@
    	$rm = new XDReportManager($user);
    
    	$returnData['status'] = 'success';
-   	
-   	$reports_in_other_roles = $rm->enumReportsUnderOtherRoles();
-   	
-   	$returnData['has_reports_in_other_roles'] = (count($reports_in_other_roles) > 0);
-   	$returnData['reports_in_other_roles'] = $reports_in_other_roles;
-   	
+
    	$returnData['queue'] = $rm->fetchReportTable();
    
    	\xd_controller\returnJSON($returnData);
