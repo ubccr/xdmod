87dcbc0df4c4a657583725b09acfb0d0d240df69
Ryan Rathsam <ryanrath@buffalo.edu>
2018-12-06 11:52:07 -0500

    Initial code coverage infrastructure
    These three files provide the basis for generating and reporting on code
    coverage data generated by XDebug within XDMoD standard docker containers. The
    script that controls the installation / setting up of things is
    `codeCoverageSetup.sh`. You can customize, via command line arguments, where
    this script can find the required support scripts ( start_xdebug.php &
    combine_xdebug.php ) as well as where they should be placed ( by default in the
    root directory of a standard XDMoD RPM install, /usr/share/xdmod ). You can also
    specify where the generated coverage data and resultant report(s) should be
    stored.
    
    **Pre-Reqs:**
      - A Docker container with:
          - XDMoD installed successfully ( RPM is preferred, but if installed via
          source you'll just need to provide additional information to the setup
          script ).
          - The XDMoD source code checked out ( which will contain these files )
    
    **Setup:**
      - Execute `codeCoverageSetup.sh`. This will install XDebug and set things up
      based on the command line arguments you provide.
        - `-b | --base_dir`: The directory that contains XDMoD source code.
        Default: `/root/src/github.com/ubccr/xdmod`
        - `-i | --install_dir`: The directory that XDMoD is installed in.
        Default: `/usr/share/xdmod`
        - `-c | --coverage_dir`: The directory that the raw coverage data files
        should be saved to. Default: `/usr/share/xdmod/coverage_data`
        - `-r | --report_dir`: The directory that the generated coverage report
        should be saved to. Default: `/usr/share/xdmod/coverage_reports`
        - `-x | --xdebug_script`: The script responsible for enabling XDebug code
        coverage. By default this file is pre-pended whenever PHP executes a file.
        Default: `/root/src/github.com/ubccr/xdmod/tools/dev/start_xdebug.php`
        - `-p | --process_script`: The script responsible for processing the raw
        xdebug data and turning it into a report ( HTML by default ).
        Default: `/root/src/github.com/ubccr/xdmod/tools/dev/combine_xdebug.php`
      - The defaults are setup to support a "standard" XDMoD RPM install in docker.
    
    **Notes:**
      - The ability to group the code coverage data files by an identifier is
      supported by either setting an environment variable `test_name` or including a
      `test_name` cookie with a web request. Additional development will need to be
      done to take this into account when generating the final reports.

---
+++ tools/dev/combine_xdebug.php
@@ -0,0 +1,37 @@
+<?php
+
+#increase the memory limit
+ini_set('memory_limit', -1);
+
+$baseDir = '__BASE_DIR__';
+$coverageDir = '__CODE_COVERAGE_DIR__';
+$installDir = '__INSTALL_DIR__';
+$reportDir = '__REPORT_DIR__';
+
+include_once("$baseDir/vendor/autoload.php");
+
+$final_coverage = new PHP_CodeCoverage();
+$final_coverage->filter()->addDirectoryToWhitelist("$installDir/classes");
+$final_coverage->filter()->addDirectoryToWhitelist("$installDir/html/controllers");
+$final_coverage->filter()->addDirectoryToWhitelist("$installDir/html/internal_dashboard");
+$final_coverage->filter()->addDirectoryToWhitelist("$installDir/xdmod/libraries");
+
+$coverages = glob("$coverageDir/*.json");
+$count = count($coverages);
+$i = 0;
+
+foreach ($coverages as $coverage_file) {
+    $i++;
+    echo "Processing coverage ($i/$count) from $coverage_file". PHP_EOL;
+    $matches = array();
+    $codecoverageData = json_decode(file_get_contents($coverage_file), JSON_OBJECT_AS_ARRAY);
+    if ($codecoverageData !== null) {
+        $test_name = str_ireplace(basename($coverage_file,".json"),"coverage-", "");
+        $final_coverage->append($codecoverageData, $test_name);
+    }
+}
+
+echo "Generating final report..." . PHP_EOL;
+$report = new PHP_CodeCoverage_Report_XML();
+$report->process($final_coverage,$reportDir);
+echo "Report generated succesfully". PHP_EOL;
