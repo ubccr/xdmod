ba0e6848703db59fd3714db03dcbab2b3a9559c0
Joseph P. White <jpwhite4@buffalo.edu>
2018-12-12 16:50:05 -0500

    Add boilerplate migration code.
    There exist database migrations in the supremm module that need
    this boilerplate to exist.

---
+++ classes/OpenXdmod/Migration/Version800To810/DatabasesMigration.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OpenXdmod\Migration\Version800To810;
+
+/**
+* Migrate databases from version 8.0.0 to 8.1.0.
+*/
+class DatabasesMigration extends \OpenXdmod\Migration\DatabasesMigration
+{
+}
