30a5391453f9e06150f28323138d7606d2695c2c
Greg Dean <gmdean@buffalo.edu>
2018-12-14 14:28:42 -0500

    removing minmaxdate statement from bootstrap.sh

--- open_xdmod/modules/xdmod/integration_tests/scripts/bootstrap.sh
+++ open_xdmod/modules/xdmod/integration_tests/scripts/bootstrap.sh
@@ -37,8 +37,6 @@ then
     # This will ensure that the users created in `/root/bin/createusers.php`
     # have their organizations set correctly.
     sudo -u xdmod php /usr/share/xdmod/tools/etl/etl_overseer.php -p xdmod.acls-import
-    #Updating minmaxdate table so data for cloud realm shows up
-    mysql -e "UPDATE modw.minmaxdate SET max_job_date = '2018-07-01';"
 fi
 
 if [ "$XDMOD_TEST_MODE" = "upgrade" ];
