10ce901d1bdb27b214b6393158f1012ec1aa95a4
ryanrath <ryanrath@buffalo.edu>
2018-12-21 10:22:58 -0500

    Move store migration code from rest endpoint to migration class.
    The store migration code currently runs every time the store
    data is requested. (There is a flag set to only perform the migration
    once per user). This means that the migration only happens when
    a user logs in and ther has to be extra code in the summary tab to handle
    the case where a user has not yet been migrated.

--- classes/OpenXdmod/Migration/Version800To810/DatabasesMigration.php
+++ classes/OpenXdmod/Migration/Version800To810/DatabasesMigration.php
@@ -12,6 +12,35 @@ use OpenXdmod\Setup\Console;
 class DatabasesMigration extends \OpenXdmod\Migration\DatabasesMigration
 {
     /**
+     * The identifier that is used to store 'queries' in the user profile.
+     *
+     * @var string
+     */
+    const _QUERIES_STORE = 'queries_store';
+
+    /**
+     * The identifier that was used to store 'queries' in the user profile.
+     * This will be used to find old values to migrate to the new storage
+     * methodology.
+     *
+     * @var string
+     */
+    const _OLD_QUERIES_STORE = 'queries';
+
+    /**
+     * The identifier that is used to store metadata about the queries.
+     *
+     * @var string
+     */
+    const _QUERY_METADATA = 'query_metadata';
+
+    /**
+     *
+     * @var string
+     */
+    const _QUERIES_MIGRATED = 'queries_migrated';
+
+    /**
      * @see \OpenXdmod\Migration\Migration::execute
      **/
     public function execute()
@@ -31,5 +60,114 @@ your cloud data using the commands recommended in our documentation.
 EOT
             );
         }
+    }
+
+    /**
+     * migrate the chart store query data for the provided user.
+     * @param XDUser $user
+     */
+    private function migrateMetricExplorerQueries(XDUser $user)
+    {
+        $metaData = new \UserStorage($user, self::_QUERY_METADATA);
+        $migrated = $this->isMetaDataValid($metaData);
+
+        if (!$migrated) {
+            $this->migrateOldQueries($user, $queries);
+            $metaData->upsert(0, array(self::_QUERIES_MIGRATED => true));
+        }
+    }
+
+    /**
+     * A helper function that is used to migrate / clean up users old query
+     * stores to the new ones.
+     *
+     * @param \XDUser $user
+     * @param \UserStorage $queries
+     * @param bool|true $removeOldQueries
+     *
+     * @throws \Exception
+     */
+    private function migrateOldQueries(\XDUser $user, \UserStorage $queries, $removeOldQueries = true)
+    {
+        if (!isset($user)) {
+            return;
+        }
+
+        $profile = $user->getProfile();
+        $oldQueries = $profile->fetchValue(self::_OLD_QUERIES_STORE);
+
+        if (isset($oldQueries) && !is_array($oldQueries)) {
+            $oldQueries = json_decode($oldQueries, true);
+        }
+
+        if (isset($oldQueries['data'])) {
+            $oldQueries = $oldQueries['data'];
+            if (isset($oldQueries) && !is_array($oldQueries)) {
+                $oldQueries = json_decode($oldQueries, true);
+            }
+        }
+
+        if (isset($oldQueries) && count($oldQueries) > 0) {
+            $validQueries = $this->retrieveValidQueries($oldQueries);
+            if (count($validQueries) > 0) {
+                foreach ($validQueries as $query) {
+                    $queries->insert($query);
+                }
+            }
+        }
+    }
+
+    /**
+     * Another helper function that just encapsulates the logic of what it means
+     * to filter valid queries from the old queries.
+     *
+     * @param array $oldQueries the array that will be used as a source for valid
+     *                          queries.
+     *
+     * @return array of queries that contain non-null and non-empty string name
+     *               and config properties.
+     */
+    private function retrieveValidQueries(array $oldQueries)
+    {
+        $results = array();
+        foreach ($oldQueries as $oldQuery) {
+            $hasName = !empty($oldQuery['name']);
+            $hasConfig = !empty($oldQuery['config']);
+            $isValid = $hasName && $hasConfig;
+
+            if ($isValid) {
+                $results[] = $oldQuery;
+            }
+        }
+        return $results;
+    }
+
+    /**
+     * @param \UserStorage $metaData
+     * @return bool
+     */
+    public function isMetaDataValid(\UserStorage $metaData)
+    {
+
+        $meta = $this->toArray($metaData->get());
+
+        if (count($meta) < 1 || (count($meta) > 1 && !isset($meta[0][self::_QUERIES_MIGRATED]))) {
+            $meta[self::_QUERIES_MIGRATED] = false;
+            $metaData->upsert(0, $meta);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     *
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    private function toArray($value)
+    {
+        return is_string($value) ? json_decode($value) : $value;
     }
 }
