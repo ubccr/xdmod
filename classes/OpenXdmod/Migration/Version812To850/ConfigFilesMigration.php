12394e407c052c7283dab6746790e6266d0618ec
Joseph P. White <jpwhite4@buffalo.edu>
2019-01-15 11:43:56 -0500

    Initial Prototype of Novice User Portal

--- classes/OpenXdmod/Migration/Version812To850/ConfigFilesMigration.php
+++ classes/OpenXdmod/Migration/Version812To850/ConfigFilesMigration.php
@@ -1,20 +1,39 @@
 <?php
+
+namespace OpenXdmod\Migration\Version810To850;
+
+use OpenXdmod\Setup\Console;
+
 /**
- * Update config files from version 8.1.2 To 8.5.0.
+ * Update config files from version 8.1.0 To 8.5.0
  */
-
-namespace OpenXdmod\Migration\Version812To850;
-
-use OpenXdmod\Migration\ConfigFilesMigration as AbstractConfigFilesMigration;
-
-class ConfigFilesMigration extends AbstractConfigFilesMigration
+class ConfigFilesMigration extends \OpenXdmod\Migration\ConfigFilesMigration
 {
     /**
-     * Update portal_settings.ini with the new version number.
+     * Execute the migration.
      */
     public function execute()
     {
         $this->assertPortalSettingsIsWritable();
-        $this->writePortalSettingsFile();
+
+        $console = Console::factory();
+
+        $console->displayMessage(<<<"EOT"
+This release of XDMoD features an optional replacement for the summary
+tab that is intended to provide easier access to XDMoD's many features
+for new or inexperienced (novice) users. Detailed information is available
+as https://open.xdmod.org/novice_user.html
+EOT
+        );
+        $console->displayBlankLine();
+        $novice_user = $console->prompt(
+            'Enable Novice User Tab?',
+            'off',
+            array('on', 'off')
+        );
+
+        $this->writePortalSettingsFile(array(
+            'features_novice_user' => $novice_user
+        ));
     }
 }
