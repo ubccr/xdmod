ab2446fdbb6463d72cabc9bec5dd4fbc31bbe643
Mohammad K. Zia <mkzia@buffalo.edu>
2019-03-12 18:19:01 -0400

    Add Charts and Reports Portlet

--- classes/Rest/Controllers/SummaryControllerProvider.php
+++ classes/Rest/Controllers/SummaryControllerProvider.php
@@ -63,8 +63,10 @@ class SummaryControllerProvider extends BaseControllerProvider
         $layout = $this->getLayout($user);
 
         $presetPortlets = array();
+        $rolesConfig = \Configuration\XdmodConfiguration::assocArrayFactory('roles.json', CONFIG_DIR);
+
         try {
-            $presetPortlets = Roles::getConfig($mostPrivilegedAcl->getName(), 'summary_portlets');
+            $presetPortlets = $rolesConfig['roles'][$mostPrivilegedAcl->getName()]['summary_portlets'];
         } catch (\Exception $e){
         }
 
@@ -85,7 +87,7 @@ class SummaryControllerProvider extends BaseControllerProvider
             );
         }
 
-        $presetCharts = Roles::getConfig($mostPrivilegedAcl->getName(), 'summary_charts');
+        $presetCharts = $rolesConfig['roles']['default']['summary_charts'];
 
         foreach ($presetCharts as $index => $presetChart)
         {
@@ -145,7 +147,7 @@ class SummaryControllerProvider extends BaseControllerProvider
         ));
     }
 
-/**
+    /**
      * Get charts and reports to display in the summary portlet.
      **/
     public function getChartsReports(Request $request, Application $app)
@@ -162,7 +164,7 @@ class SummaryControllerProvider extends BaseControllerProvider
             // fetch reports
             $rm = new \XDReportManager($user);
             $reports = $rm->fetchReportTable();
-            
+
             foreach ($reports as &$report) {
                 $tmp = array();
                 $tmp['type'] = 'Report';
--- html/gui/js/report_builder/ReportsOverview.js
+++ html/gui/js/report_builder/ReportsOverview.js
@@ -233,6 +233,7 @@ XDMoD.ReportsOverview = Ext.extend(Ext.Panel,  {
 
          store: this.reportStore,
          //id: 'reportPool_queueGrid' + Ext.id(),
+         itemId: 'reportQueueGrid',
 
          viewConfig: {
             emptyText: reportsEmptyText,
@@ -253,7 +254,19 @@ XDMoD.ReportsOverview = Ext.extend(Ext.Panel,  {
             {header: 'Schedule', width: 70, dataIndex: 'report_schedule', sortable: true},
             {header: 'Delivery Format', width: 70, dataIndex: 'report_format', sortable: true, renderer: reportFormatColumnRenderer},
             {header: '# Charts', width: 70, dataIndex: 'chart_count', sortable: true, renderer: numChartsColumnRenderer}
-         ]
+         ],
+         listeners: {
+            load_report: function (reportId) {
+               var queueGrid_self = this;
+               this.store.load({
+                  callback: function (records, operation, success) {
+                     var index = queueGrid_self.store.find('report_id', reportId);
+                     queueGrid_self.getSelectionModel().selectRow(index);
+                     editReport();
+                  }
+               });
+            }
+         }
 
       });//queueGrid
 
