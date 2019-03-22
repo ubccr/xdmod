207c389edc5f38a7f3072ebee3210e1ef2214b67
Ben Plessinger <ben@plessinger.us>
2019-03-22 14:48:35 -0400

    allow for timing to be recorded and fix up runtests

--- open_xdmod/modules/xdmod/regression_tests/lib/Controllers/UsageExplorerTest.php
+++ open_xdmod/modules/xdmod/regression_tests/lib/Controllers/UsageExplorerTest.php
@@ -8,6 +8,7 @@ class UsageExplorerTest extends \PHPUnit_Framework_TestCase
     protected static $helper;
     protected static $messages = array();
     protected static $expectedEndpoint;
+    protected static $timingOutputDir;
     /*
      * These are used when testing against two different databases
      * such as when doing federation testing, this allows certain values to be
@@ -56,6 +57,11 @@ class UsageExplorerTest extends \PHPUnit_Framework_TestCase
             $response = self::$helper->post('/controllers/user_interface.php', null, $input);
             $csvdata = $response[0];
             $curldata = $response[1];
+            if(!empty(self::$timingOutputDir)){
+                $time_data = $fullTestName . "," . $curldata['total_time'] . "," . $curldata['starttransfer_time'] . "\n";
+                $outputCSV = self::$timingOutputDir . "timings.csv";
+                file_put_contents($outputCSV, $time_data, FILE_APPEND | LOCK_EX);
+            }
             /*
              * this temporarliy allows the "failed" tests of the public
              * user to pass, need to figure out a more robust way for
@@ -124,6 +130,12 @@ class UsageExplorerTest extends \PHPUnit_Framework_TestCase
     }
     protected function defaultSetup(){
         self::$helper = new \TestHarness\XdmodTestHelper();
+
+        $timingTestDir = getenv('REG_TIME_LOGDIR');
+        if(!empty($timingTestDir)){
+            self::$timingOutputDir = $timingTestDir;
+        }
+
         $envUserrole = getenv('REG_TEST_USER_ROLE');
         if(!empty($envUserrole)){
             self::$helper->authenticate($envUserrole);
