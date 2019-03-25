60244a3e20e244d4b930f8b32ea0482a15db3f61
Ben Plessinger <ben@plessinger.us>
2019-03-25 10:53:15 -0400

    Make travis happy, remove imagine

--- html/about/federated.php
+++ html/about/federated.php
@@ -1,11 +1,16 @@
 <?php
 namespace Xdmod\About;
+
 require_once __DIR__ . '/../../configuration/linker.php';
 use CCR\DB;
 ?>
 <h1>Federated Open XDMoD</h1>
 <p>
-    Imagine a collection of independent XDMoD instances, each monitoring its own set of computing resources. Each instance may comprise very different underlying resources, configurations, and aggregation levels. Reporting on resource consumption on any one of these XDMoD instances could be done easily through the powerful user interface. Reporting on the collection, however, would be difficult without a way to associate the data (and the aggregations) from these different instances. Federation is the answer to reporting on this imagined collection of XDMoD instances. Federation provides a combined, master view of job and performance data collected from individual XDMoD instances.
+    A collection of independent XDMoD instances, each monitoring its own set of computing resources.
+    Each instance may comprise very different underlying resources, configurations, and aggregation levels.
+    Reporting on resource consumption on any one of these XDMoD instances could be done easily through the powerful user interface.
+    Reporting on the collection, however, would be difficult without a way to associate the data (and the aggregations) from these different instances.
+    Federation is the answer to reporting on this collection of XDMoD instances. Federation provides a combined, master view of job and performance data collected from individual XDMoD instances.
 </p>
 <p>
     <div style="text-align:center; width:65%;">
@@ -13,17 +18,24 @@ use CCR\DB;
         <div>
             <fig>
                 <em>
-                    Example data flow from heterogeneous computing resources to an XDMoD federated hub. XDMoD instances X and Y ingest data into their databases from the computing resources that they monitor. Following ingestion on the satellite instances, job data are replicated to the federated hub's database, where they are aggregated for use in the federated XDMoD user interface.
+                    Example data flow from heterogeneous computing resources to an XDMoD federated hub.
+                    XDMoD instances X and Y ingest data into their databases from the computing resources that they monitor.
+                    Following ingestion on the satellite instances, job data are replicated to the federated hub's database, where they are aggregated for use in the federated XDMoD user interface.
                 </em>
             </fig>
         </div>
     </div>
 </p>
 <p>
-    Thus, federation provides resource managers with a unified XDMoD monitor that consolidates the data from a network of disjoint XDMoD instances. Once data is ingested on the individual XDMoD instances, it undergoes live replication to the central federation hub database, where it is then aggregated as appropriate to the requirements of the whole collection (aggregation is customized on each instance using local configuration files). The federation hub can then provide an integrated view of job and performance data collected from entirely independent XDMoD instances.
+    Thus, federation provides resource managers with a unified XDMoD monitor that consolidates the data from a network of disjoint XDMoD instances.
+    Once data is ingested on the individual XDMoD instances, it undergoes live replication to the central federation hub database, where it is then aggregated as appropriate to the requirements of the whole collection (aggregation is customized on each instance using local configuration files).
+    The federation hub can then provide an integrated view of job and performance data collected from entirely independent XDMoD instances.
 </p>
 <p>
-    The independent XDMoD instances in a federation need have no knowledge of one another. They may be closely related, and managed with the intent to fully share all data. Alternatively, their data can be kept isolated from one another, and only made visible to the federation hub. The only requirement is that each individual XDMoD instance must run the same version of XDMoD.
+    The independent XDMoD instances in a federation need have no knowledge of one another.
+    They may be closely related, and managed with the intent to fully share all data.
+    Alternatively, their data can be kept isolated from one another, and only made visible to the federation hub.
+    The only requirement is that each individual XDMoD instance must run the same version of XDMoD.
 </p>
 <p>
     For more information see Section II of <a href="https://ieeexplore.ieee.org/document/8514918">Federating XDMoD to Monitor Affiliated Computing Resources</a>.
@@ -46,7 +58,7 @@ use CCR\DB;
  *
  * @return mixed
  **/
-function getConfigValue($section, $key, $default=null)
+function getConfigValue($section, $key, $default = null)
 {
     try {
         $result = \xd_utilities\getConfiguration($section, $key);
@@ -62,7 +74,7 @@ if($role === 'instance'){
     echo '<h2>This instance is part of a federation</h2>';
     echo 'Federation Hub: <a href="' . $hubUrl .'">' . $hubUrl .'</a>';
 }
-else if ($role === 'hub'){
+elseif ($role === 'hub'){
     $db = DB::factory('datawarehouse');
     $instanceResults = $db->query('SELECT * FROM federation_instances;');
     $instances = array();
