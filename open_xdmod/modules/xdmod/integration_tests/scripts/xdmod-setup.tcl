1eb9bb46f297d6b8838c1fcc7273606f876ae85f
Jeffrey T. Palmer <jeffrey.t.palmer@gmail.com>
2019-03-29 13:59:43 -0400

    Add storage realm tests (#880)
    Add storage resources during automated tests for setup and upgrade.
    Ingest storage data during automated tests.  Add regression tests and
    refactor the other usage explorer CSV regression tests.  Update output
    data for related integration tests.  Fix bug in CSV exports that caused
    an error when exporting an empty data set.  Add ordering to realm
    queries to ensure consistent ordering of realms in the usage explorer.

--- open_xdmod/modules/xdmod/integration_tests/scripts/xdmod-setup.tcl
+++ open_xdmod/modules/xdmod/integration_tests/scripts/xdmod-setup.tcl
@@ -6,11 +6,14 @@
 #-------------------------------------------------------------------------------
 # Configuration settings for the XDMoD resources
 set resources [list]
-lappend resources [list frearson Frearson 400 4000]
-lappend resources [list mortorq Mortorq 400 4000]
-lappend resources [list phillips Phillips 400 4000]
-lappend resources [list pozidriv Posidriv 400 4000]
-lappend resources [list robertson Robertson 400 4000]
+lappend resources [list frearson Frearson hpc 400 4000]
+lappend resources [list mortorq Mortorq hpc 400 4000]
+lappend resources [list phillips Phillips hpc 400 4000]
+lappend resources [list pozidriv Posidriv hpc 400 4000]
+lappend resources [list robertson Robertson hpc 400 4000]
+lappend resources [list openstack OpenStack cloud 123 234]
+lappend resources [list recex Recex tape 0 0]
+lappend resources [list torx Torx stgrid 0 0]
 
 # Load helper functions from helper-functions.tcl
 source [file join [file dirname [info script]] helper-functions.tcl]
@@ -55,17 +58,10 @@ foreach resource $resources {
 	selectMenuOption 1
 	provideInput {Resource Name:} [lindex $resource 0]
 	provideInput {Formal Name:} [lindex $resource 1]
-	provideInput {Resource Type*} {}
-	provideInput {How many nodes does this resource have?} [lindex $resource 2]
-	provideInput {How many total processors (cpu cores) does this resource have?} [lindex $resource 3]
+	provideInput {Resource Type*} [lindex $resource 2]
+	provideInput {How many nodes does this resource have?} [lindex $resource 3]
+	provideInput {How many total processors (cpu cores) does this resource have?} [lindex $resource 4]
 }
-
-selectMenuOption 1
-provideInput {Resource Name:} openstack
-provideInput {Formal Name:} OpenStack
-provideInput {Resource Type*} cloud
-provideInput {How many nodes does this resource have?} 123
-provideInput {How many total processors (cpu cores) does this resource have?} 234
 
 selectMenuOption s
 confirmFileWrite yes
--- open_xdmod/modules/xdmod/regression_tests/runtests.sh
+++ open_xdmod/modules/xdmod/regression_tests/runtests.sh
@@ -1,21 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-REGUSER=""
-PI=""
-CD=""
-CS=""
-PUB=""
+junit_output_dir=""
 
 if [ "$1" = "--junit-output-dir" ];
 then
-    REGUSER="--log-junit $2/xdmod-regression-user.xml"
-    PI="--log-junit $2/xdmod-regression-principalinvestigator.xml"
-    CD="--log-junit $2/xdmod-regression-centerdirector.xml"
-    CS="--log-junit $2/xdmod-regression-centerstaff.xml"
-    PUB="--log-junit $2/xdmod-regression-public.xml"
+    junit_output_dir="$2"
 fi
+
+# Output PHPUnit logging options.  First argument is a unique identifier that
+# will be used in the log file name.
+log_opts() {
+    if [ "$junit_output_dir" = "" ]; then
+        return
+    fi
+
+    echo "--log-junit $junit_output_dir/xdmod-regression-$1.xml"
+}
 
 cd $(dirname $0)
 
@@ -33,37 +35,40 @@ if [ ! -x "$phpunit" ]; then
     echo phpunit not found, run \"composer install\" 1>&2
     exit 127
 fi
-if [ "$REG_TEST_ALL" == "1" ]; then
-    set +e
-    $phpunit $PUB lib/Controllers/UsageChartsTest.php
-    $phpunit $PUB lib/Controllers/UsageExplorerTest.php
-    REG_TEST_USER_ROLE=usr $phpunit $REGUSER lib/Controllers/UsageExplorerTest.php
-    REG_TEST_USER_ROLE=pi $phpunit $PI lib/Controllers/UsageExplorerTest.php
-    REG_TEST_USER_ROLE=cd $phpunit $CD lib/Controllers/UsageExplorerTest.php
-    REG_TEST_USER_ROLE=cs $phpunit $CS lib/Controllers/UsageExplorerTest.php
 
-    $phpunit $PUB lib/Controllers/UsageExplorerCloudTest.php
-    REG_TEST_USER_ROLE=usr $phpunit $REGUSER lib/Controllers/UsageExplorerCloudTest.php
-    REG_TEST_USER_ROLE=pi $phpunit $PI lib/Controllers/UsageExplorerCloudTest.php
-    REG_TEST_USER_ROLE=cd $phpunit $CD lib/Controllers/UsageExplorerCloudTest.php
-    REG_TEST_USER_ROLE=cs $phpunit $CS lib/Controllers/UsageExplorerCloudTest.php
+roles=( pub usr pi cd cs )
+
+if [ "$REG_TEST_ALL" = "1" ]; then
+    set +e
+    $phpunit $(log_opts "UsageCharts-pub") --filter UsageChartsTest .
+
+    for role in ${roles[@]}; do
+        opts="$(log_opts "UsageExplorer-${role}") --filter 'UsageExplorer\w+Test'"
+        if [ $role = "pub" ]; then
+            $phpunit $opts .
+        else
+            REG_TEST_USER_ROLE=$role $phpunit $opts .
+        fi
+    done
 else
     pids=()
-    $phpunit $PUB lib/Controllers/UsageChartsTest.php & pids+=("$!")
-    $phpunit $PUB lib/Controllers/UsageExplorerTest.php & pids+=("$!")
-    REG_TEST_USER_ROLE=usr $phpunit $REGUSER lib/Controllers/UsageExplorerTest.php & pids+=("$!")
-    REG_TEST_USER_ROLE=pi $phpunit $PI lib/Controllers/UsageExplorerTest.php & pids+=("$!")
-    REG_TEST_USER_ROLE=cd $phpunit $CD lib/Controllers/UsageExplorerTest.php & pids+=("$!")
-    REG_TEST_USER_ROLE=cs $phpunit $CS lib/Controllers/UsageExplorerTest.php & pids+=("$!")
 
-    REG_TEST_USER_ROLE=usr $phpunit $REGUSER lib/Controllers/UsageExplorerCloudTest.php & pids+=("$!")
-    REG_TEST_USER_ROLE=pi $phpunit $PI lib/Controllers/UsageExplorerCloudTest.php & pids+=("$!")
-    REG_TEST_USER_ROLE=cd $phpunit $CD lib/Controllers/UsageExplorerCloudTest.php & pids+=("$!")
-    REG_TEST_USER_ROLE=cs $phpunit $CS lib/Controllers/UsageExplorerCloudTest.php & pids+=("$!")
-    $phpunit $PUB lib/Controllers/UsageExplorerCloudTest.php & pids+=("$!")
+    $phpunit $(log_opts "UsageCharts-pub") --filter UsageChartsTest . &
+    pids+=($!)
+
+    for role in ${roles[@]}; do
+        opts="$(log_opts "UsageExplorer-${role}") --filter 'UsageExplorer\w+Test'"
+        if [ $role = "pub" ]; then
+            $phpunit $opts . &
+            pids+=($!)
+        else
+            REG_TEST_USER_ROLE=$role $phpunit $opts . &
+            pids+=($!)
+        fi
+    done
 
     EXIT_STATUS=0
-    for pid in "${pids[@]}";
+    for pid in ${pids[@]}
     do
         wait "$pid"
         if [ "$?" -ne "0" ];
