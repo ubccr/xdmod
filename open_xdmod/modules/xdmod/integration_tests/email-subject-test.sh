6d8296878509f47dbe0edf8a1771bd00a1125d28
Benjamin D. Plessinger <ben@plessinger.us>
2019-04-25 22:22:20 -0400

    Initial updates

--- open_xdmod/modules/xdmod/integration_tests/email-subject-test.sh
+++ open_xdmod/modules/xdmod/integration_tests/email-subject-test.sh
@@ -5,6 +5,13 @@ curl "${host}controllers/mailer.php" --compressed -H 'X-Requested-With: XMLHttpR
 
 sed -i -- 's/subject_prefix = "SEND BEN MONEY"/subject_prefix = ""/' /etc/xdmod/portal_settings.ini
 
+# sometimes it takes a second to send the emails
+queueLength=`postqueue -p | tail -n1 | awk '{print $5}'`
+if [ "$queueLength" != ""  ]; then
+    echo "queue has not been emptied, waiting 1 second."
+    sleep 1
+fi
+
 emailsubjects=`grep 'SEND BEN MONEY' /var/mail/root -c`
 
 if [ $emailsubjects != 2 ]; then
