2b6617e4f71ae6b15e054141b25f56357537beea
Jeanette Sperhac <jsperhac@buffalo.edu>
2019-04-29 13:13:50 -0400

    Being modern, we are now binding variables...

--- classes/DataWarehouse/Export/QueryHandler.php
+++ classes/DataWarehouse/Export/QueryHandler.php
@@ -4,14 +4,14 @@
  * Class governing database access by Data Warehouse Export batch script
  *
  * TODO: Notes and choices: evaluate these:
- *  make this class a singleton--does it make sense?
+ *  Perhaps no: make this class a singleton--does it make sense?
  *      or do I need to have multiple instances--and who instantiates them?
  *      say I'm using it for a rest endpoint--what I do need to know?
- *  use the DataWarehouse.php (DataWarehouse class) to issue queries?
- *      No, use CCR\DB itself, which will give us PDODB.
- *      (note that it's looking rather long in the tooth. There may be a more modern way.)
+ *  For database access and to issue queries:
+ *      use CCR\DB namespace, which contains PDODB class that we want.
  *  deciding against e.g. making the state transitions reflected by classes. Too much.
- *  think about binding variables and safe access...
+ *  think about and safe access...
+ *      do I need to do more to secure vars coming in
  *  determine current testing practices and write tests to them
  *      these need to be component tests
  *
@@ -26,114 +26,141 @@ use CCR\DB;
 
 class QueryHandler
 {
-    // Fetch the database handle
-    $pdo = DB::factory('database');
+    private $pdo; // populated in the constructor
 
 
-    // TODO verify: singleton class should be appropriate here
     private function __construct()
     {
+        // Fetch the database handle
+        $pdo = DB::factory('database');
     }
 
     /* transition between request record states */
 
     // create request record for specified export request
-    public static function createRequestRecord($realm, $startDate, $endDate)
+    public function createRequestRecord($userId, $realm, $startDate, $endDate)
     {
-        // TODO
         $sql = "INSERT INTO batch_export_requests
                 (requested_datetime, user_id, realm, start_date, end_date)
                 VALUES
-                ()";
+                (NOW(), :user_id, :realm, :start_date, :end_date)";
 
-        // TODO: return the id
+        $params = array('user_id' => $userId,
+                        'realm' => $realm
+                        'start_date' => $startDate,
+                        'end_date' => $endDate);
+
+        // return the id for the inserted record
+        $id = $this->pdo->insert($sql);
+
+        return($id);
     }
 
     // transition specified export request to Failed state from Submitted.
-    public static function submittedToFailed($id)
+    public function submittedToFailed($id)
     {
         $sql = "UPDATE batch_export_requests
                 SET export_succeeded=0
-                WHERE id=$id";
-        // TODO
+                WHERE id=:id";
 
+        $params = array('id' => $id);
+
+        // Return count of affected rows--should be 1.
+        $result = $this->pdo->execute($sql, $params);
+
+        return($result);
     }
 
     // transition specified export request to Available state from Submitted.
-    public static function submittedToAvailable($id)
+    // All time is current time as relative to database.
+    public function submittedToAvailable($id)
     {
-        // read export retention duration from config file
+        // read export retention duration from config file. Value is stored in days.
         $expires_in_days = xd_utilities\getConfiguration('data_warehouse_export','retention_duration');
-
-        // Nah, don't:
-        // either here, or below...
-        //$sqlDate = "SELECT CAST(NOW() as DATETIME";
-        // select it
-        //$currentDatetime =
 
         $sql = "UPDATE batch_export_requests
                 SET
                     export_created=CAST(NOW() as DATETIME),
-                    export_expires=DATE_ADD(CAST(NOW() as DATETIME), INTERVAL $expires_in_days DAY),
+                    export_expires=DATE_ADD(CAST(NOW() as DATETIME), INTERVAL :expires_in_days DAY),
                     export_succeeded=1
-                WHERE id=$id";
-        // TODO
+                WHERE id=:id";
 
+        $params = array('expires_in_days' => $expires_in_days,
+                        'id' => $id);
+
+        // Return count of affected rows--should be 1.
+        $result = $this->pdo->execute($sql, $params);
+
+        return($result);
     }
 
     // transition specified export request to Expired state from Available.
     // Note that when the table grows large we don't want to do date comparisons...
-    public static function availableToExpired($id)
+    // Therefore we should create an export_expired flag on the db table.
+    public function availableToExpired($id)
     {
         // TODO: create the field in the DB.
-        // no-op
-        // TODO: really?
+
         $sql = "UPDATE batch_export_requests
                 SET export_expired=1
-                WHERE id=$id";
+                WHERE id=:id";
+
+        $params = array('id' => $id);
+
+        // Return count of affected rows--should be 1.
+        $result = $this->pdo->execute($sql, $params);
+
+        return($result);
     }
 
     /* list request records states */
 
     // Return count of export requests presently in Submitted state.
-    public static function countSubmittedRecords()
+    public function countSubmittedRecords()
     {
         $sql = "SELECT COUNT(id)
             FROM batch_export_requests
             WHERE export_succeeded IS NULL";
-        // TODO, you meant the PDODB execute() function here. Decide.
-        $count = \DataWarehouse::connect()->execute($sql);
 
-        return $count;
+        // Return count of rows.
+        $result = $this->pdo->execute($sql);
+
+        return($result);
     }
 
     // Return details of all export requests presently in Submitted state.
-    public static function listSubmittedRecords()
+    public function listSubmittedRecords()
     {
-
         $sql = "SELECT id, realm, start_date, end_date
             FROM batch_export_requests
             WHERE export_succeeded IS NULL";
-        $results = \DataWarehouse::connect()->query($sql);
 
-        return $results;
+        // Return query results.
+        $result = $this->pdo->query($sql);
+
+        // TODO: Logic to return neat table of records and states
+        // (or in another function that handles that part?)
+        return($result);
     }
 
     // Return details of all export requests made by specified user.
-    public static function listRequestsForUser($user_id)
+    public function listRequestsForUser($user_id)
     {
-        // TODO: bind your user_id as appropriate please.
         $sql = "SELECT id, realm, start_date, end_date,
             export_succeeded, expires_datetime,
             export_created_datetime,
             FROM batch_export_requests
-            WHERE user_id=".$user_id.
-            "ORDER BY id";
-        $results = \DataWarehouse::connect()->query($sql);
+            WHERE user_id=:user_id
+            ORDER BY id";
+
+        $params = array('user_id' => $user_id);
+
+        // Return query results.
+        $result = $this->pdo->query($sql, $params);
 
         // TODO: Logic to return neat table of records and states
         // (or in another function that handles that part?)
-        return $results;
+        return $result;
     }
 }
 
