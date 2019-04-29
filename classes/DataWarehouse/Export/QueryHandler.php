00ccd7ea6b74fe3f47ab9aa5e2fa7d6b8d757d8c
Jeanette Sperhac <jsperhac@buffalo.edu>
2019-04-29 10:28:17 -0400

    Current status of class to handle querying for export.

--- classes/DataWarehouse/Export/QueryHandler.php
+++ classes/DataWarehouse/Export/QueryHandler.php
@@ -5,54 +5,92 @@
  *
  * TODO: Notes and choices: evaluate these:
  *  make this class a singleton--does it make sense?
+ *      or do I need to have multiple instances--and who instantiates them?
+ *      say I'm using it for a rest endpoint--what I do need to know?
  *  use the DataWarehouse.php (DataWarehouse class) to issue queries?
- *      note that it's looking rather long in the tooth. There may be a more modern way.
+ *      No, use CCR\DB itself, which will give us PDODB.
+ *      (note that it's looking rather long in the tooth. There may be a more modern way.)
  *  deciding against e.g. making the state transitions reflected by classes. Too much.
  *  think about binding variables and safe access...
- * ==========================================================================================
+ *  determine current testing practices and write tests to them
+ *      these need to be component tests
+ *
+ *  timestamp and current datetime: always use the database's value
+ *  ==========================================================================================
  */
 
 namespace DataWarehouse\Export;
 
 use Exception;
-use DataWarehouse; // TODO: DB access here! or??
+use CCR\DB;
 
 class QueryHandler
 {
+    // Fetch the database handle
+    $pdo = DB::factory('database');
 
-    // singleton class should be appropriate here
+
+    // TODO verify: singleton class should be appropriate here
     private function __construct()
     {
     }
 
     /* transition between request record states */
 
-    public static function createRequestRecord(realm, startDate, endDate)
+    // create request record for specified export request
+    public static function createRequestRecord($realm, $startDate, $endDate)
     {
         // TODO
+        $sql = "INSERT INTO batch_export_requests
+                (requested_datetime, user_id, realm, start_date, end_date)
+                VALUES
+                ()";
+
+        // TODO: return the id
     }
 
     // transition specified export request to Failed state from Submitted.
     public static function submittedToFailed($id)
     {
-        $sql = "UPDATE batch_export_requests SET export_succeeded=0
+        $sql = "UPDATE batch_export_requests
+                SET export_succeeded=0
                 WHERE id=$id";
         // TODO
 
     }
 
     // transition specified export request to Available state from Submitted.
-    public static function submittedToAvailable()
+    public static function submittedToAvailable($id)
     {
+        // read export retention duration from config file
+        $expires_in_days = xd_utilities\getConfiguration('data_warehouse_export','retention_duration');
+
+        // Nah, don't:
+        // either here, or below...
+        //$sqlDate = "SELECT CAST(NOW() as DATETIME";
+        // select it
+        //$currentDatetime =
+
+        $sql = "UPDATE batch_export_requests
+                SET
+                    export_created=CAST(NOW() as DATETIME),
+                    export_expires=DATE_ADD(CAST(NOW() as DATETIME), INTERVAL $expires_in_days DAY),
+                    export_succeeded=1
+                WHERE id=$id";
         // TODO
 
     }
 
     // transition specified export request to Expired state from Available.
-    public static function availableToExpired()
+    // Note that when the table grows large we don't want to do date comparisons...
+    public static function availableToExpired($id)
     {
+        // TODO: create the field in the DB.
         // no-op
         // TODO: really?
+        $sql = "UPDATE batch_export_requests
+                SET export_expired=1
+                WHERE id=$id";
     }
 
     /* list request records states */
