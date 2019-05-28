0df25f5a09e7a66660bfe31f3d28798c7cd3fd7e
Jeanette Sperhac <jsperhac@buffalo.edu>
2019-05-28 16:26:36 -0400

    Current working state, added state requirements to transitions, and added state to user request list report.

--- classes/DataWarehouse/Export/QueryHandler.php
+++ classes/DataWarehouse/Export/QueryHandler.php
@@ -18,6 +18,25 @@
  *  TODO, possibly: return list of ids for records that need to be marked 'export_expired'
  *
  *  timestamp and current datetime: always use the database's value
+ *
+ *  Recognized states enforced in this class:
+ *
+ *  State       export_succeeded    export_created_datetime     export_expired
+ *  -----       ----------------    -----------------------     --------------
+ *  Submitted   NULL                NULL                        FALSE
+ *  Available   TRUE                NOT NULL                    FALSE
+ *  Expired     TRUE                NOT NULL                    TRUE
+ *  Failed      FALSE               NULL                        FALSE
+ *  (Deleted)       not   present    in    database   or    filesystem
+ *
+ *  State transitions enforced in this class:
+ *
+ *    Submitted -> Available -> Expired
+ *      |
+ *      v
+ *    Failed
+ *
+ *    ...any state can transition to Deleted.
  *  ==========================================================================================
  */
 
@@ -30,6 +49,9 @@ class QueryHandler
 {
     private $pdo; // populated in the constructor
 
+    // Definition of Submitted state:
+    private $whereSubmitted = "WHERE export_succeeded is NULL and export_created_datetime is NULL and export_expired = FALSE ";
+
 
     function __construct()
     {
@@ -39,18 +61,21 @@ class QueryHandler
 
     /* transition between request record states */
 
-    // create request record for specified export request
-    public function createRequestRecord($userId, $realm, $startDate, $endDate)
+    // Create request record for specified export request
+    // Result is a single request in Submitted state.
+    public function createRequestRecord($userId, $realm, $startDate, $endDate, $format)
     {
         $sql = "INSERT INTO batch_export_requests
-                (requested_datetime, user_id, realm, start_date, end_date)
+                (requested_datetime, user_id, realm, start_date, end_date, export_file_format)
                 VALUES
-                (NOW(), :user_id, :realm, :start_date, :end_date)";
+                (NOW(), :user_id, :realm, :start_date, :end_date, :export_file_format)";
 
         $params = array('user_id' => $userId,
                         'realm' => $realm,
                         'start_date' => $startDate,
-                        'end_date' => $endDate);
+                        'end_date' => $endDate,
+                        'export_file_format' => $format
+                    );
 
         // return the id for the inserted record
         $id = $this->pdo->insert($sql, $params);
@@ -58,22 +83,24 @@ class QueryHandler
         return($id);
     }
 
-    // transition specified export request to Failed state from Submitted.
+    // Transition specified export request from Submitted state to Failed state.
     public function submittedToFailed($id)
     {
         $sql = "UPDATE batch_export_requests
-                SET export_succeeded=0
-                WHERE id=:id";
+                SET export_succeeded=0 " .
+                $this->whereSubmitted .
+                "AND id=:id";
 
         $params = array('id' => $id);
 
         // Return count of affected rows--should be 1.
         $result = $this->pdo->execute($sql, $params);
 
-        return(count($result)==1);
+        //return(count($result)==1);
+        return($result);
     }
 
-    // transition specified export request to Available state from Submitted.
+    // transition specified export request from Submitted state to Available state.
     // All time is current time as relative to database.
     public function submittedToAvailable($id)
     {
@@ -81,11 +108,10 @@ class QueryHandler
         $expires_in_days = \xd_utilities\getConfiguration('data_warehouse_export','retention_duration');
 
         $sql = "UPDATE batch_export_requests
-                SET
-                    export_created_datetime=CAST(NOW() as DATETIME),
+                SET export_created_datetime=CAST(NOW() as DATETIME),
                     export_expires_datetime=DATE_ADD(CAST(NOW() as DATETIME), INTERVAL :expires_in_days DAY),
-                    export_succeeded=1
-                WHERE id=:id";
+                    export_succeeded=1 " .
+                $this->whereSubmitted . "AND id=:id";
 
         $params = array('expires_in_days' => $expires_in_days,
                         'id' => $id);
@@ -96,14 +122,14 @@ class QueryHandler
         return(count($result)==1);
     }
 
-    // transition specified export request to Expired state from Available.
-    // Note that when the table grows large we don't want to do date comparisons...
-    // Therefore we should create an export_expired flag on the db table.
+    // Transition specified export request from Available state to Expired state.
     public function availableToExpired($id)
     {
         $sql = "UPDATE batch_export_requests
                 SET export_expired=1
-                WHERE id=:id";
+                WHERE id=:id AND
+                export_succeeded = TRUE AND export_created_datetime IS NOT NULL
+                AND export_expired = FALSE";
 
         $params = array('id' => $id);
 
@@ -118,9 +144,7 @@ class QueryHandler
     // Return count of export requests presently in Submitted state.
     public function countSubmittedRecords()
     {
-        $sql = "SELECT COUNT(id)
-            FROM batch_export_requests
-            WHERE export_succeeded IS NULL";
+        $sql = "SELECT COUNT(id) FROM batch_export_requests " . $this->whereSubmitted;
 
         // Return count of rows.
         try {
@@ -135,16 +159,12 @@ class QueryHandler
     // Return details of all export requests presently in Submitted state.
     public function listSubmittedRecords()
     {
-        $sql = "SELECT id, realm, start_date, end_date,
-            export_file_format, requested_datetime
-            FROM batch_export_requests
-            WHERE export_succeeded IS NULL";
+        $sql = "SELECT id, realm, start_date, end_date, export_file_format, requested_datetime
+            FROM batch_export_requests " . $this->whereSubmitted;
 
         // Return query results.
         $result = $this->pdo->query($sql);
 
-        // TODO: Logic to return neat table of records and states
-        // (or in another function that handles that part?)
         return($result);
     }
 
@@ -170,9 +190,45 @@ class QueryHandler
         // Return query results.
         $result = $this->pdo->query($sql, $params);
 
-        // TODO: Logic to return neat table of records and states
-        // (or in another function that handles that part?)
         return $result;
+    }
+
+    // Return details (including state) of all export requests made by specified user.
+    public function listUserRequestsByState($user_id)
+    {
+        $attributes = "SELECT id, realm, start_date, end_date, export_succeeded, export_expired, export_expires_datetime, export_created_datetime, export_file_format, requested_datetime, ";
+        $fromTable = "FROM batch_export_requests ";
+        //$whereSubmitted = "WHERE export_succeeded is NULL and export_created_datetime is NULL and export_expired = FALSE ";
+        $whereAvailable = "WHERE export_succeeded = TRUE and export_created_datetime is NOT NULL and export_expired = FALSE ";
+        $whereExpired = "WHERE export_succeeded = TRUE and export_created_datetime is NOT NULL and export_expired = TRUE ";
+        $whereFailed = "WHERE export_succeeded = FALSE and export_created_datetime is NULL and export_expired = FALSE ";
+        $userClause = "AND user_id = :user_id ";
+
+        $sql =  $attributes . "'Submitted' as state " . $fromTable . $this->whereSubmitted . $userClause . "UNION " .
+                $attributes . "'Available' as state " . $fromTable . $whereAvailable . $userClause . "UNION " .
+                $attributes . "'Expired' as state "   . $fromTable . $whereExpired . $userClause . "UNION " .
+                $attributes . "'Failed' as state "    . $fromTable . $whereFailed . $userClause . "ORDER BY requested_datetime";
+
+        $params = array('user_id' => $user_id);
+
+        // Return query results.
+        $result = $this->pdo->query($sql, $params);
+        return $result;
+    }
+
+    // Delete specified record, regardless of its state.
+    // Only the user who submittedthe request may delete it.
+    public function deleteRequest($id, $user)
+    {
+        // delete record, providing that requesting user owns specified record
+        $sql = "DELETE FROM batch_export_requests WHERE id=:request_id AND user_id=:user_id";
+        $params = array('request_id' => $id,
+                        'user_id' => $user
+                    );
+
+        // Return count of deleted rows--should be 1 if successful.
+        $result = $this->pdo->execute($sql, $params);
+        return($result);
     }
 }
 
--- tests/component/lib/Export/ExportDBTest.php
+++ tests/component/lib/Export/ExportDBTest.php
@@ -15,7 +15,7 @@ class ExportDBTest extends BaseTest
 {
     static $dbh = null;
     static $maxId = null;
-    private static $userName = self::NORMAL_USER_USER_NAME;
+    //private static $userName = self::NORMAL_USER_USER_NAME;
     private static $debug = TRUE;
 
     public function testCountSubmitted()
@@ -29,14 +29,24 @@ class ExportDBTest extends BaseTest
         if (self::$debug) print("\n".__FUNCTION__.": submittedRecords=$submittedCount\n");
     }
 
+    // Specify a user that actually exists in moddb.Users table.
     private function acquireUserId()
     {
         $userId = static::$dbh->query('SELECT MIN(id) AS id FROM Users')[0]['id'];
         if ($userId == NULL) {
-            $userId = XDUser::getUserByUserName(self::$userName);
+            // TODO throw an exception, we have no users in the db
         }
         return $userId;
     }
+
+    /*
+    // Specify a different user that actually exists in moddb.Users table.
+    private function acquireMaxUserId()
+    {
+        return static::$dbh->query('SELECT MAX(id) AS id FROM Users')[0]['id'];
+    }
+     */
+
 
     private function findSubmittedRecord()
     {
@@ -46,12 +56,27 @@ class ExportDBTest extends BaseTest
         // Find or create a record in submitted status to transition
         $maxSubmitted = static::$dbh->query('SELECT MAX(id) AS id FROM batch_export_requests where export_succeeded IS NULL')[0]['id'];
         if ($maxSubmitted == NULL) {
-            $maxSubmitted = $query->createRequestRecord($userId, 'Jobs', '2017-01-01','2017-08-01');
+            $maxSubmitted = $query->createRequestRecord($userId, 'Jobs', '2017-01-01','2017-08-01','CSV');
         }
 
         if (self::$debug) print("\n".__FUNCTION__.": maxSubmitted ID=$maxSubmitted\n");
 
         return($maxSubmitted);
+    }
+
+    private function findAvailableRecord()
+    {
+        $query = new QueryHandler();
+
+        // Find or create a record in available status to transition
+        $maxAvailable = static::$dbh->query('SELECT MAX(id) AS id FROM batch_export_requests where
+                                            export_succeeded IS TRUE
+                                            AND export_expired IS FALSE')[0]['id'];
+        if ($maxAvailable == NULL) {
+            $maxSubmitted = $this->findSubmittedRecord();
+            $maxAvailable = $query->submittedToAvailable($maxSubmitted);
+        }
+        return($maxAvailable);
     }
 
     // Create two new records to enable testing of transition to Available, and transition to Expired:
@@ -65,11 +90,11 @@ class ExportDBTest extends BaseTest
         $initialCount = $query->countSubmittedRecords();
 
         // add new record and verify
-        $requestId = $query->createRequestRecord($userId, 'Jobs', '2019-01-01','2019-03-01');
+        $requestId = $query->createRequestRecord($userId, 'Jobs', '2019-01-01','2019-03-01','CSV');
         $this->assertNotNull($requestId);
 
         // add another new record and verify
-        $requestId2 = $query->createRequestRecord($userId, 'Accounts', '2016-12-01','2017-01-01');
+        $requestId2 = $query->createRequestRecord($userId, 'Accounts', '2016-12-01','2017-01-01','JSON');
 
         $this->assertNotNull($requestId2 );
         $this->assertTrue($requestId2-$requestId==1);
@@ -85,6 +110,39 @@ class ExportDBTest extends BaseTest
         if (self::$debug) print("\n".__FUNCTION__.": initialCount=$initialCount finalCount=$finalCount requestId=$requestId requestId2=$requestId2\n");
     }
 
+    // Test should fail. Do not allow this transition.
+    public function testAvailableToFailed()
+    {
+        $query = new QueryHandler();
+
+        // Find or create a record in available status
+        $maxAvailable = $this->findAvailableRecord();
+        if (self::$debug) print("\n".__FUNCTION__.": max=$maxAvailable\n");
+
+        // Attempt to transition the record
+        $result = $query->submittedToFailed($maxAvailable);
+        $test = static::$dbh->query("SELECT export_succeeded FROM batch_export_requests where id=:id",
+                                    array('id'=>$maxAvailable))[0]['export_succeeded'];
+
+        // Assert that:
+        //
+        // Exactly zero records were transitioned
+        $this->assertTrue($result==0);
+
+        // That record is marked export_succeeded=TRUE
+        $this->assertEquals($test, 1);
+
+        // That record has a non-null export created datetime
+        $export_created = static::$dbh->query("SELECT export_created_datetime FROM batch_export_requests where id=:id",
+                                    array('id'=>$maxAvailable))[0]['export_created_datetime'];
+
+        // That record is marked export_created_datetime=NOT NULL
+        // todo: has a datetime
+        //$this->assertEquals($test, 1);
+
+        // debug
+        if (self::$debug) print("\n".__FUNCTION__.": NON transitioned Id=$maxAvailable\n");
+    }
     public function testSubmittedToFailed()
     {
         $query = new QueryHandler();
@@ -135,13 +193,7 @@ class ExportDBTest extends BaseTest
         $query = new QueryHandler();
 
         // Find or create a record in available status to transition
-        $maxAvailable = static::$dbh->query('SELECT MAX(id) AS id FROM batch_export_requests where
-                                            export_succeeded IS TRUE
-                                            AND export_expired IS FALSE')[0]['id'];
-        if ($maxAvailable == NULL) {
-            $maxSubmitted = $this->findSubmittedRecord();
-            $maxAvailable = $query->submittedToAvailable($maxSubmitted);
-        }
+        $maxAvailable = $this->findAvailableRecord();
 
         $result = $query->availableToExpired($maxAvailable);
         $test = static::$dbh->query("SELECT export_expired FROM batch_export_requests where id=:id",
@@ -167,14 +219,16 @@ class ExportDBTest extends BaseTest
             'id',
             'realm',
             'start_date',
-            'end_date'
+            'end_date',
+            'export_file_format',
+            'requested_datetime'
         );
 
         $actual = $query->listSubmittedRecords();
 
         if (count($actual) > 0) {
 
-            // check that you get the same fields back from the query...
+            // assert that the expected fields are returned from the query
             $this->assertEquals($expectedKeys, array_keys($actual[0]));
         }
     }
@@ -193,7 +247,9 @@ class ExportDBTest extends BaseTest
             'export_succeeded',
             'export_expired',
             'export_expires_datetime',
-            'export_created_datetime'
+            'export_created_datetime',
+            'export_file_format',
+            'requested_datetime'
         );
 
         // Requests via this user have been created as part of these tests
@@ -201,19 +257,95 @@ class ExportDBTest extends BaseTest
 
         if (count($actual) > 0) {
 
-            // check that you get the same fields back from the query...
+            // assert that the expected fields are returned from the query
             $this->assertEquals($expectedKeys, array_keys($actual[0]));
+        }
+    }
+
+    public function testUserRecordReportStates()
+    {
+        $query = new QueryHandler();
+        $userId = $this->acquireUserId(); //XDUser::getUserByUserName(self::$userName);
+
+        // Expect these keys from the associative array
+        $expectedKeys = array(
+            'id',
+            'realm',
+            'start_date',
+            'end_date',
+            'export_succeeded',
+            'export_expired',
+            'export_expires_datetime',
+            'export_created_datetime',
+            'export_file_format',
+            'requested_datetime',
+            'state'
+        );
+
+        // Requests via this user have been created as part of these tests
+        $actual = $query->listUserRequestsByState($userId);
+
+        if (count($actual) > 0) {
+
+            // assert that the expected fields are returned from the query
+            $this->assertEquals($expectedKeys, array_keys($actual[0]));
+        }
+
+
+    }
+
+    // verify that user that created request can delete it
+    public function testRecordDeleteCorrectUser()
+    {
+        $query = new QueryHandler();
+        $userId = $this->acquireUserId(); //XDUser::getUserByUserName(self::$userName);
+
+        // Requests via this user have been created as part of these tests
+        $actual = $query->listRequestsForUser($userId);
+
+        if (count($actual) > 0) {
+
+            // pick the first such request and try to delete it:
+            $testVal = $query->deleteRequest($actual[0]['id'], $userId);
+
+            // assert that the delete affected 1 row
+            $this->assertEquals($testVal, 1);
+            if (self::$debug) print("\n".__FUNCTION__.": deleted record? $testVal\n");
+        }
+    }
+
+    // verify that user that did not create request cannot delete it
+    public function testRecordDeleteIncorrectUser()
+    {
+        $query = new QueryHandler();
+        $userId = $this->acquireUserId();
+        $wrongUserId = \XDUser::PUBLIC_USER;
+
+        // Requests via user with $userId have been created as part of these tests
+        $actual = $query->listRequestsForUser($userId);
+
+        // Provided that different users created and try to delete:
+        if (count($actual) > 0 && $userId != $wrongUserId) {
+
+            // pick the first such request and try to delete it:
+            $testVal = $query->deleteRequest($actual[0]['id'], $wrongUserId);
+
+            // assert that the delete attempt affected 0 rows
+            $this->assertEquals($testVal, 0);
+            if (self::$debug) print("\n".__FUNCTION__.": deleted record? $testVal\n");
         }
     }
 
     // determine initial max id to enable cleanup after testing
     public static function setUpBeforeClass()
     {
+        // so long as you use PUBLIC_USER_NAME or the like
         parent::setUpBeforeClass();
         static::$dbh = DB::factory('database');
         static::$maxId = static::$dbh->query('SELECT COALESCE(MAX(id), 0) AS id FROM batch_export_requests')[0]['id'];
     }
 
+    // TODO: put it back...
     // Reset the table to where it started
     public static function tearDownAfterClass()
     {
