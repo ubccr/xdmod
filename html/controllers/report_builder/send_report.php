1d0e929fa05a16558479a3fd46d990da672641b2
Mohammad K. Zia <mkzia@buffalo.edu>
2019-06-03 13:16:11 -0400

    Update conditional to check for empty post param

--- html/controllers/report_builder/send_report.php
+++ html/controllers/report_builder/send_report.php
@@ -22,7 +22,7 @@
          $export_format = $_POST['export_format'];
       }   
 
-      if (isset($_POST['start_date']) && isset($_POST['end_date'])) {
+      if (isset($_POST['start_date']) && !empty($_POST['start_date']) && isset($_POST['end_date']) && !empty($_POST['end_date'])) {
          $start_date = $_POST['start_date'];
          $end_date = $_POST['end_date'];
       } else {
