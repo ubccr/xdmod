4db546c63da8ff6b5d31cbc5dbdcad9c59f3a118
Greg Dean <gmdean@buffalo.edu>
2019-06-28 15:32:35 -0400

    style updates

--- html/controllers/user_admin/reset_user_tour_viewed.php
+++ html/controllers/user_admin/reset_user_tour_viewed.php
@@ -1,13 +1,23 @@
 <?php
 $logged_in_user = \xd_security\assertDashboardUserLoggedIn();
+$params = array('uid' => RESTRICTION_UID);
+
+$returnData  = array(
+    'success' => true,
+    'total' => 1,
+    'message' => 'This user will be now be propmted to view the New User Tour the next time they visit XDMoD'
+);
+
 $isValid = xd_security\secureCheck($params, 'POST');
+\xd_security\assertParameterSet('uid', RESTRICTION_UID);
+
 if (!$isValid) {
     $returnData['success'] = false;
     $returnData['status'] = 'invalid_id_specified';
+    $returnData['total'] = 0;
+    $returnData['message'] = 'invalid_id_specified';
     xd_controller\returnJSON($returnData);
 };
-
-\xd_security\assertParameterSet('uid', RESTRICTION_UID);
 
 $user = XDUser::getUserByID($_POST['uid']);
 if ($user == null) {
@@ -17,8 +27,4 @@ if ($user == null) {
 $storage = new \UserStorage($user, 'viewed_user_tour');
 $storage->upsert(0, ['uid' => $_POST['uid'], 'viewedTour' => $_POST['viewed_user_tour']]);
 
-\xd_controller\returnJSON(array(
-    'success' => true,
-    'total' => 1,
-    'message' => 'This user will be now be propmted to view the New User Tour the next time they visit XDMoD'
-));
+\xd_controller\returnJSON($returnData);
