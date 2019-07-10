8a81fddbc0f33f03d42fe4eef00acbcf7f3ca7f0
Jeffrey T. Palmer <jtpalmer@buffalo.edu>
2019-07-10 09:09:38 -0400

    Add missing function call

--- background_scripts/batch_export_manager.php
+++ background_scripts/batch_export_manager.php
@@ -64,7 +64,7 @@ try {
         'mail' => false,
         'consoleLogLevel' => $logLevel
     );
-    $logger = Log('batch-export', $logConf);
+    $logger = Log::factory('batch-export', $logConf);
     $logger->info('Command: ' . implode(' ', array_map('escapeshellarg', $argv)));
     // NOTE: "process_start_time" is needed for the log summary.
     $logger->notice(['message' => 'batch_export_manager start', 'process_start_time' => date('Y-m-d H:i:s')]);
