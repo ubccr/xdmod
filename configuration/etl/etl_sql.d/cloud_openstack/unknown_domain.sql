77e888f60de6e72ef2cb42f1a097ec0b5727ecc1
Ryan Rathsam <ryanrath@buffalo.edu>
2019-09-17 13:13:51 -0400

    Removing `domain_id` from `domains` table.
    - Updated the `domains` table to not include a `domain_id` column as this was
      never really used.
    - Updated the `OpenStackDomainIngestor` to basically be
      `OpenStackDomainStagingIngestor`.
    - Removed the `OpenStackDomainStagingIngestor` as the functionality it provided
      was moved to `OpenStackDomainIngestor`.
    - Updated `unknown_domain.sql` to work with the updated `domains` table. Also
      updated how the `resource_id` is selected. Previously the `domains_staging`
      table was queried. But since this table is no longer present we instead check
      the `openstack_raw_event` table first, if one is not found there then we check
      the `domains` table and finally if neither of those are found then we default
      to -1.

--- configuration/etl/etl_sql.d/cloud_openstack/unknown_domain.sql
+++ configuration/etl/etl_sql.d/cloud_openstack/unknown_domain.sql
@@ -1,16 +1,25 @@
-INSERT INTO ${DESTINATION_SCHEMA}.`domains` (id, domain_id, resource_id, name)
+INSERT INTO ${DESTINATION_SCHEMA}.`domains` (id, resource_id, name)
 SELECT inc.*
 FROM (
-         SELECT  1                                                                                                        as id,
-                -1                                                                                                        as domain_id,
-                /* We need a valid resource_id so attempt to find the one associated with the `default`
-                   domain and utilize that. If we can't find one then fall back to -1. */
-                COALESCE((SELECT resource_id FROM ${DESTINATION_SCHEMA}.`domains_staging` d WHERE LOWER(d.name) = 'default'),-1)  as resource_id,
-                'Unknown'                                                                                                 as name
+         SELECT 1           as id,
+             /* We need a valid resource_id so attempt to find the one associated with the `default`
+                domain and utilize that. If we can't find one then fall back to a default entry in
+                domains. If that's not present then fall back to -1. */
+                COALESCE(
+                        COALESCE(
+                                (SELECT resource_id
+                                 FROM ${DESTINATION_SCHEMA}.openstack_raw_event ore
+                                 WHERE LOWER(ore.domain) = 'default'
+                                 LIMIT 1),
+                                (SELECT resource_id
+                                 FROM ${DESTINATION_SCHEMA}.domains d
+                                 WHERE LOWER(d.name) = 'default')
+                            ),
+                        -1) as resource_id,
+                'Unknown'   as name
      ) inc
-         LEFT JOIN ${DESTINATION_SCHEMA}.`domains` cur
+         LEFT JOIN modw_cloud.`domains` cur
                    ON cur.id = inc.id
                        AND cur.resource_id = inc.resource_id
-                       AND cur.domain_id = inc.domain_id
                        AND cur.name = inc.name
 WHERE cur.id IS NULL;
