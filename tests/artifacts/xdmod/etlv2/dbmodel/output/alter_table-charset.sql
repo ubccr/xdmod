2c9a1fa31d228af23baef9416f38edceccd1ec90
Joseph P. White <jpwhite4@buffalo.edu>
2019-12-02 15:50:38 -0500

    Update existing test artifacts

--- tests/artifacts/xdmod/etlv2/dbmodel/output/alter_table-charset.sql
+++ tests/artifacts/xdmod/etlv2/dbmodel/output/alter_table-charset.sql
@@ -2,9 +2,11 @@ ALTER TABLE `test_db_model`
 CHARSET = utf8mb4,
 COLLATE = utf8mb4_general_ci,
 DROP INDEX `fk_instance`,
-ADD INDEX `fk_instance` USING BTREE (`instance_id`, `inferred`),
-DROP FOREIGN KEY `con_col1`,
-ADD CONSTRAINT `con_col1` FOREIGN KEY (`col1`) REFERENCES `db_test_model2` (`col4`);
+ADD INDEX `fk_instance` USING BTREE (`instance_id`, `inferred`);
 ALTER TABLE `test_db_model`
 CHANGE COLUMN `col1` `col1` varchar(32) CHARSET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'mydefault' ,
 CHANGE COLUMN `instance_id` `instance_id` int(11) NULL DEFAULT -1 ;
+ALTER TABLE `test_db_model`
+DROP FOREIGN KEY `con_col1`;
+ALTER TABLE `test_db_model`
+ADD CONSTRAINT `con_col1` FOREIGN KEY (`col1`) REFERENCES `db_test_model2` (`col4`);
