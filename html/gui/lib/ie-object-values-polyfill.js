d64d8dd4d0cd4bc16e71b309fa3b52f766e9e627
Joseph P. White <jpwhite4@buffalo.edu>
2019-12-12 11:02:32 -0500

    Add polyfills for missing IE11 functions.
    The dashboard components needed Array.find() and String.padStart() which
    are missing from IE11. Also since the polyfills are being added
    consolidate the existing mishmash of includes so that all polyfills for
    internet explorer are in one place.

--- html/gui/lib/ie-object-values-polyfill.js
+++
@@ -1,9 +0,0 @@
-if (!Object.values) {
-    Object.values = function (obj) {
-        return Object.keys(obj).map(
-          function (key) {
-              return obj[key];
-          }
-        );
-    };
-}
