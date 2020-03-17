a205094679836f39e9aa4c345aa679c0c0c942d1
Greg Dean <gmdean@buffalo.edu>
2020-03-17 08:47:05 -0400

    adding documentation

--- configuration/etl/etl_sql.d/migrations/cloud-migration-8-5-1_9-0-0.sql
+++ configuration/etl/etl_sql.d/migrations/cloud-migration-8-5-1_9-0-0.sql
@@ -1,3 +1,9 @@
+-- In order to chnage from a multi column to single column unique auto increment id field
+-- we change in id field on the table to not be auto increment, then drop the auto increment
+-- key and finally drop the id field. Once ingestion is run again at the end of the migration
+-- pipeline the id field is remade as a unique field with auto incremented id's. The aggregation
+-- tables use the account_id and instance_type_id fields and are updated when aggregation is run
+-- at the end of the migration script
 ALTER TABLE modw_cloud.account MODIFY account_id INT NOT NULL;
 ALTER TABLE modw_cloud.account DROP INDEX autoincrement_key;
 ALTER TABLE modw_cloud.account DROP COLUMN account_id;
@@ -6,6 +12,10 @@ ALTER TABLE modw_cloud.instance_type MODIFY instance_type_id INT NOT NULL;
 ALTER TABLE modw_cloud.instance_type DROP INDEX increment_key;
 ALTER TABLE modw_cloud.instance_type DROP COLUMN instance_type_id;
 //
+-- The instance_id in used on the event table so we need to update the event table
+-- with the new instance_id value. The existing multi column auto increment key is
+-- dropped and a new field is created. Once the event table is updated the old instance_id
+-- is dropped and the new instance_id field is renamed
 ALTER TABLE modw_cloud.instance MODIFY instance_id INT(11) NOT NULL;
 ALTER TABLE modw_cloud.instance DROP INDEX increment_key;
 ALTER TABLE modw_cloud.instance ADD COLUMN new_instance_id INT(11) UNSIGNED NOT NULL auto_increment unique;
