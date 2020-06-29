de51625894b7dbbb9f1395db49cc1a17b8f995bc
Ryan Rathsam <ryanrath@buffalo.edu>
2020-06-29 09:20:11 -0400

    Finally removing Roles / UserRole tables
    We have been running for some time without actually utilizing these tables and as such
    it should be safe to remove them. These changes will make sure that the tables are not
    created / referenced in ETL actions as well as making sure that they are removed in
    existing installations when an upgrade is performed.

---
+++ configuration/etl/etl_sql.d/migrations/8.5.1-9.0.0/role-table-cleanup.sql
@@ -0,0 +1,4 @@
+-- These tables are no longer user and as such are being removed.
+DROP TABLE IF EXISTS moddb.UserRoleParameters;
+DROP TABLE IF EXISTS moddb.UserRoles;
+DROP TABLE IF EXISTS moddb.Roles;
