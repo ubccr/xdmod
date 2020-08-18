413528968316846d2dd34f3875e95e598ee12ef9
Ben Plessinger <plessbd@users.noreply.github.com>
2020-08-18 17:20:38 -0400

    Initial XDMoD 9.5 change (#1422)

--- configuration/etl/etl_sql.d/migrations/8.5.1-9.0.0/role-table-cleanup.sql
+++
@@ -1,4 +0,0 @@
--- These tables are no longer user and as such are being removed.
-DROP TABLE IF EXISTS moddb.UserRoleParameters;
-DROP TABLE IF EXISTS moddb.UserRoles;
-DROP TABLE IF EXISTS moddb.Roles;
