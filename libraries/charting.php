048aaa8eaf9e5cd6f1e219b1a3ab93b6b1e553d4
Benjamin D. Plessinger <ben@plessinger.us>
2020-08-20 09:27:46 -0400

    Cleanup exportHighcharts and Related Functions

--- libraries/charting.php
+++ libraries/charting.php
@@ -46,10 +46,6 @@ function exportHighchart(
     $effectiveWidth = (int)($width*$scale);
     $effectiveHeight = (int)($height*$scale);
 
-    $base_filename = sys_get_temp_dir() . '/' . md5(rand() . microtime());
-
-    $tmp_html_filename     = $base_filename . '.html';
-
     $html_dir = __DIR__ . "/../html";
     $template = file_get_contents($html_dir . "/highchart_template.html");
 
@@ -63,15 +59,9 @@ function exportHighchart(
     $template = str_replace('_globalChartOptions_', json_encode($globalChartOptions), $template);
     $template = str_replace('_width_',$effectiveWidth, $template);
     $template = str_replace('_height_',$effectiveHeight, $template);
-    file_put_contents($tmp_html_filename,$template);
-
-    $data = getScreenFromChromium($tmp_html_filename, $effectiveWidth, $effectiveHeight, $format ===  'pdf' ? 'svg' : $format);
-    @unlink($tmp_html_filename);
+    $data = getScreenFromChromium($template, $effectiveWidth, $effectiveHeight, $format ===  'pdf' ? 'svg' : $format);
     if($format === 'pdf'){
-        $svgFile = $tmp_html_filename . '.svg';
-        file_put_contents($svgFile, $data);
-        $data = svg2pdf($svgFile, round($width / 90.0 * 72.0), round($height / 90.0 * 72.0), $fileMetadata);
-        @unlink($svgFile);
+        $data = svg2pdf($data, round($width / 90.0 * 72.0), round($height / 90.0 * 72.0), $fileMetadata);
     }
     return $data;
 }
@@ -81,7 +71,7 @@ function exportHighchart(
  *
  * For svg generation uses chromium repl
  *
- * @param string $file location of html template file to use
+ * @param string $html html that should be used by chromium
  * @param int $width desired width of output
  * @param int $height desired height of output
  * @param string $format of output (png or svg)
@@ -91,7 +81,17 @@ function exportHighchart(
  *
  * @throws \Exception on invalid format, command execution failure, or non zero exit status
  */
-function getScreenFromChromium($file, $width, $height, $format){
+function getScreenFromChromium($html, $width, $height, $format){
+
+    // Chromium requires the file to have a .html extension
+    // cant use datauri as it will not execute embdeeded javascript
+    $tmpFile = tempnam(sys_get_temp_dir(), 'xdmod-chromiumHtml-');
+    $tmpHtmlFile = $tmpFile . '.html';
+    if ($tmpFile === false || rename($tmpFile, $tmpHtmlFile) === false) {
+        @unlink($tmpFile);
+        throw \Exception('Error creating temporary html file for chromium');
+    }
+    file_put_contents($tmpHtmlFile, $html);
 
     $repl = '';
     if ($format == 'svg'){
@@ -99,7 +99,10 @@ function getScreenFromChromium($file, $width, $height, $format){
         $outputType = '-repl';
     }
     elseif ($format == 'png'){
-        $outputFile = $file . '.png';
+        $outputFile = tempnam(sys_get_temp_dir(), 'xdmod-chromiumScreenshot-');
+        if ($outputFile === false) {
+            throw \Exception('Error creating temporary png file for chromium');
+        }
         $outputType = '--screenshot=' . $outputFile;
     }
     else {
@@ -115,7 +118,7 @@ function getScreenFromChromium($file, $width, $height, $format){
         '--disable-extensions',
         '--incognito',
         $outputType,
-        $file
+        $tmpHtmlFile
     );
     $command = $chromiumPath . ' ' . implode(' ', $chromiumOptions);
     $pipes = array();
@@ -136,8 +139,9 @@ function getScreenFromChromium($file, $width, $height, $format){
     $err = stream_get_contents($pipes[2]);
     fclose($pipes[1]);
     fclose($pipes[2]);
-
     $return_value = proc_close($process);
+
+    @unlink($tmpHtmlFile);
 
     if ($return_value != 0) {
         throw new \Exception('Unable execute command: "'. $command . '". Details: ' . $err);
@@ -156,7 +160,7 @@ function getScreenFromChromium($file, $width, $height, $format){
 /**
  * Use rsvg-convert to convert svg to pdf
  *
- * @param string path to input file
+ * @param string $svgData string of the SVG
  * @param int $width the new paper size in postscript points (72 ppi).
  * @param int $height the new paper size in postscript points (72 ppi).
  * @param array $metaData array containing metadata fields
@@ -164,9 +168,8 @@ function getScreenFromChromium($file, $width, $height, $format){
  * @throws Exception when unable to execute or non-zero return code
  */
 
-function svg2pdf($inputFilename, $width, $height, $metaData){
-    $outputFilename = $inputFilename . '.pdf';
-    $command = 'rsvg-convert -w ' .$width. ' -h '.$height.' -f pdf -o ' . $outputFilename  . ' ' . $inputFilename;
+function svg2pdf($svgData, $width, $height, $metaData){
+    $command = 'rsvg-convert -w ' .$width. ' -h '.$height.' -f pdf';
     $pipes = array();
     $descriptor_spec = array(
         0 => array('pipe', 'r'),
@@ -177,7 +180,8 @@ function svg2pdf($inputFilename, $width, $height, $metaData){
     if (!is_resource($process)) {
         throw new \Exception('Unable execute command: "'. $command . '". Details: ' . print_r(error_get_last(), true));
     }
-
+    fwrite($pipes[0], $svgData);
+    fclose($pipes[0]);
     $out = stream_get_contents($pipes[1]);
     $err = stream_get_contents($pipes[2]);
 
@@ -189,26 +193,24 @@ function svg2pdf($inputFilename, $width, $height, $metaData){
     if ($return_value != 0) {
         throw new \Exception("$command returned $return_value, stdout: $out stderr: $err");
     }
-    $data = getPdfWithMetadata($outputFilename, $metaData);
-    @unlink($outputFilename);
-    return $data;
+    return getPdfWithMetadata($out, $metaData);
 }
 
 /**
  * Use exiftool to set document metadata.
  *
- * @param string $pdfFilename of PDF file
+ * @param string $pdf string representation of  PDF file
  * @param array $docmeta array containing metadata fields
  *
  * @return string PDF document
  */
-function getPdfWithMetadata($fileName, $docmeta){
+function getPdfWithMetadata($pdf, $docmeta){
     $author = isset($docmeta['author']) ? addcslashes($docmeta['author'], "()\n\\") : 'XDMoD';
     $subject = isset($docmeta['subject']) ? addcslashes($docmeta['subject'], "()\n\\") : 'XDMoD chart';
     $title = isset($docmeta['title']) ? addcslashes($docmeta['title'], "()\n\\") :'XDMoD PDF chart export';
     $creator = addcslashes('XDMoD ' . OPEN_XDMOD_VERSION, "()\n\\");
 
-    $command = "exiftool -Title='$title' -Author='$author' -Subject='$subject' -Creator='$creator' -o - $fileName";
+    $command = "exiftool -Title='$title' -Author='$author' -Subject='$subject' -Creator='$creator' -o - -";
     $pipes = array();
     $descriptor_spec = array(
         0 => array('pipe', 'r'),
@@ -220,6 +222,9 @@ function getPdfWithMetadata($fileName, $docmeta){
     if (!is_resource($process)) {
         throw new \Exception('Unable execute command: "'. $command . '". Details: ' . print_r(error_get_last(), true));
     }
+    fwrite($pipes[0], $pdf);
+    fclose($pipes[0]);
+
     $out = stream_get_contents($pipes[1]);
     $err = stream_get_contents($pipes[2]);
 
