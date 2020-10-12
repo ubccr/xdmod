0bea9a10798b1f4e76320ffec472fe64eae32d12
Ryan Rathsam <ryanrath@buffalo.edu>
2020-10-12 12:28:04 -0400

    Updating Log Levels

--- html/internal_dashboard/controllers/log/get_levels.php
+++ html/internal_dashboard/controllers/log/get_levels.php
@@ -1,25 +1,23 @@
 <?php
 /**
- * Return PEAR Log levels.
+ * Return Monolog Logger levels.
  *
  * @author Jeffrey T. Palmer <jtpalmer@ccr.buffalo.edu>
  */
 
 try {
 
-    require_once 'Log.php';
-
     $returnData = array(
         'success' => true,
         'response' => array(
-            array('id' => PEAR_LOG_EMERG,   'name' => 'Emergency'),
-            array('id' => PEAR_LOG_ALERT,   'name' => 'Alert'),
-            array('id' => PEAR_LOG_CRIT,    'name' => 'Critical'),
-            array('id' => PEAR_LOG_ERR,     'name' => 'Error'),
-            array('id' => PEAR_LOG_WARNING, 'name' => 'Warning'),
-            array('id' => PEAR_LOG_NOTICE,  'name' => 'Notice'),
-            array('id' => PEAR_LOG_INFO,    'name' => 'Info'),
-            array('id' => PEAR_LOG_DEBUG,   'name' => 'Debug'),
+            array('id' => \Monolog\Logger::EMERGENCY, 'name' => 'Emergency'),
+            array('id' => \Monolog\Logger::ALERT,     'name' => 'Alert'),
+            array('id' => \Monolog\Logger::CRITICAL,  'name' => 'Critical'),
+            array('id' => \Monolog\Logger::ERROR,     'name' => 'Error'),
+            array('id' => \Monolog\Logger::WARNING,   'name' => 'Warning'),
+            array('id' => \Monolog\Logger::NOTICE,    'name' => 'Notice'),
+            array('id' => \Monolog\Logger::INFO,      'name' => 'Info'),
+            array('id' => \Monolog\Logger::DEBUG,     'name' => 'Debug'),
         ),
     );
 
