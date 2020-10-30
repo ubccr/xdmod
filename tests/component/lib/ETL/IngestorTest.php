9876fae30a031c17f1f4c3423811870f54c3f822
Ryan Rathsam <ryanrath@buffalo.edu>
2020-10-30 16:24:14 -0400

    strpos tests don't need characeters escaped

--- tests/component/lib/ETL/IngestorTest.php
+++ tests/component/lib/ETL/IngestorTest.php
@@ -78,7 +78,7 @@ class IngestorTest extends \PHPUnit_Framework_TestCase
         $numWarnings = 0;
         if ( ! empty($result['stdout']) ) {
             foreach ( explode(PHP_EOL, trim($result['stdout'])) as $line ) {
-                if ( false !== strpos($line, '\[warning\]') ) {
+                if ( false !== strpos($line, '[warning]') ) {
                     $numWarnings++;
                 }
             }
@@ -127,7 +127,7 @@ class IngestorTest extends \PHPUnit_Framework_TestCase
 
         if ( ! empty($result['stdout']) ) {
             foreach ( explode(PHP_EOL, trim($result['stdout'])) as $line ) {
-                if ( false !== strpos($line, '\[warning\]') ) {
+                if ( false !== strpos($line, '[warning]') ) {
                     $numWarnings++;
                 }
             }
@@ -143,7 +143,7 @@ class IngestorTest extends \PHPUnit_Framework_TestCase
 
         if ( ! empty($result['stdout']) ) {
             foreach ( explode(PHP_EOL, trim($result['stdout'])) as $line ) {
-                if ( false !== strpos($line, '\[warning\]') ) {
+                if ( false !== strpos($line, '[warning]') ) {
                     $numWarnings++;
                 }
             }
