3a90ce2c92a5308507be4aa869db65209177f3be
ryanrath <ryanrath@buffalo.edu>
2021-02-12 10:21:04 -0500

    Adding CircleCI Support
    Since Shippable is imminitely end-of-life we need to find, yet again, a new CI
    service for hosting XDMoD. After trying Github Actions CircleCI was the next up
    and it was pretty darn easy to modify our Shippable workflow to instead work
    with CircleCI. The latest changes ensure that the SSO tests run successfully

--- tests/ui/test/specs/xdmod/SSOLogin.js
+++ tests/ui/test/specs/xdmod/SSOLogin.js
@@ -1,13 +1,18 @@
 describe('Single Sign On Login', () => {
     it('Should have the Single Sign On option', () => {
-        browser.url('/');
+        browser.url('https://');
         browser.waitForInvisible('.ext-el-mask-msg');
         browser.waitAndClick('a[href*=actionLogin]');
+    });
+    it('Should let us select the SSO Login button', () => {
         browser.waitForVisible('#SSOLoginLink');
         browser.waitAndClick('#SSOLoginLink');
     });
     it('Should goto the Single Sign On login page and login', () => {
-        browser.waitForExist('form[action="/signin"]');
+        browser.waitUntil(() => {
+            return browser.getTitle() === 'Simple Identity Provider';
+        }, 30000);
+        browser.waitForVisible('form[action="/signin"]', 30000);
         browser.submitForm('form[action="/signin"]');
     });
     it('Display Logged in Users Name', () => {
