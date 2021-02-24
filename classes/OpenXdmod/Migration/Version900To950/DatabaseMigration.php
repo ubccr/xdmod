1067a930f9aab613ba608f79e89f187b679a539b
Greg Dean <gmdean@buffalo.edu>
2021-02-24 08:18:28 -0500

    fixing migration

--- classes/OpenXdmod/Migration/Version900To950/DatabaseMigration.php
+++ classes/OpenXdmod/Migration/Version900To950/DatabaseMigration.php
@@ -19,10 +19,10 @@ class DatabasesMigration extends \OpenXdmod\Migration\DatabasesMigration
         parent::execute();
 
         $dbh = DB::factory('datawarehouse');
-        $mysql_helper = \CCR\DB\MySQLHelper($dbh);
+        $mysql_helper = \CCR\DB\MySQLHelper::factory($dbh);
         if ($mysql_helper->tableExists('modw_cloud.cloud_resource_specs')) {
             Utilities::runEtlPipeline(
-                ['cloud-migration-9-0-0_9-5-0'],
+                ['cloud-migration-9_0_0-9_5_0'],
                 $this->logger,
                 [
                     'last-modified-start-date' => '2017-01-01 00:00:00'
