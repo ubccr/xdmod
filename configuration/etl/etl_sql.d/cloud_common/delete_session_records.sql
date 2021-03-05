fe9b27b0adf7d19b977ff3c1fc2fe08bd55486bf
Greg Dean <gmdean@buffalo.edu>
2021-03-05 10:40:23 -0500

    removing rows from cloudfact_by_day_sessionlist for instances that are being re-aggregated

--- configuration/etl/etl_sql.d/cloud_common/delete_session_records.sql
+++ configuration/etl/etl_sql.d/cloud_common/delete_session_records.sql
@@ -1,2 +1,10 @@
-DELETE FROM modw_cloud.session_records WHERE instance_id IN (SELECT DISTINCT instance_id FROM modw_cloud.event_reconstructed)
+DELETE FROM
+  ${SOURCE_SCHEMA}.cloudfact_by_day_sessionlist
+WHERE
+  session_id IN (SELECT DISTINCT session_id FROM ${SOURCE_SCHEMA}.session_records WHERE instance_id IN (SELECT DISTINCT instance_id FROM ${SOURCE_SCHEMA}.event_reconstructed))
+//
+DELETE FROM
+  ${SOURCE_SCHEMA}.session_records
+WHERE
+  instance_id IN (SELECT DISTINCT instance_id FROM ${SOURCE_SCHEMA}.event_reconstructed)
 //
