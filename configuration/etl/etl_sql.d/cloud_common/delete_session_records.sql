657ddb718dfa01d0bb834f4de7957e90e69b4524
Greg Dean <gmdean@buffalo.edu>
2021-03-11 10:06:56 -0500

    changing JobListAggregator to accept different table name

--- configuration/etl/etl_sql.d/cloud_common/delete_session_records.sql
+++ configuration/etl/etl_sql.d/cloud_common/delete_session_records.sql
@@ -1,9 +1,4 @@
 DELETE FROM
-  ${SOURCE_SCHEMA}.cloudfact_by_day_sessionlist
-WHERE
-  session_id IN (SELECT DISTINCT session_id FROM ${SOURCE_SCHEMA}.session_records WHERE instance_id IN (SELECT DISTINCT instance_id FROM ${SOURCE_SCHEMA}.event_reconstructed))
-//
-DELETE FROM
   ${SOURCE_SCHEMA}.session_records
 WHERE
   instance_id IN (SELECT DISTINCT instance_id FROM ${SOURCE_SCHEMA}.event_reconstructed)
