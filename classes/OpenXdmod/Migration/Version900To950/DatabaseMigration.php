8befbf6570c91b647566c2b48c8a24e303544ec2
Greg Dean <gmdean@buffalo.edu>
2021-03-15 11:31:41 -0400

    style updates

--- classes/OpenXdmod/Migration/Version900To950/DatabaseMigration.php
+++ classes/OpenXdmod/Migration/Version900To950/DatabaseMigration.php
@@ -29,9 +29,9 @@ class DatabasesMigration extends \OpenXdmod\Migration\DatabasesMigration
                     FROM
                         modw_cloud.staging_resource_specifications
                     GROUP BY
-                    	   resource_id, hostname, fact_date
+                    	  resource_id, hostname, fact_date
                     HAVING
-                    	   COUNT(*) > 1";
+                        COUNT(*) > 1";
 
             $staging_result = $dbh->query($staging_resource_sql);
 
