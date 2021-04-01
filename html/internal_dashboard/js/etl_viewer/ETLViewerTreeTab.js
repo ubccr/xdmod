72cb34a45f5ebf4a62bc5acceb6a63c79124d391
Ryan Rathsam <build@shippable.com>
2021-04-01 16:25:18 -0400

    Simplifying the ETLViewer and adding a few features
    - removed the File Select / Search Panel as this isn't being used atm.
    - Added the ability for users to shift+click a tree node and either recursively
      expanding or collapsing all of the child nodes.
    - Added the ability for the ETLViewer's tree ( Ext.ux.tree.TreeGrid ) to
      automatically expand a column to take up the remaining space via the
      `autoExpandColumn` property.

--- html/internal_dashboard/js/etl_viewer/ETLViewerTreeTab.js
+++ html/internal_dashboard/js/etl_viewer/ETLViewerTreeTab.js
@@ -3,70 +3,56 @@ Ext.namespace('XDMoD', 'XDMoD.Admin', 'XDMoD.Admin.ETL');
 XDMoD.Admin.ETL.ETLViewerTreeTab = Ext.extend(Ext.Panel, {
     title: 'ETL Tree View',
     layout: 'border',
-
     initComponent: function () {
-
-        this.fileSelect = new Ext.form.ComboBox({
-            fieldLabel: 'ETL File',
-            valueField: 'name',
-            displayField: 'name',
-            typeAhead: true,
-            lazyRender: true,
-            triggerAction: 'all',
-            store: new Ext.data.JsonStore({
-                autoLoad: true,
-                url: XDMoD.REST.url  + '/etl/files',
-                root: 'results',
-                idProperty: 'name',
-                fields: ['name']
-            })
-        });
-
-        this.searchPanel = new Ext.Panel({
-            title: 'Search',
-            region: 'west',
-            split: true,
-            collapsible: true,
-            collapsed: false,
-            collapseFirst: false,
-            width: 375,
-            layout: 'border',
-            margins: '2 0 2 2',
-            border: true,
-            items: [
-                {
-                    xtype: 'form',
-                    layout: 'fit',
-                    region: 'center',
-                    height: 90,
-                    border: false,
-                    items: [
-                        {
-                            xtype: 'fieldset',
-                            header: false,
-                            layout: 'form',
-                            hideLabels: false,
-                            border: false,
-                            defaults: {
-                                anchor: '0'
-                            },
-                            items: [
-                                this.fileSelect
-                            ]
-                        }
-                    ]
-                }
-            ]
-        });
-
+        var self = this;
         this.tree = new Ext.ux.tree.TreeGrid({
             region: 'center',
             closable: false,
             updateHistory: true,
-            title: 'ETL Pipeline Viewer',
             autoScroll: true,
             enableDD: true,
+            autoExpandColumn: 'value',
+            tbar: {
+                items: [
+                    {
+                        xtype: 'button',
+                        text: 'Expand All',
+                        iton: '',
+                        cls: 'x-btn-text-icon'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'Collapse All',
+                        icon: '',
+                        cls: 'x-btn-text-icon'
+                    },
+                    {
+                        xtype: 'textfield',
+                        emptyText: 'Enter Search Term Here'
+                    }
+                ]
+            },
             dataUrl: XDMoD.REST.url  + '/etl/pipelines/actions',
+            keys: [
+                {
+                    key: Ext.EventObject.SHIFT,
+                    fn: function() {
+                        self.shiftClicked = !self.shiftClicked;
+                    }
+                }
+            ],
+            listeners: {
+                expandnode: function(node) {
+                    if (CCR.xdmod.shiftKey === true && node.hasChildNodes()) {
+                        node.expandChildNodes(true);
+                    }
+                },
+                collapsenode: function(node) {
+                    if (CCR.xdmod.shiftKey === true && node.hasChildNodes()) {
+                        node.collapseChildNodes(true);
+                    }
+                }
+            },
             columns: [
                 {
                     header: 'Name',
@@ -78,7 +64,36 @@ XDMoD.Admin.ETL.ETLViewerTreeTab = Ext.extend(Ext.Panel, {
                     dataIndex: 'value',
                     width: 500
                 }
-            ]
+            ],
+            updateColumnWidths : function() {
+                Ext.ux.tree.TreeGrid.updateColumnWidths();
+                this.fitColumns();
+            },
+
+            /**
+             * Ext.ux.TreeGrid doesn't honor `autoExpandColumn` by default as it's based on TreePanel and not GridPanel.
+             * This function is what allows the column identified by the `autoExpandColumn` property to expand into
+             * whatever space is left over in the this components container.
+             */
+            fitColumns: function() {
+                var totalColWidth = this.getTotalColumnWidth(),
+                    outerCtWidth = this.outerCt.getWidth(),
+                    groups = this.outerCt.query('colgroup'),
+                    extraWidth = outerCtWidth - totalColWidth - Ext.getScrollBarWidth(),
+                    i, j;
+
+                for ( i = 0; i < this.columns.length; i++) {
+                    var column = this.columns[i];
+                    if (column.dataIndex === this.autoExpandColumn && extraWidth !== 0) {
+                        column.width = extraWidth;
+                        for ( j = 0; j < groups.length; j++) {
+                            g = groups[j];
+                            g.childNodes[i].style.width = extraWidth + 'px';
+                        }
+                    }
+                    this.updateColumnWidths();
+                }
+            }
         });
 
         Ext.apply(this, {
