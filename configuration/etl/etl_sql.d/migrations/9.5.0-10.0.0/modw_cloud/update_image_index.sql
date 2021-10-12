328b30190fad23af30261d99aa744faf50ce4e3c
Greg Dean <gmdean@buffalo.edu>
2021-10-12 08:57:27 -0400

    removing unneeded changes and renaming temporary indexes used in upgrade

--- configuration/etl/etl_sql.d/migrations/9.5.0-10.0.0/modw_cloud/update_image_index.sql
+++ configuration/etl/etl_sql.d/migrations/9.5.0-10.0.0/modw_cloud/update_image_index.sql
@@ -8,8 +8,8 @@ ALTER TABLE modw_cloud.image MODIFY image_id INT NOT NULL;
 ALTER TABLE modw_cloud.image DROP INDEX autoincrement_key;
 ALTER TABLE modw_cloud.image ADD COLUMN new_image_id INT(11) UNSIGNED NOT NULL auto_increment unique;
 ALTER TABLE modw_cloud.instance_data DROP PRIMARY KEY;
-CREATE INDEX host_resource_idx ON modw_cloud.image (image_id, resource_id);
-CREATE INDEX host_resource_idx ON modw_cloud.instance_data (image_id, resource_id);
+CREATE INDEX image_resource_idx ON modw_cloud.image (image_id, resource_id);
+CREATE INDEX image_resource_idx ON modw_cloud.instance_data (image_id, resource_id);
 
 UPDATE
   modw_cloud.instance_data as id
@@ -23,7 +23,7 @@ SET
 ALTER TABLE modw_cloud.image DROP COLUMN image_id;
 ALTER TABLE modw_cloud.image CHANGE new_image_id image_id INT(11) UNSIGNED;
 ALTER TABLE modw_cloud.instance_data ADD PRIMARY KEY(resource_id, event_id);
-DROP INDEX host_resource_idx ON modw_cloud.image;
-DROP INDEX host_resource_idx ON modw_cloud.instance_data;
+DROP INDEX image_resource_idx ON modw_cloud.image;
+DROP INDEX image_resource_idx ON modw_cloud.instance_data;
 
 UNLOCK TABLES;
