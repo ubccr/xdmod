0f50cf27b8a4b417a6008eedc77b27287b7f3645
Greg Dean <gmdean@buffalo.edu>
2021-10-19 11:59:15 -0400

    updating sql for upgrade and component tests to use host_id instead of hostname

--- configuration/etl/etl_sql.d/migrations/9.5.0-10.0.0/modw_cloud/update_cloud_resource_specs_host.sql
+++ configuration/etl/etl_sql.d/migrations/9.5.0-10.0.0/modw_cloud/update_cloud_resource_specs_host.sql
@@ -1,5 +1,6 @@
 ALTER TABLE modw_cloud.cloud_resource_specs MODIFY host_id INT(11) NOT NULL;
 ALTER TABLE modw_cloud.cloud_resource_specs DROP INDEX autoincrement_key;
+ALTER TABLE modw_cloud.staging_resource_specifications ADD COLUMN host_id INT(11) NOT NULL;
 
 -- Any hostnames in the cloud_resource_specs table that do not exist in
 -- `modw_cloud`.`host` should be added to that table
@@ -15,6 +16,15 @@ ON DUPLICATE KEY UPDATE
   resource_id = VALUES(resource_id), hostname = VALUES(hostname);
 
 -- Update the new host_id field to a value that is in `modw_cloud`.`host`
+UPDATE
+  `modw_cloud`.`staging_resource_specifications` AS srs
+JOIN
+  `modw_cloud`.`host` as h
+ON
+  h.hostname = srs.hostname and h.resource_id = srs.resource_id
+SET
+  srs.host_id = h.host_id;
+
 UPDATE
   `modw_cloud`.`cloud_resource_specs` AS rs
 JOIN
