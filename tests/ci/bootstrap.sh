aff07d40706c3b4654583ae32580c441f6be90b1
ryanrath <ryanrath@buffalo.edu>
2022-02-02 12:43:19 -0500

    Temporary Fix for Centos8 repo change (#1605)
    Redhat moved the centos8 repos to vault today. These changes allow our
    Centos8 builds to work while we move towards supporting Rocky.
    
    Co-authored-by: Ryan Rathsam <build@shippable.com>

--- tests/ci/bootstrap.sh
+++ tests/ci/bootstrap.sh
@@ -22,6 +22,16 @@ cp -r $REF_SOURCE /var/tmp/
 set -e
 set -o pipefail
 
+# Detect what version of CentOS we're running in, if it's 8 then we need to do some rejiggering of the yum repos to
+# make things work. From: https://stackoverflow.com/questions/70926799/centos-through-vm-no-urls-in-mirrorlist
+OS_VERSION=$(grep VERSION_ID < /etc/os-release  | cut -d"=" -f 2 | tr -d '"')
+case "$OS_VERSION" in
+    "8")
+        sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
+        sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
+        ;;
+esac
+
 if [ "$XDMOD_TEST_MODE" = "fresh_install" ];
 then
     rpm -qa | grep ^xdmod | xargs yum -y remove || true
