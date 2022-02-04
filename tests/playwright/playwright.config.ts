aabdc6181ee742745becb28bb3c7e2809b267ffe
Ryan Rathsam <build@shippable.com>
2022-02-04 07:09:14 -0500

    UI to Playwright Test Migration
    These changes migrate our current UI test framework from webdriverio to
    playwright. The reasons for the migration include but are not limited to:
    - During testing Playwright proved to be more resilient to timining issues than
      webdriverio. This should mean less failures of CI builds due to timing issues
      which means less time dealt with timing issue failures during CI builds.
    - Playwright supports parallel test execution so CI builds end up taking
      significantly less time.
    
    The original ui tests in `tests/ui/*` have been removed as they are no longer
    needed.

---
+++ tests/playwright/playwright.config.ts
@@ -0,0 +1,33 @@
+import {PlaywrightTestConfig, devices} from '@playwright/test';
+const config: PlaywrightTestConfig = {
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    timeout: 50000,
+    workers: 5,
+    use: {
+        trace: 'on-first-retry',
+        video: 'on-first-retry',
+        screenshot: 'only-on-failure',
+        ignoreHTTPSErrors: true,
+        viewport: {width: 2560, height: 1600},
+        baseURL: process.env.BASE_URL,
+        sso: process.env.SSO ? true : false,
+        timeout: 15000
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: {...devices['Desktop Chrome']}
+        },
+        /*{
+            name: 'firefox',
+            use: {...devices['Desktop Firefox']}
+        },
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] }
+        }*/
+    ]
+};
+
+export default config;
