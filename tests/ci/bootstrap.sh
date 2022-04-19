eee35835799cf33c35c941c82e84be9aba71e589
Joe White <jpwhite4@buffalo.edu>
2022-04-19 10:46:34 -0400

    Centos 8

--- tests/ci/bootstrap.sh
+++ tests/ci/bootstrap.sh
@@ -31,7 +31,7 @@ case "$OS_VERSION" in
     "8")
         sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Linux-*
         sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-Linux-*
-        PYTHON_SCIPY=python-scipy
+        PYTHON_SCIPY=python3-scipy
         ;;
 esac
 
