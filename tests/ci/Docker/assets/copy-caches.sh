1b836e4ba091a470c80d824042baf21b547de201
Ryan Rathsam <build@shippable.com>
2022-05-05 14:41:56 -0400

    Reverting copy-caches

--- tests/ci/Docker/assets/copy-caches.sh
+++ tests/ci/Docker/assets/copy-caches.sh
@@ -9,10 +9,10 @@ if [[ ! -f $ASSETS/browser-tests-node-modules.tar.gz ]]; then
 fi
 mv $ASSETS/browser-tests-node-modules.tar.gz /root
 
+# Copy optional assets.
 if [[ -f $ASSETS/chromedriver_linux64.zip ]]; then
     mv $ASSETS/chromedriver_linux64.zip /root
 fi
-
 if [[ -f $ASSETS/saml-idp.tar.gz ]]; then
     mv $ASSETS/saml-idp.tar.gz /root
 fi
