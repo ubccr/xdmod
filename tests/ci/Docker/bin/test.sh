109e73025809643b534196294790468619fcc148
lagao <lagao@buffalo.edu>
2022-08-15 15:32:20 -0400

    Refactor CircleCI config file for docker compose  - Split executors to individual yml files and include docker-compose.yml  - Split Installation and Test processes

---
+++ tests/ci/Docker/bin/test.sh
@@ -0,0 +1,40 @@
+#!bin/bash
+#Setup & Run QA Tests
+./tests/ci/scripts/qa-test-setup.sh
+
+#Make sure that the Composer Test Depedencies are installed
+composer install --no-progress
+
+./tests/regression/runtests.sh
+
+#Setup Configuration Files for Integration Tests
+mv ./configuration/organization.json ./configuration/organization.json.old
+mv ./configuration/portal_settings.ini ./configuration/portal_settings.ini.old
+cp /etc/xdmod/portal_settings.ini ./configuration/portal_settings.ini
+cp /etc/xdmod/organization.json ./configuration/organization.json
+
+./tests/integration/runtests.sh --junit-output-dir ~/phpunit
+./tests/regression/post_ingest_test.sh --junit-output-dir ~/phpunit
+./tests/component/runtests.sh --junit-output-dir ~/phpunit
+
+if [ $1 = 'centos7' ]; then
+  # Centos7 specific:
+  scl enable rh-nodejs6 "./tests/ui/runtests.sh --headless --log-junit ~/phpunit"
+
+  # Run SSO Tests
+  scl enable rh-nodejs6 "./tests/ci/samlSetup.sh"
+  scl enable rh-nodejs6 "./tests/ui/runtests.sh --headless --log-junit ~/phpunit --sso"
+  ./vendor/phpunit/phpunit/phpunit -c ./tests/integration/phpunit.xml.dist --testsuite sso --log-junit ~/phpunit/xdmod-sso-integration.xml
+else
+  # Centos8/Rocky8 specific:
+  pushd ./tests/ui && npm install && popd
+  ./tests/ui/runtests.sh --headless --log-junit ~/phpunit
+
+  # Run SSO Tests:
+  ./tests/ci/samlSetup.sh
+  ./tests/ui/runtests.sh --headless --log-junit ~/phpunit --sso
+  ./vendor/phpunit/phpunit/phpunit -c ./tests/integration/phpunit.xml.dist --testsuite sso --log-junit ~/phpunit/xdmod-sso-integration.xml
+fi
+
+# Ensure that no unexpected Apache errors were gathered
+test `fgrep -v ' [ssl:warn] ' /var/log/xdmod/apache-error.log | wc -l` = 0
