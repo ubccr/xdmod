8dc4b3dd340a297efa29fb330af3d3f54c4b64ac
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-03-27 12:02:08 -0400

    Switch to using `'exact'` validation
    except where a schema is needed for string pattern matching
    (e.g., tokens, dates).

--- tests/integration/lib/Controllers/UserControllerProviderTest.php
+++ tests/integration/lib/Controllers/UserControllerProviderTest.php
@@ -57,46 +57,17 @@ class UserControllerProviderTest extends BaseUserAdminTest
     {
         if ('pub' === $role) {
             foreach (array('get', 'create', 'revoke') as $action) {
-                $this->makeTokenRequest(
-                    $action,
-                    401,
-                    'authentication_error.spec'
-                );
+                $this->makeTokenRequest($action, 401, 'authentication_error');
             }
         } else {
             $this->helper->authenticate($role);
             $this->helper->delete('rest/users/current/api/token');
-            $this->makeTokenRequest(
-                'get',
-                200,
-                'get_user_token_failure.spec'
-            );
-            $this->makeTokenRequest(
-                'create',
-                200,
-                'create_user_token_success.spec'
-            );
-            $this->makeTokenRequest(
-                'create',
-                200,
-                'create_user_token_failure.spec'
-            );
-            $this->makeTokenRequest(
-                'get',
-                200,
-                'get_user_token_success.spec',
-                'schema'
-            );
-            $this->makeTokenRequest(
-                'revoke',
-                200,
-                'revoke_user_token_success.spec'
-            );
-            $this->makeTokenRequest(
-                'revoke',
-                200,
-                'revoke_user_token_failure.spec'
-            );
+            $this->makeTokenRequest('get', 200, 'get_failure');
+            $this->makeTokenRequest('create', 200, 'create_success', 'schema');
+            $this->makeTokenRequest('create', 200, 'create_failure');
+            $this->makeTokenRequest('get', 200, 'get_success', 'schema');
+            $this->makeTokenRequest('revoke', 200, 'revoke_success');
+            $this->makeTokenRequest('revoke', 200, 'revoke_failure');
             $this->helper->logout();
         }
     }
@@ -160,15 +131,9 @@ class UserControllerProviderTest extends BaseUserAdminTest
         $expected = $options->expected;
         $test = $options->test;
 
-        if ('pub' === $role) {
-            $failureHttpCode = array(401, 400);
-            $failureContentType = 'application/json';
-            $failureSchema = 'authentication_error.spec';
-        } else {
-            $failureHttpCode = $expected->test->failure->http_code;
-            $failureContentType = $expected->test->failure->content_type;
-            $failureSchema = $expected->test->failure->schema;
-        }
+        $failureHttpCode = $expected->test->failure->http_code;
+        $failureContentType = $expected->test->failure->content_type;
+        $failureOutputFileName = $expected->test->failure->file_name;
 
         // Attempt to make a request to the controller endpoint unauthenticated in any way.
         // This should fail.
@@ -181,8 +146,8 @@ class UserControllerProviderTest extends BaseUserAdminTest
             $failureHttpCode,
             $failureContentType,
             'integration/rest/user/api_token',
-            $failureSchema,
-            'schema'
+            $failureOutputFileName,
+            'exact'
         );
 
         // Now go ahead and authenticate the test user so we can create / use their API Token.
@@ -194,7 +159,8 @@ class UserControllerProviderTest extends BaseUserAdminTest
             $tokenResponse = $this->makeTokenRequest(
                 'create',
                 200,
-                'create_user_token_success.spec'
+                'create_success',
+                'schema'
             );
 
             $token = $tokenResponse->data->token;
@@ -227,11 +193,7 @@ class UserControllerProviderTest extends BaseUserAdminTest
             // clean up the helper's headers.
             $this->helper->addheader('Authorization', null);
 
-            $this->makeTokenRequest(
-                'revoke',
-                200,
-                'revoke_user_token_success.spec'
-            );
+            $this->makeTokenRequest('revoke', 200, 'revoke_success');
 
             // And finally make sure that we log out.
             $this->helper->logout();
@@ -281,7 +243,7 @@ class UserControllerProviderTest extends BaseUserAdminTest
         $action,
         $httpCode,
         $outputFileName,
-        $validationType = 'schema'
+        $validationType = 'exact'
     ) {
         if ('get' === $action) {
             $verb = 'get';
@@ -299,7 +261,7 @@ class UserControllerProviderTest extends BaseUserAdminTest
             $httpCode,
             'application/json',
             'integration/rest/user/api_token',
-            $outputFileName,
+            $outputFileName . ($validationType === 'schema' ? '.spec' : ''),
             $validationType
         );
     }
