821b73156e045957c0376fd188846cca5869153c
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-03-27 16:20:36 -0400

    Create method in `TokenHelper`
    to run standard endpoint token authentication tests: empty token,
    malformed token, invalid token, valid token, expired token,
    and deleted token. This improves the test coverage of the
    `Tokens::authenticateToken()` method. The commit also changes
    `expireToken` to set a date in the past rather than the current
    time, since a test could run fast enough that the difference
    between the "current" and "past" times was insignificant.

--- tests/integration/lib/Controllers/UserControllerProviderTest.php
+++ tests/integration/lib/Controllers/UserControllerProviderTest.php
@@ -77,11 +77,11 @@ class UserControllerProviderTest extends BaseUserAdminTest
      * `html/controllers/metric_explorer.php?operation=get_dw_descripter`.
      *
      * @dataProvider provideControllerTokenTest
-     * @param stdClass $options
+     * @param array $options
      * @return void
      * @throws Exception
      */
-    public function testControllerTokenAuthentication($role, stdClass $options)
+    public function testControllerTokenAuthentication($role, array $options)
     {
         $this->endpointTokenAuthenticationTest($role, $options);
     }
@@ -92,112 +92,38 @@ class UserControllerProviderTest extends BaseUserAdminTest
      *
      * @dataProvider provideRestTokenTest
      *
-     * @param stdClass $options
+     * @param array $options
      * @return void
      * @throws Exception
      */
-    public function testRestTokenAuthentication($role, stdClass $options)
+    public function testRestTokenAuthentication($role, array $options)
     {
         $this->endpointTokenAuthenticationTest($role, $options);
     }
 
-    /**
-     * A helper function that will test the end point provided with $options for it's ability to support API Token
-     * authentication. Currently API Token authentication is supported for both controllers/* and Rest endpoints. This
-     * is accomplished by a user logging into XDMoD normally and generating an API Token. Then, when making a request to
-     * and endpoint that supports API Token authentication they include this token as a request header
-     * 'Bearer: <token_value>`.
-     *
-     * The steps of this test are as follows:
-     *   - Attempt a request to the endpoint located at $options->test->url before authorization. This is expected to fail.
-     *   - If we're not testing the public user then log the test user.
-     *   - Create a new API token for the currently logged in user.
-     *   - Add the newly created token to $this->helpers->headers so that it will be included when making requests.
-     *   - Attempt a request to the test endpoint again this time w/ the token in the headers.
-     *   - Verify that the results of the API Token authenticated request are as expected.
-     *   - Begin cleanup by clearing the API Token from the helpers headers.
-     *   - Revoke the previously created API Token.
-     *   - and finally, if the test user is not the public user then log them out.
-     *
-     * @param string $role
-     * @param stdclass $options
-     *
-     * @return void
-     *
-     * @throws Exception
-     */
-    private function endpointTokenAuthenticationTest($role, stdclass $options)
+    private function endpointTokenAuthenticationTest($role, array $options)
     {
-        $expected = $options->expected;
-        $test = $options->test;
-
-        $failureHttpCode = $expected->test->failure->http_code;
-        $failureContentType = $expected->test->failure->content_type;
-        $failureOutputFileName = $expected->test->failure->file_name;
-
-        // Attempt to make a request to the controller endpoint unauthenticated in any way.
-        // This should fail.
-        $this->makeRequest(
+        $tokenHelper = new TokenHelper(
+            $this,
             $this->helper,
-            $test->url,
-            $test->verb,
-            $test->parameters,
-            $test->data,
-            $failureHttpCode,
-            $failureContentType,
-            'integration/rest/user/api_token',
-            $failureOutputFileName,
-            'exact'
+            $role,
+            $options['test']['url'],
+            $options['test']['verb'],
+            $options['test']['parameters'],
+            $options['test']['data'],
+            $options['expected']['test']['failure']['http_code'],
+            $options['expected']['test']['failure']['file_name']
         );
-
-        // Now go ahead and authenticate the test user so we can create / use their API Token.
-        if ('pub' !== $role) {
-            $this->helper->authenticate($role);
-
-            $this->helper->delete('rest/users/current/api/token');
-
-            $tokenResponse = $this->makeTokenRequest(
-                'create',
-                200,
-                'create_success',
-                'schema'
-            );
-
-            $token = $tokenResponse->data->token;
-            // We add the token to the request headers so that we can use the token authentication.
-            $this->helper->addheader('Authorization', sprintf('%s %s', Tokens::HEADER_KEY, $token));
-
-            // While we prefer the 'Authorization' header approach to token authentication. We also support having it
-            // provided as a query parameter. Also, retrieving the 'Authorization' header on el7 isn't working, so
-            // this is the only way to test authentication.
-            if (!property_exists($test, 'parameters') || !isset($test->parameters)) {
-                $test->parameters = array();
-            } elseif (is_object($test->parameters)) {
-                $test->parameters = (array)$test->parameters;
+        $tokenHelper->runEndpointTests(
+            function ($token) use ($tokenHelper, $options) {
+                $tokenHelper->runEndpointTest(
+                    $token,
+                    $options['expected']['test']['success']['http_code'],
+                    $options['expected']['test']['success']['file_group'],
+                    $options['expected']['test']['success']['file_name']
+                );
             }
-
-            $test->parameters[Tokens::HEADER_KEY] = $token;
-            $this->makeRequest(
-                $this->helper,
-                $test->url,
-                $test->verb,
-                $test->parameters,
-                $test->data,
-                $expected->test->success->http_code,
-                $expected->test->success->content_type,
-                $expected->test->success->file_group,
-                $expected->test->success->file_name,
-                'exact'
-            );
-
-            // clean up the helper's headers.
-            $this->helper->addheader('Authorization', null);
-
-            $this->makeTokenRequest('revoke', 200, 'revoke_success');
-
-            // And finally make sure that we log out.
-            $this->helper->logout();
-        }
+        );
     }
 
     /**
@@ -229,7 +155,7 @@ class UserControllerProviderTest extends BaseUserAdminTest
                 $fileName,
                 'input'
             ),
-            false
+            true
         );
         return array_map(
             function ($roleArray) use ($json) {
--- tests/integration/lib/TestHarness/TokenHelper.php
+++ tests/integration/lib/TestHarness/TokenHelper.php
@@ -3,29 +3,129 @@
 namespace TestHarness;
 
 use CCR\DB;
+use Models\Services\Tokens;
 
 /**
  *
  */
-abstract class TokenHelper
+class TokenHelper
 {
+    private $testInstance;
+    private $testHelper;
+    private $role;
+    private $path;
+    private $verb;
+    private $params;
+    private $data;
+    private $errorHttpCode;
+    private $errorOutputFileName;
+
+    public function __construct(
+        $testInstance,
+        $testHelper,
+        $role,
+        $path,
+        $verb,
+        $params,
+        $data,
+        $errorHttpCode,
+        $errorOutputFileName
+    ) {
+        $this->testInstance = $testInstance;
+        $this->testHelper = $testHelper;
+        $this->role = $role;
+        $this->path = $path;
+        $this->verb = $verb;
+        $this->params = $params;
+        $this->data = $data;
+        $this->errorHttpCode = $errorHttpCode;
+        $this->errorOutputFileName = $errorOutputFileName;
+    }
+
+    public function runEndpointTests($callback)
+    {
+        if ('pub' === $this->role) {
+            self::runEndpointTest('');
+            self::runEndpointTest('asdf');
+        } else {
+            $this->testHelper->authenticate($this->role);
+            $this->testHelper->delete('rest/users/current/api/token');
+            $response = $this->testHelper->post(
+                'rest/users/current/api/token',
+                null,
+                null
+            );
+            $token = $response[0]['data']['token'];
+            $userId = substr($token, 0, strpos($token, Tokens::DELIMITER));
+            $this->testHelper->logout();
+            self::runEndpointTest($userId . Tokens::DELIMITER . 'asdf');
+            $callback($token);
+            self::expireToken($userId);
+            self::runEndpointTest($token);
+            $this->testHelper->authenticate($this->role);
+            $this->testHelper->delete('rest/users/current/api/token');
+            $this->testHelper->logout();
+            self::runEndpointTest($token);
+        }
+    }
+
+    public function runEndpointTest(
+        $token,
+        $httpCode = null,
+        $outputTestGroup = 'integration/rest/user/api_token',
+        $outputFileName = null
+    ) {
+        if (null === $httpCode) {
+            $httpCode = $this->errorHttpCode;
+        }
+        if (null === $outputFileName) {
+            $outputFileName = $this->errorOutputFileName;
+        }
+        $authHeader = $this->testHelper->getheader('Authorization');
+        $this->testHelper->addheader(
+            'Authorization',
+            Tokens::HEADER_KEY . ' ' . $token
+        );
+        if (null === $this->params) {
+            $this->params = array();
+        }
+        $this->params[Tokens::HEADER_KEY] = $token;
+        $this->testInstance->makeRequest(
+            $this->testHelper,
+            $this->path,
+            $this->verb,
+            $this->params,
+            $this->data,
+            $httpCode,
+            'application/json',
+            $outputTestGroup,
+            $outputFileName,
+            'exact'
+        );
+        unset($this->params[Tokens::HEADER_KEY]);
+        $this->testHelper->addheader('Authorization', $authHeader);
+    }
+
     /**
      * A helper function that will allow us to test the expiration of a token.
      *
-     * Note: We need to directly access the database as we do not have an endpoint for expiring
-     * a token.
+     * Note: We need to directly access the database as we do not have an
+     * endpoint for expiring a token.
      *
      * @param string $userId the userId whose token should be expired.
      *
      * @return bool true if the token was successfully expired.
      *
-     * @throws Exception if there is a problem parsing the the provided $rawToken.
-     * @throws Exception if there is a problem connecting to or executing the update statement against the database.
+     * @throws Exception if there is a problem parsing the the provided
+     *                   $rawToken.
+     * @throws Exception if there is a problem connecting to or executing the
+     *                   update statement against the database.
      */
     public static function expireToken($userId)
     {
         $db = DB::factory('database');
-        $query = 'UPDATE moddb.user_tokens SET expires_on = NOW() WHERE user_id = :user_id';
+        $query = 'UPDATE moddb.user_tokens SET expires_on = SUBDATE(NOW(), 1)'
+            . ' WHERE user_id = :user_id';
         $params = array(':user_id' => $userId);
         return $db->execute($query, $params) === 1;
     }
