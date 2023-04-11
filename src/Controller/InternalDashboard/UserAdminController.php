9fd9d704c482218a4399fec96fc49d0b38c8e8ce
Ryan Rathsam <ryanrath@buffalo.edu>
2023-04-11 15:07:54 -0400

    Fixing a problem w/ a leading space being present

--- src/Controller/InternalDashboard/UserAdminController.php
+++ src/Controller/InternalDashboard/UserAdminController.php
@@ -400,7 +400,7 @@ class UserAdminController extends BaseController
         return $this->json([
             'success'   => true,
             'status'    => sprintf(
-                '%s User <b>%s</b> updated successfully',
+                '%sUser <b>%s</b> updated successfully',
                 $userToUpdate->isSSOUser() ? 'Single Sine On' : '',
                 $userName
             ),
