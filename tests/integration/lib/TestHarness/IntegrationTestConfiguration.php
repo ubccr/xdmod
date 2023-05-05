d20e269db3acfaac32b87447d129ec0986da9835
Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
2023-05-05 13:31:26 -0400

    Refactor based on testing other PRs:
    - Allow `runTokenAuthTests()` to run multiple successful tests as
    defined in JSON input/output test artifacts.
    - When running a `runTokenAuthTest()`, make one request with
    the token in the header and one with it only in the query
    parameters to ensure the result is the same.
    - Instead of transforming fragment-only references, just ignore
    them and let the JSON schema validator handle them.
    - Add `"$id"` keys to the JSON schemas so that the validator
    can properly process `"$ref"` keys if the object is in an `allOf`
    statement.
    - Rename `success.json` to `success_body.json` to be
    consistent with other names.
    - For test assertion messages, include both the path to where the
    JSON artifact is defined and the (truncated) text of the artifact
    itself.

--- tests/integration/lib/TestHarness/IntegrationTestConfiguration.php
+++ tests/integration/lib/TestHarness/IntegrationTestConfiguration.php
@@ -12,12 +12,10 @@ use TestHarness\TestFiles;
  * Read and parse a JSON configuration file, replacing the string
  * "${INTEGRATION_ROOT}" with the path to the integration test artifacts root
  * directory (e.g., tests/artifacts/xdmod/integration), and using
- * @see JsonSchemaAnchorReferenceTransformer.
+ * JsonFragmentOnlyReferenceTransformer.
  */
 class IntegrationTestConfiguration extends Configuration
 {
-    private $numSchemasSeen = 0;
-
     /**
      * Load a JSON configuration file into an associative array, replacing the
      * string "${INTEGRATION_ROOT}" with the path to the integration test
@@ -48,44 +46,14 @@ class IntegrationTestConfiguration extends Configuration
     }
 
     /**
-     * @return bool true if we are currently inside a schema while running
-     * @see processKeyTransformers().
-     */
-    public function inSchema()
-    {
-        return $this->numSchemasSeen > 0;
-    }
-
-    /**
-     * Add the @see JsonSchemaAnchorReferenceTransformer.
+     * @see Configuration::preTransformTasks()
      */
     protected function preTransformTasks()
     {
         $this->addKeyTransformer(
-            new JsonSchemaAnchorReferenceTransformer($this->logger)
+            new JsonFragmentOnlyReferenceTransformer($this->logger)
         );
         parent::preTransformTasks();
         return $this;
-    }
-
-    /**
-     * If the given object has a '$schema' property, increment the number of
-     * schemas seen before transforming it, so that the
-     * @see JsonSchemaAnchorReferenceTransformer can do its transformations,
-     * and then decrement the number of schemas seen after transforming it.
-     *
-     * @param stdClass $obj the object to transform.
-     * @return stdClass the transformed object.
-     */
-    protected function processKeyTransformers(stdClass $obj)
-    {
-        if (property_exists($obj, '$schema')) {
-            $this->numSchemasSeen++;
-        }
-        $obj = parent::processKeyTransformers($obj);
-        if (property_exists($obj, '$schema')) {
-            $this->numSchemasSeen--;
-        }
-        return $obj;
     }
 }
