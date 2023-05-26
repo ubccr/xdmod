6b80737b08a547737513fef9817e4d65aa803cba
lagao <lagao@buffalo.edu>
2023-05-26 16:36:48 -0400

    Resolve reportGen fail attempt1

--- tests/playwright/tests/reportGenerator/reportGenerator.spec.ts
+++ tests/playwright/tests/reportGenerator/reportGenerator.spec.ts
@@ -951,7 +951,9 @@ test.describe('Report Generator', async () => {
             await loginPage.login('centerdirector', 'centerdirector', 'Reed Bunting');
             await reportGeneratorPage.selectTab();
             await test.step('Select a report', async () => {
-                const report:MyReportsRow = (await reportGeneratorPage.getMyReportsRows())[0];
+                const rows = await reportGeneratorPage.getMyReportsRows();
+                const report:MyReportsRow = rows[0];
+                //const report:MyReportsRow = (await reportGeneratorPage.getMyReportsRows())[0];
                 await report.click();
             });
             await test.step('Preview selected report', async () => {
