434226b0c7a03d7664d89f036fbf22ee343b11a4
Joseph White <jpwhite4@buffalo.edu>
2023-05-26 12:55:24 -0400

    Add a couple more tests.

--- tests/integration/lib/Controllers/MetricExplorerTest.php
+++ tests/integration/lib/Controllers/MetricExplorerTest.php
@@ -18,6 +18,61 @@ class MetricExplorerTest extends TokenAuthTest
     }
 
     /**
+     * Check for correct handling of invalid or missing input parameters to the
+     * metric explorer charting controller
+     */
+    public function testInvalidChartRequests() {
+
+        $params = array(
+            "show_title" => "y",
+            "timeseries" => "y",
+            "aggregation_unit" => "Auto",
+            "start_date" => "2016-12-29",
+            "end_date" => "2017-01-01",
+            "global_filters" => "%7B%22data%22%3A%5B%5D%2C%22total%22%3A0%7D",
+            "title" => "untitled query 1",
+            "show_filters" => "true",
+            "show_warnings" => "true",
+            "show_remainder" => "false",
+            "start" => "0",
+            "limit" => "10",
+            "timeframe_label" => "User Defined",
+            "operation" => "get_data",
+            "data_series" => "%5B%7B%22group_by%22%3A%22none%22%2C%22color%22%3A%22auto%22%2C%22log_scale%22%3Afalse%2C%22std_err%22%3Afalse%2C%22value_labels%22%3Afalse%2C%22display_type%22%3A%22line%22%2C%22combine_type%22%3A%22side%22%2C%22sort_type%22%3A%22value_desc%22%2C%22ignore_global%22%3Afalse%2C%22long_legend%22%3Atrue%2C%22x_axis%22%3Afalse%2C%22has_std_err%22%3Afalse%2C%22trend_line%22%3Afalse%2C%22line_type%22%3A%22Solid%22%2C%22line_width%22%3A2%2C%22shadow%22%3Afalse%2C%22filters%22%3A%7B%22data%22%3A%5B%5D%2C%22total%22%3A0%7D%2C%22z_index%22%3A0%2C%22visibility%22%3Anull%2C%22enabled%22%3Atrue%2C%22metric%22%3A%22job_count%22%2C%22realm%22%3A%22Jobs%22%2C%22category%22%3A%22Jobs%22%2C%22id%22%3A2627927508440901%7D%5D",
+            "swap_xy" => "false",
+            "share_y_axis" => "false",
+            "hide_tooltip" => "false",
+            "show_guide_lines" => "y",
+            "showContextMenu" => "y",
+            "scale" => "1",
+            "format" => "hc_jsonstore",
+            "width" => "1500",
+            "height" => "626",
+            "legend_type" => "bottom_center",
+            "font_size" => "3",
+            "featured" => "false",
+            "trendLineEnabled" => "",
+            "x_axis" => "%7B%7D",
+            "y_axis" => "%7B%7D",
+            "legend" => "%7B%7D",
+            "defaultDatasetConfig" => "%7B%7D",
+            "controller_module" => "metric_explorer"
+        );
+
+        $this->helper->authenticate('cd');
+
+        unset($params['font_size']);
+
+        $response = $this->helper->post('/controllers/metric_explorer.php', null, $params);
+        $output = json_decode($response[0]);
+        $this->assertEquals($output->data[0]->title->style->fontSize, "19px");
+
+        $params['data_series'] = '[object Object]';
+        $response = $this->helper->post('/controllers/metric_explorer.php', null, $params);
+        $this->assertEquals(400, $response[1]['http_code']);
+    }
+
+    /**
      * @dataProvider provideTokenAuthTestData
      */
     public function testGetDwDescripterTokenAuth($role, $tokenType) {
