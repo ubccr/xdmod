b9eee0ea6ea363223b3aacf5841474c5b30dbe09
lagao <lagao@buffalo.edu>
2023-06-01 15:07:34 -0400

    Fix?

--- tests/playwright/lib/reportGenerator.page.ts
+++ tests/playwright/lib/reportGenerator.page.ts
@@ -470,6 +470,7 @@ export class ReportGenerator {
      */
     async waitForMyReportsPanelVisible() {
         await this.page.isVisible(this.selectors.myReports.panel());
+        await expect(this.page.locator(this.selectors.myReports.panel())).toBeVisible();
     }
 
     /**
@@ -594,9 +595,8 @@ export class ReportGenerator {
         await this.waitForMyReportsPanelVisible();
         const selector = this.selectors.myReports.reportList.rows();
         const computed = await this.page.$$(selector);
-        //const result =
-        return await Promise.all(computed.map(async(element, i) => new MyReportsRow(`(${selector})[${i + 1}]`, this.page)));
-        //return result;
+        let result = await Promise.all(computed.map(async(element, i) => new MyReportsRow(`(${selector})[${i + 1}]`, this.page)));
+        return result;
     }
 
     /**
@@ -752,17 +752,12 @@ export class ReportGenerator {
         // There are two separate "New Based On" buttons.  Only one should be
         // visible at a time.
         const buttons = await this.page.$$(this.selectors.myReports.toolbar.newBasedOnVisibleButton());
-        console.log(buttons);
         await expect(buttons.length, 'Two "New Based On" button are present').toEqual(2);
         const button1 = await this.page.locator(this.selectors.myReports.toolbar.numNewBasedOnVisibleButton(1));
         const button2 = await this.page.locator(this.selectors.myReports.toolbar.numNewBasedOnVisibleButton(2));
         if (await button1.isVisible()) {
-            console.log("button1 click");
-            console.log(button1)
             await button1.click();
         } else if (await button2.isVisible()){
-            console.log("button2.click");
-            console.log(button2);
             await button2.click();
         } else {
             throw new Error('There is no button');
