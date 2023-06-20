98a4e6598d71226959ada6dbf7b5a344685c0e7b
Andrew Stoltman <aestoltm@buffalo.edu>
2023-06-20 16:58:25 -0400

    Linting issues with plotly utility function.

--- html/gui/js/libraries/PlotlyUtilities.js
+++ html/gui/js/libraries/PlotlyUtilities.js
@@ -4,21 +4,20 @@
  * @param{dict} Record containing chart data
  *
  */
-function generateChartOptions (record) {
+function generateChartOptions(record) { // eslint-disable-line no-unused-vars
     var colors = ['#2f7ed8', '#0d233a', '#8bbc21', '#910000', '#1aadce', '#492970',
-    '#f28f43', '#77a1e5', '#c42525', '#a6c96a'];
+        '#f28f43', '#77a1e5', '#c42525', '#a6c96a'];
     var data = [];
     var isEnvelope = false;
     var tz = moment.tz.zone(record.data.schema.timezone).abbr(record.data.series[0].data[0].x);
-    var ymin, ymax;
-    ymin = record.data.series[0].data[0].y;
-    ymax = ymin;
+    var ymin = record.data.series[0].data[0].y;
+    var ymax = ymin;
     var sid = 0;
     if (record.data.series[sid].name === 'Range') {
-            sid++;
-            isEnvelope = true;
-            ymin = record.data.series[1].data[0].y;
-            ymax = ymin;
+        sid++;
+        isEnvelope = true;
+        ymin = record.data.series[1].data[0].y;
+        ymax = ymin;
     }
     for (sid; sid < record.data.series.length; sid++) {
         var x = [];
@@ -26,7 +25,7 @@ function generateChartOptions (record) {
         var qtip = [];
         var color = colors[sid % 10];
 
-        for(var i = 0; i < record.data.series[sid].data.length; i++) {
+        for (var i = 0; i < record.data.series[sid].data.length; i++) {
             x.push(moment.tz(record.data.series[sid].data[i].x, record.data.schema.timezone).format('Y-MM-DD HH:mm:ss.SSS'));
             y.push(record.data.series[sid].data[i].y);
             qtip.push(record.data.series[sid].data[i].qtip);
@@ -44,25 +43,25 @@ function generateChartOptions (record) {
                 color: color
             },
             text: qtip,
-            hovertemplate:
-                "%{x|%A, %b %e, %H:%M:%S.%L} " + tz + "<br>" +
-                "<span style='color:"+color+";'>●</span> " + record.data.series[sid].name + ": <b>%{y:}</b>" +
-                "<extra></extra>",
-            name: record.data.series[sid].name, chartSeries: record.data.series[sid], type: 'scatter', mode: 'markers+lines'
+            hovertemplate: '%{x|%A, %b %e, %H:%M:%S.%L} ' + tz + '<br> <span style=color:'
+            + color + ';>●</span> ' + record.data.series[sid].name + ': <b>%{y:}</b> <extra></extra>',
+            name: record.data.series[sid].name,
+            chartSeries: record.data.series[sid],
+            type: 'scatter',
+            mode: 'markers+lines'
         };
 
-        if (record.data.series[sid].name === "Median" || record.data.series[sid].name === "Minimum"){
+        if (record.data.series[sid].name === 'Median' || record.data.series[sid].name === 'Minimum') {
             trace['fill'] = 'tonexty';
             trace['fillcolor'] = '#5EA0E2';
         }
 
-        if (isEnvelope){
-            trace.hovertemplate = "<span style='color:"+color+";'>[%{text}]</span> " + "%{x|%A, %b %e, %H:%M:%S.%L} " + tz + "<br>" +
-                "<span style='color:"+color+";'>●</span> " + record.data.series[sid].name + ": <b>%{y:}</b>" +
+        if (isEnvelope) {
+            trace.hovertemplate = '<span style=color:'+color+';>[%{text}]</span> %{x|%A, %b %e, %H:%M:%S.%L} ' + tz + '<br> <span style=color:' + color + ';>●</span> ' + record.data.series[sid].name + ': <b>%{y:}</b>' +
                 "<extra></extra>";
         }
 
-        if (record.data.series[sid].data.length === 1){
+        if (record.data.series[sid].data.length === 1) {
             trace.marker.size = 20;
             trace.mode = 'markers';
             delete trace.line;
@@ -71,8 +70,12 @@ function generateChartOptions (record) {
         data.push(trace);
         var tempMin = Math.min.apply(null, y);
         var tempMax = Math.max.apply(null, y);
-        if (tempMin < ymin) ymin = tempMin;
-        if (tempMax > ymax) ymax = tempMax;
+        if (tempMin < ymin) {
+            ymin = tempMin;
+        }
+        if (tempMax > ymax) {
+            ymax = tempMax;
+        }
     }
 
     var layout = {
@@ -80,7 +83,7 @@ function generateChartOptions (record) {
             bgcolor: '#ffffff'
         },
         xaxis: {
-            title: '<b>' + 'Time (' + record.data.schema.timezone + ')' + '</b>',
+            title: '<b> Time (' + record.data.schema.timezone + ') </b>',
             titlefont: {
                 family: 'Open-Sans, verdana, arial, sans-serif',
                 size: 12,
@@ -91,7 +94,7 @@ function generateChartOptions (record) {
             tickcolor: '#c0cfe0',
             linecolor: '#c0cfe0',
             automargin: true,
-            showgrid: false,
+            showgrid: false
         },
         yaxis: {
             title: '<b>' + record.data.schema.units + '</b>',
@@ -116,7 +119,7 @@ function generateChartOptions (record) {
                 family: 'Lucida Grande, Lucida Sans Unicode, Arial, Helvetica, sans-serif',
                 color: '#444b6e',
                 size: 16
-            },
+            }
         },
         annotations: [{
             text: record.data.schema.source + '. Powered by XDMoD/Plotly',
