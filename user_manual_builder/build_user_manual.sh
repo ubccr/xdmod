9686b1bc4e8345730acb77bbeb42703832abc3c4
rmcclenahan <rmcclenahan@wesleyan.edu>
2023-07-17 14:45:53 -0400

    add new build process script

---
+++ user_manual_builder/build_user_manual.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# -------
+# build user manual from restructured text files
+# -------
+
+# Destination directory
+DEST_DIR=html/user_manual
+
+# Directory where manuals are built
+BASE_BUILD_DIR=user_manual_builder
+
+# Version of the manual (XDMoD version)
+MANUAL_VERSION=Open
+
+# ------
+# Parse command line arguments
+# ------
+
+ARGS=$(getopt -o "b:d:v:h" -l "builddir:,destdir:,version:,help" -n "build_user_manual" -- "$@");
+
+eval set -- "$ARGGS";
+
+while true; do
+	case "$1" in
+	-b|--builddir)
+		if [ -n "$2" ]; then
+			BASE_BUILD_DIR=$2
+			shift 2
+		fi
+		;;
+	-d|--destdir)
+		if [ -n "$2" ]; then
+			DEST_DIR=$2
+			shift 2
+		fi
+		;;
+	-v|--version)
+		if [ -n "$2" ]; then
+			MANUAL_VERSION=$2
+			shift 2
+		fi
+		;;
+	-h|--help)
+		echo "Usage: $0 \\" >&2
+		echo "  -v|--version manual_version : Manual version to be linked (open or xsede) [$MANUAL_VERSION] \\" >&2
+		echo "  [-b|--builddir dir] : Directory where the manual will be built [$BASE_BUILD_DIR]" >&2
+		echo "  [-d|--destdir dir] : Directory that the tarball will unpack into [$DEST_DIR]" >&2
+		echo "  [-h|--help] : Display this help" >&2
+		exit 1
+		;;
+	--) shift ; break ;;
+	esac
+done
+
+#
+# Verify arguments
+#
+
+if [ -z "$BASE_BUILD_DIR" -o -z "$DEST_DIR" -o -z "$MANUAL_VERSION" ]; then
+	echo "Must specify build_dir, dest_dir and manual_version" >&2 
+	exit 1
+elif [ ! -d "$BASE_BUILD_DIR" ]; then
+	mkdir -p $BASE_BUILD_DIR
+	if [ $? -ne 0 ]; then
+		echo "Error creating base build directory: '$BASE_BUILD_DIR'" >&2
+		exit 1
+	fi
+fi
+
+# ------
+# Build the manual
+# -----
+
+cp $DEST_DIR/index.rst.in $DEST_DIR/index.rst
+
+if [ "$MANUAL_VERSION" = "XSEDE" ]; then
+    sed -e "s/<XSEDE>//g" "$DEST_DIR/index.rst" 
+else
+    sed -e "/<XSEDE>/d" "$DEST_DIR/index.rst"
+fi
+
+sphinx-build -t $MANUAL_VERSION $BASE_BUILD_DIR $DEST_DIR
