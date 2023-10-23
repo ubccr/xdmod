780d3fb114d34b4aedd62e71598cc3c3540a7747
Ryan Rathsam <ryanrath@buffalo.edu>
2023-10-23 14:08:49 -0400

    Ghetto "fix" for sso login errors.
    Catches when we have a "user already exists" error and redirects to home instead
    of just JSON encoding the response.

--- src/Errors/ErrorController.php
+++ src/Errors/ErrorController.php
@@ -2,8 +2,11 @@
 
 namespace Access\Errors;
 
+use Symfony\Component\ErrorHandler\ErrorRenderer\ErrorRendererInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Throwable;
 use function xd_response\buildError;
 
@@ -12,6 +15,10 @@ use function xd_response\buildError;
  */
 class ErrorController extends \Symfony\Component\HttpKernel\Controller\ErrorController
 {
+    public function __construct(HttpKernelInterface $kernel, $controller, ErrorRendererInterface $errorRenderer)
+    {
+        parent::__construct($kernel, $controller, $errorRenderer);
+    }
 
     /**
      * Specifically designed to work with instances of FlattenException. We return a JsonResponse due to XDMoD expecting
@@ -25,6 +32,13 @@ class ErrorController extends \Symfony\Component\HttpKernel\Controller\ErrorCont
         $headers = [];
         if (method_exists($exception, 'getHeaders')) {
             $headers = $exception->getHeaders();
+        }
+
+        $message = $exception->getMessage();
+        $userPos = strpos($message, 'User');
+        $alreadyExistsPos = strpos($message, 'already exists');
+        if ($userPos && $alreadyExistsPos) {
+            return new RedirectResponse('/');
         }
 
         return new JsonResponse(buildError($exception), 200, $headers);
