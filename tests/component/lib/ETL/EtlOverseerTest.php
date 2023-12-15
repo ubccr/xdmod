4017635fc18bd8f72816bfe969ef2ffd45f6908d
Greg Dean <gmdean@buffalo.edu>
2023-12-15 09:01:29 -0500

    PHP8 Related Changes
    - acl-config
      - `join` function signature has changed in PHP8
    - RealmManager.php
      - The order in which these values were being returned differed between PHP7.2
        and PHP8.0. I've just added code to force the ordering to be as we expect.
    - ExportBuilder.php
      - PHP8 is more strict in what it allows in function signatures and optional
        arguments (arguments with default values) cannot come before required
        arguments (arguments without default values).
    - Visualization.php
      - HSVtoRGB: PHP7.2 did more coercing of types than PHP8 does so we needed an
        explicit cast to int here.
    - pdoAggregator.php
      - The previous code here expected that `current` would always return an
        `array` when it can also return a `bool`. I've just updated the code to take
        this into account.
    - HpcdbHostsIngestor.php
      - updated the function signature of `transform` so that it matches it's parent
        `pdoIngestor`
    - aRdbmsDestinationAction.php
      - This code previously assumed that `$item['alias']` was going to be an array
        and that it would contain a key 'as'. I just made it explicit.
    - composer.json
      - Added PHP8.0 to the list of supported PHP's.
      - Updated phpoffice/phpword's version to * because the previous version
        doesn't support PHP8
      - Updated PHPUnit to 9+ as it was the most recent version that supported
        PHP8.0
    
    PHPUnit9+ changes
    
    The "types" of changes in this commit are all related to PHPUnit9+ and PHP8:
    - PHP8 reports a larger mantissa than in PHP7.2 so a few values have been
    updated to reflect that.
    - PHPUnit9+ uses actual namespacing so anywhere there was
      `PHPUnit_Framework_TestCase` has been changed to
      `\PHPUnit\Framework\TestCase`. The same basic idea was applied to Errors and
      Exceptions as well.
    - PHPUnit9+ has added some new functions, deprecated others, and removed yet
      others. There are numerous locations where substitutions for curently
      recommended functions were made.
    - phpunit.xml.dist files have been updated
    
    Documentation Changes
    - The upgrade documentation has been upgraded to reflect the update of PHP to
      8.0. Upgrade scenarios include:
      - XDMoD 10.0 on EL7 w/ PHP 5.4
      - XDMoD 10.5 on EL7 w/ PHP 7.2
      - XDMoD 10.5 on EL8 w/ PHP 7.2
    
    Co-authored-by: Joe White <jpwhite4@buffalo.edu>

--- tests/component/lib/ETL/EtlOverseerTest.php
+++ tests/component/lib/ETL/EtlOverseerTest.php
@@ -79,6 +79,7 @@ class EtlOverseerTest extends \PHPUnit\Framework\TestCase
             self::$overseerOptions->setIncludeOnlyResourceCodes('resource1');
             $overseer = new EtlOverseer(self::$overseerOptions);
             $overseer->execute(self::$etlConfig);
+            $this->assertTrue(true);
         } catch ( Exception $e ) {
             $this->fail($e->getMessage());
         }
@@ -89,6 +90,7 @@ class EtlOverseerTest extends \PHPUnit\Framework\TestCase
             self::$overseerOptions->setIncludeOnlyResourceCodes(array('resource1', 'resource2'));
             $overseer = new EtlOverseer(self::$overseerOptions);
             $overseer->execute(self::$etlConfig);
+            $this->assertTrue(true);
         } catch ( Exception $e ) {
             $this->fail($e->getMessage());
         }
@@ -99,6 +101,7 @@ class EtlOverseerTest extends \PHPUnit\Framework\TestCase
             self::$overseerOptions->setExcludeResourceCodes('resource1');
             $overseer = new EtlOverseer(self::$overseerOptions);
             $overseer->execute(self::$etlConfig);
+            $this->assertTrue(true);
         } catch ( Exception $e ) {
             $this->fail($e->getMessage());
         }
@@ -109,6 +112,7 @@ class EtlOverseerTest extends \PHPUnit\Framework\TestCase
             self::$overseerOptions->setExcludeResourceCodes(array('resource1', 'resource2'));
             $overseer = new EtlOverseer(self::$overseerOptions);
             $overseer->execute(self::$etlConfig);
+            $this->assertTrue(true);
         } catch ( Exception $e ) {
             $this->fail($e->getMessage());
         }
