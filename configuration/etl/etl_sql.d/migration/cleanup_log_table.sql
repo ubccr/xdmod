a15c217a04def9bb0ae28cab8b64c0945154cab8
Ryan Rathsam <ryanrath@buffalo.edu>
2024-01-17 13:59:43 -0500

    blarg

--- configuration/etl/etl_sql.d/migration/cleanup_log_table.sql
+++ configuration/etl/etl_sql.d/migration/cleanup_log_table.sql
@@ -1,12 +1,12 @@
 /* Backfill a username value into log entries where we're able to find a valid SessionManager.token['user_id'] -> Users.id relationship. */
 UPDATE mod_logger.log_table as lt JOIN (SELECT
-                                            id,
-                                            message,
-                                            REPLACE(JSON_EXTRACT(message, '$.data.token'), '"', '') as token
-                                        FROM mod_logger.log_table
+                                            l2.id,
+                                            l2.message,
+                                            REPLACE(JSON_EXTRACT(l2.message, '$.data.token'), '"', '') as token
+                                        FROM mod_logger.log_table as l2
                                         WHERE
-                                              ident IN ('rest.logger.db', 'controller.log')
-                                          AND priority = 6) AS data JOIN moddb.SessionManager sm ON sm.session_token = data.token JOIN moddb.Users u ON u.id = sm.user_id
+                                              l2.ident IN ('rest.logger.db', 'controller.log')
+                                          AND l2.priority = 6) AS data JOIN moddb.SessionManager sm ON sm.session_token = data.token JOIN moddb.Users u ON u.id = sm.user_id
 SET lt.message = JSON_REPLACE(lt.message, '$.data.username', u.username)
 WHERE
       lt.ident IN ('rest.logger.db', 'controller.log')
