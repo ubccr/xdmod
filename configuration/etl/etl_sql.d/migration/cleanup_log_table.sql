8412f0817b8d58a017a8cfe6095484c8bd8dc355
Ryan Rathsam <ryanrath@buffalo.edu>
2024-01-17 13:19:53 -0500

    Updating username backfill SQL
    Just updating the username backfill SQL query to use the `JSON_EXTRACT`
    function as its more consistent than trying to use `INSTR/SUBSTR`.

--- configuration/etl/etl_sql.d/migration/cleanup_log_table.sql
+++ configuration/etl/etl_sql.d/migration/cleanup_log_table.sql
@@ -1,19 +1,17 @@
 /* Backfill a username value into log entries where we're able to find a valid SessionManager.token['user_id'] -> Users.id relationship. */
-UPDATE mod_logger.log_table as lt
-    JOIN (SELECT
-            id,
-            SUBSTR(message, INSTR(message, '"token":"') + 9,
-                   INSTR(SUBSTR(message, INSTR(message, '"token":"') + 9), '",') -
-                   1) as token
-        FROM mod_logger.log_table
-        WHERE ident IN ('rest.logger.db', 'controller.log') AND priority = 6) as data ON data.id = lt.id
-    JOIN moddb.SessionManager sm ON sm.session_token = data.token
-    JOIN moddb.Users u ON u.id = sm.user_id
+UPDATE mod_logger.log_table as lt JOIN (SELECT
+                                            id,
+                                            message,
+                                            REPLACE(JSON_EXTRACT(message, '$.data.token'), '"', '') as token
+                                        FROM mod_logger.log_table
+                                        WHERE
+                                              ident IN ('rest.logger.db', 'controller.log')
+                                          AND priority = 6) AS data JOIN moddb.SessionManager sm ON sm.session_token = data.token JOIN moddb.Users u ON u.id = sm.user_id
 SET
-    message = CONCAT(SUBSTR(message, 1, INSTR(message, '"username":null') - 1), '"username": "', u.username, '",',
-                     SUBSTR(message, INSTR(message, '"username":null,') + 16))
+    message = JSON_REPLACE(message, '$.data.username', u.username)
 WHERE
-    ident IN ('rest.logger.db', 'controller.log') AND priority = 6;
+      ident IN ('rest.logger.db', 'controller.log')
+  AND priority = 6;
 //
 
 /* backfill for removing extra escaping from log_table, only for rest.logger.db and controller.log. */
@@ -22,12 +20,13 @@ SET
     message = TRIM(TRAILING '"}' FROM
                    REPLACE(REPLACE(REPLACE(SUBSTR(message, 13), '\\"', '"'), '\\\\\\/', '/'), '\\/', '/'))
 WHERE
-    ident IN ('rest.logger.db', 'controller.log') AND priority = 6;
+      ident IN ('rest.logger.db', 'controller.log')
+  AND priority = 6;
 
 //
 /* backfill for removing the extra escaping for the other ident types */
 UPDATE mod_logger.log_table lt
-SET
-    message = REPLACE(REPLACE(REPLACE(REPLACE(message, '\\"', '"'), '\\\\\\/', '/'), '\\/', '/'), '\\\\', '\\')
+SET message = REPLACE(REPLACE(REPLACE(REPLACE(message, '\\"', '"'), '\\\\\\/', '/'), '\\/', '/'), '\\\\', '\\')
 WHERE
-    ident NOT IN ('rest.logger.db', 'controller.log') AND priority = 6;
+      ident NOT IN ('rest.logger.db', 'controller.log')
+  AND priority = 6;
