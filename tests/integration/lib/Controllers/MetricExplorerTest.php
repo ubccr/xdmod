d8dc061ada0c14c252a5499087f6a610fdb6f174
Andrew Stoltman <aestoltm@buffalo.edu>
2024-02-16 09:51:48 -0500

    more test updates

--- tests/integration/lib/Controllers/MetricExplorerTest.php
+++ tests/integration/lib/Controllers/MetricExplorerTest.php
@@ -134,7 +134,7 @@ class MetricExplorerTest extends TokenAuthTest
 
         $response = $this->helper->post('/controllers/metric_explorer.php', null, $params);
         $output = json_decode($response[0]);
-        $this->assertEquals($output->data[0]->layout->title->font->size, "19px");
+        $this->assertEquals($output->data[0]->layout->annotations[0]->font->size, "19");
 
         $params['data_series'] = '[object Object]';
         $response = $this->helper->post('/controllers/metric_explorer.php', null, $params);
--- tests/integration/lib/Controllers/ReportBuilderTest.php
+++ tests/integration/lib/Controllers/ReportBuilderTest.php
@@ -603,8 +603,7 @@ class ReportBuilderTest extends BaseTest
         $this->assertEquals(200, $response[1]['http_code']);
 
         $json = $response[0];
-        $this->log($response[0]);
-        $this->assertArrayHasKey('action', $json);
+        $this->assertArrayHasKey('action', $json, print_r($json, true));
         $this->assertArrayHasKey('phase', $json);
         $this->assertArrayHasKey('status', $json);
         $this->assertArrayHasKey('success', $json);
