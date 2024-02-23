dd042a066efc9b82b63d61df8dc6cb0b1258b949
Andrew Stoltman <aestoltm@buffalo.edu>
2024-02-23 17:45:53 -0500

    Fix for appkernels

--- html/gui/js/PlotlyChartWrapper.js
+++ html/gui/js/PlotlyChartWrapper.js
@@ -191,6 +191,12 @@ XDMoD.utils.createChart = function (chartOptions, extraHandlers) {
         }
 
         Plotly.relayout(baseChartOptions.renderTo, update);
+
+        if (baseChartOptions.layout.shapes && baseChartOptions.layout.shapes.length != 0) {
+            if (baseChartOptions.layout.shapes[0].name === 'Change Indicator') {
+                
+            }
+        }
     });
 
     return chart;
