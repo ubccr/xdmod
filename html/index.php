c714e740e6176f963e0d59e3c99a49628912a31a
Aaron Weeden <aaronwee@buffalo.edu>
2024-10-10 09:40:34 -0400

    Restore keywords; some search engines might still use them.

--- html/index.php
+++ html/index.php
@@ -104,9 +104,12 @@ $page_title = xd_utilities\getConfiguration('general', 'title');
     $meta_description = "XDMoD (XD Metrics on Demand) is a comprehensive auditing framework for NSF's ACCESS program, the follow-on to NSF's XSEDE and TeraGrid programs.  " .
         "XDMoD provides statistics, analytics, visualization, and reporting of cyberinfrastructure/HPC resource utilization and performance across multiple resource providers.";
 
+    $meta_keywords = "xdmod, access, xsede, analytics, metrics on demand, cyberinfrastructure, hpc, visualization, statistics, reporting, auditing, nsf, resources, resource providers";
+
     ?>
 
     <meta name="description" content="<?php print $meta_description; ?>"/>
+    <meta name="keywords" content="<?php print $meta_keywords; ?>"/>
 
     <title><?php print $page_title; ?></title>
 
