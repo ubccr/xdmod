b00b54964befe4f39448f72babcdce9943c56e30
Aaron Weeden <aaronwee@buffalo.edu>
2024-11-01 14:16:30 -0400

    Prep for v11.0.1 development.

---
+++ classes/OpenXdmod/Migration/Version1100To1101/ConfigFilesMigration.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Update config files from version 11.0.0 to 11.0.1
+ */
+
+namespace OpenXdmod\Migration\Version1100To1101;
+
+use OpenXdmod\Migration\ConfigFilesMigration as AbstractConfigFilesMigration;
+
+class ConfigFilesMigration extends AbstractConfigFilesMigration
+{
+
+    /**
+     * Update portal_settings.ini with the new version number.
+     */
+    public function execute()
+    {
+        $this->assertPortalSettingsIsWritable();
+        $this->assertModulePortalSettingsAreWritable();
+        $this->writePortalSettingsFile();
+        $this->writeModulePortalSettingsFiles();
+    }
+}
