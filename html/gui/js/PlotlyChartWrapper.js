3999129a605ff78e3fbeaab7f4435dc516851f41
Andrew Stoltman <aestoltm@buffalo.edu>
2024-12-18 17:27:43 -0500

    Revert code from a seperate PR

--- html/gui/js/PlotlyChartWrapper.js
+++ html/gui/js/PlotlyChartWrapper.js
@@ -16,12 +16,7 @@ Ext.namespace('XDMoD.utils');
 
 XDMoD.utils.createChart = function (chartOptions, extraHandlers) {
     const baseChartOptions = {};
-    const configs = {
-        displayModeBar: false,
-        doubleClick: 'reset',
-        doubleClickDelay: 500,
-        showAxisRangeEntryBoxes: false
-    };
+    const configs = { displayModeBar: false, doubleClick: 'reset', doubleClickDelay: 500 };
     XDMoD.utils.deepExtend(baseChartOptions, chartOptions);
     const isEmpty = (!baseChartOptions.data) || (baseChartOptions.data && baseChartOptions.data.length === 0);
 
@@ -97,9 +92,10 @@ XDMoD.utils.createChart = function (chartOptions, extraHandlers) {
         // for comparison idea
         baseChartOptions.data.sort((trace1, trace2) => {
             if (baseChartOptions.layout.barmode !== 'group') {
-                return Math.sign(trace2.traceorder - trace1.traceorder);
+                return Math.sign(trace2.zIndex - trace1.zIndex) || Math.sign(trace2.traceorder - trace1.traceorder);
             }
-            return Math.sign(trace1.traceorder - trace2.traceorder);
+            const res = Math.sign(trace1.zIndex - trace2.zIndex) || Math.sign(trace1.traceorder - trace2.traceorder);
+            return res;
         });
     }
 
