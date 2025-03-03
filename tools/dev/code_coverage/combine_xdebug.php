69a25f221eb56cfd720eeb8ad1c687570b921f83
Alex Tovar <alex@Mac-16.lan>
2025-03-03 15:04:26 -0500

    added toosl change

--- tools/dev/code_coverage/combine_xdebug.php
+++ tools/dev/code_coverage/combine_xdebug.php
@@ -1,5 +1,11 @@
 <?php
-
+use SebastianBergmann\CodeCoverage\Filter;
+use SebastianBergmann\CodeCoverage\Driver\Selector;
+use SebastianBergmann\CodeCoverage\CodeCoverage;
+use SebastianBergmann\CodeCoverage\Report\Xml\Facade as XmlReport;
+use SebastianBergmann\CodeCoverage\Report\Html\Facade as HtmlReport;
+use SebastianBergmann\CodeCoverage\RawCodeCoverageData;
+use Composer\InstalledVersions;
 #increase the memory limit
 ini_set('memory_limit', -1);
 
@@ -10,35 +16,83 @@ $reportDir = '__REPORT_DIR__';
 
 include_once("$baseDir/vendor/autoload.php");
 
+
+echo "base dir " . $baseDir . "\n";
+echo "coverage dir " . $coverageDir . "\n";
+echo "install dir " . $installDir . "\n";
+echo "report dir ". $reportDir . "\n";
+
+
 $coverageData = array();
 
 $coverageFiles = glob("$coverageDir/*.json");
+
 $count = count($coverageFiles);
+echo "Found $count coverage files\n";
 $i = 0;
 
 foreach ($coverageFiles as $coverageFile) {
     $i++;
     $matches = array();
-    $codeCoverageData = json_decode(file_get_contents($coverageFile), JSON_OBJECT_AS_ARRAY);
+    $codeCoverageData = json_decode(file_get_contents($coverageFile), true);
+    if ($codeCoverageData === null) {
+        echo "Error reading $coverageFile\n";
+        continue;
+    }
+    $rawCodeCoverageData = RawCodeCoverageData::fromXdebugWithPathCoverage($codeCoverageData);
     if ($codeCoverageData !== null) {
         $testName = str_ireplace("coverage-", "", basename($coverageFile, ".json"));
         $testName = substr($testName, 0, strrpos($testName, '-'));
+
         if (!array_key_exists($testName, $coverageData)) {
-            $coverage = new PHP_CodeCoverage();
-            $coverage->filter()->addDirectoryToWhitelist("$installDir/classes");
-            $coverage->filter()->addDirectoryToWhitelist("$installDir/html/controllers");
-            $coverage->filter()->addDirectoryToWhitelist("$installDir/html/internal_dashboard");
-            $coverage->filter()->addDirectoryToWhitelist("$installDir/libraries");
+
+            $filter = new Filter;
+
+            $filter->includeDirectory($installDir . '/classes');
+            $filter->includeDirectory($installDir . '/html/controllers');
+            $filter->includeDirectory($installDir . '/html/internal_dashboard');
+            $filter->includeDirectory($installDir . '/libraries');
+            $filter->excludeDirectory($installDir . '/vendor');
+
+            #TODO exclude vendor directory
+
+            $coverage = new CodeCoverage(
+                (new Selector)->forLineCoverage($filter),
+                $filter
+            );
+
             $coverageData[$testName] = $coverage;
+
+
+
+//            #TODO Change to reflect readme
+//            $coverage = new PHP_CodeCoverage();
+//            $coverage->filter()->addDirectoryToWhitelist("$installDir/classes");
+//            $coverage->filter()->addDirectoryToWhitelist("$installDir/html/controllers");
+//            $coverage->filter()->addDirectoryToWhitelist("$installDir/html/internal_dashboard");
+//            $coverage->filter()->addDirectoryToWhitelist("$installDir/libraries");
+//            $coverageData[$testName] = $coverage;
+//            echo "Ending coverage for $testName\n";
         }
+
         $testCoverage = $coverageData[$testName];
-        $testCoverage->append($codeCoverageData, $testName);
+
+        #Append requires $CodeCoverageData to be RawCodeCoverageData
+        $testCoverage->append($rawCodeCoverageData, $testName);
 
         $coverageData[$testName] = $testCoverage;
+
     }
 }
 
+$phpunitVersion = InstalledVersions::getVersion('phpunit/phpunit');
+
 foreach($coverageData as $testName => $coverage) {
-    $report = new PHP_CodeCoverage_Report_XML();
+    echo "Writing coverage for $testName\n";
+    #TODO Change to reflect readme
+    $report = new XmlReport($phpunitVersion);
     $report->process($coverage, $reportDir . DIRECTORY_SEPARATOR . $testName);
+
+    (new HtmlReport)->process($coverage, $reportDir . DIRECTORY_SEPARATOR . $testName);
+    echo "Done\n";
 }
--- tools/dev/code_coverage/start_xdebug.php
+++ tools/dev/code_coverage/start_xdebug.php
@@ -1,7 +1,18 @@
 <?php
 namespace CCR\CodeCoverage;
+\xdebug_set_filter(
+    XDEBUG_FILTER_CODE_COVERAGE,
+    XDEBUG_PATH_INCLUDE,
+    [
+        "/usr/share/xdmod/html/controllers/",
+        "/usr/share/xdmod/html/internal_dashboard/",
+        "/usr/share/xdmod/libraries/",
+        "/usr/share/xdmod/classes/",
+    ]
+);
 
-xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
+
+\xdebug_start_code_coverage(XDEBUG_CC_UNUSED | XDEBUG_CC_DEAD_CODE);
 
 function end_coverage()
 {
@@ -16,11 +27,11 @@ function end_coverage()
     $test_name = preg_replace('/::/', '-', $test_name);
 
     try {
-        xdebug_stop_code_coverage(false);
+        \xdebug_stop_code_coverage(false);
         $coverageName = '__CODE_COVERAGE_DIR__/coverage-' . $test_name . '-' . microtime(true);
-        $codecoverageData = json_encode(xdebug_get_code_coverage());
+        $codecoverageData = json_encode(\xdebug_get_code_coverage());
         file_put_contents($coverageName . '.json', $codecoverageData);
-    } catch (Exception $ex) {
+    } catch (\Exception $ex) {
         file_put_contents($coverageName . '.ex', $ex);
     }
 }
@@ -31,7 +42,7 @@ class coverage_dumper
     {
         try {
             end_coverage();
-        } catch (Exception $ex) {
+        } catch (\Exception $ex) {
             echo (string)$ex;
         }
     }
