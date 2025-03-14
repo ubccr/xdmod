8f4aa8c49155b857f6d045f56553eb18ae45fccf
Rose Tovar <15618284+rvtovar@users.noreply.github.com>
2025-03-14 11:30:44 -0400

    Playwright and docker compose integration for Ciricle CI testing (#1985)
    * UI to Playwright Test Migration
    
    These changes migrate our current UI test framework from webdriverio to
    playwright. The reasons for the migration include but are not limited to:
    - During testing Playwright proved to be more resilient to timining issues than
      webdriverio. This should mean less failures of CI builds due to timing issues
      which means less time dealt with timing issue failures during CI builds.
    - Playwright supports parallel test execution so CI builds end up taking
      significantly less time.
    
    The original ui tests in `tests/ui/*` have been removed as they are no longer
    needed.
    
    * Testing the removal of some env variables
    
    * Additional Cleanup
    
    * more code review changes
    
    * Appeasing the style gods
    
    * more appeasing of the gods
    
    * askdjf
    
    * Extracting qa-test-setup.sh into CircleCI config
    
    * Updates for qa tests
    
    * lksdjf
    
    * updates
    
    * slkdjf
    
    * Re-adding some circleci steps
    
    * Additional circleci changes
    
    * alskdjf
    
    * Updates per code review by @aaronweeden
    
    * additional changes per @aaronweeden
    
    * updates
    
    * Update .circleci/config.yml
    
    Co-authored-by: Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
    
    * Updates to get things running
    
    * Fixing mariadb server config
    
    * Exploring one way of resolving regressiontest failure
    
    * Appeasing the style gods
    
    * Updating docker-compose XDMOD_REALMS
    
    * Plotly updates for playwright tests
    
    * maybe
    
    * aslkdjhf
    
    * Increase test timeout
    
    * slkdfj
    
    * I Hate Trello Boards
    
    * wait for trello some more
    
    * getting to the good stuff
    
    * disable Trello content tests
    
    * Re-jiggering the SSO test
    
    * Turning on the other tests now
    
    * Use docker exec to perform shell commands
    
    * Syntax error
    
    * Adjust syntax for lines that run multiple commands
    
    * Specify mongodb version for pecl install
    
    * Based off CI test results saying it could not find the PHP mongodb module, try adding the extension to PHP etc
    
    * Try setting flag causing the error
    
    * Remove specified version
    
    * Update docker image to latest
    
    * Update branch to 'main'
    
    * Remove unused command
    
    * Add back command from merge commit
    
    * Adjust sorting due to header being place at bottom of data set
    
    * Remove unused sort flag
    
    * Try using newer playwright image
    
    * Linter fix
    
    * updated circleci
    
    * playing with config.yml
    
    * updated to root
    
    * added root to buildrpm xdmod
    
    * modified setup.sh
    
    * added root to config.yml
    
    * added more root
    
    * changed root on playwright
    
    * update to jammy
    
    * added git config
    
    * added a docker git config
    
    * added copy test results to ciricle ci
    
    * change script to exec
    
    * change back
    
    * insert os using shell scriping
    
    * added quotes and seperated parameters.os from copy test results ainto unit
    
    * added spaces
    
    * adding circle ci string_os
    
    * added list
    
    * removed the script that i made to do ciricle ci stuff
    
    * removed sphinx_venv
    
    * removed pycache
    
    * removed venv from user_manual html
    
    * removed user manual
    
    * removing cache
    
    * removing cache
    
    * removed untracked files and set portal_settings.ini back to default
    
    * removed lock file
    
    * added lock file back
    
    * added files back
    
    * updated reportsOverview.js
    
    * updated docker compose to use new version of playwright
    
    * updated playwright to 1.51.0
    
    * env and dockerfile were not doing anything removed them
    
    * removed not needed configuration files
    
    ---------
    
    Co-authored-by: Ryan Rathsam <build@shippable.com>
    Co-authored-by: Ryan Rathsam <ryanrath@buffalo.edu>
    Co-authored-by: Aaron Weeden <31246768+aaronweeden@users.noreply.github.com>
    Co-authored-by: Ryan Rathsam <rrathsam@gmail.com>
    Co-authored-by: Andrew Stoltman <aestoltm@buffalo.edu>
    Co-authored-by: Andrew Stoltman <60742416+aestoltm@users.noreply.github.com>
    Co-authored-by: Alex Tovar <alex@dhcp-10-83-133-27.wireless.buffalo.edu>
    Co-authored-by: Alex Tovar <alex@dhcp-10-83-130-147.wireless.buffalo.edu>
    Co-authored-by: Alex Tovar <alex@Mac-18.lan>
    Co-authored-by: Alex Tovar <alex@Mac-156.lan>

--- tests/ci/bootstrap.sh
+++ tests/ci/bootstrap.sh
@@ -63,20 +63,22 @@ then
     dnf install -y ~/rpmbuild/RPMS/*/*.rpm
     mysql_install_db --user mysql
 
-    # Make sure that the db config file is setup correctly w/ `sql_mode=`
-    echo "# this is read by the standalone daemon and embedded servers
-          [server]
-          sql_mode=
-          # this is only for the mysqld standalone daemon
-          # Settings user and group are ignored when systemd is used.
-          # If you need to run mysqld under a different user or group,
-          # customize your systemd unit file for mysqld/mariadb according to the
-          # instructions in http://fedoraproject.org/wiki/Systemd
-          [mysqld]
-          datadir=/var/lib/mysql
-          socket=/var/lib/mysql/mysql.sock
-          log-error=/var/log/mariadb/mariadb.log
-          pid-file=/run/mariadb/mariadb.pid" > /etc/my.cnf.d/mariadb-server.cnf
+    if [ -f /etc/my.cnf.d/mariadb-server.cnf ]; then
+        >/etc/my.cnf.d/mariadb-server.cnf
+        echo "# this is read by the standalone daemon and embedded servers
+              [server]
+              sql_mode=
+              # this is only for the mysqld standalone daemon
+              # Settings user and group are ignored when systemd is used.
+              # If you need to run mysqld under a different user or group,
+              # customize your systemd unit file for mysqld/mariadb according to the
+              # instructions in http://fedoraproject.org/wiki/Systemd
+              [mysqld]
+              datadir=/var/lib/mysql
+              socket=/var/lib/mysql/mysql.sock
+              log-error=/var/log/mariadb/mariadb.log
+              pid-file=/run/mariadb/mariadb.pid" > /etc/my.cnf.d/mariadb-server.cnf
+    fi
 
     copy_template_httpd_conf
     ~/bin/services start
