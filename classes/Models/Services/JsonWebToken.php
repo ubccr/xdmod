af8f35ea3323a507829ad8045d286599ec8dcf39
Conner Saeli <connersa@buffalo.edu>
2025-04-10 13:54:21 -0400

    Fixing mistakes.

--- classes/Models/Services/JsonWebToken.php
+++ classes/Models/Services/JsonWebToken.php
@@ -11,46 +11,44 @@ use XDUser;
 
 class JsonWebToken
 {
-    const
+    public static $signingAlgorithm = 'HS256';
 
-    public static signingAlgorithm = 'HS256';
-
-    private static claimKeyIssuedAtTime = 'iat';
-    private static claimKeyTokenId      = 'jti';
-    private static claimKeyExpiration   = 'exp';
-    private static claimKeySubject      = 'sub';
-    private static claimKeyAudience     = 'aud';
-    private static claimKeyIssuer       = 'iss';
+    public static $claimKeyIssuedAtTime = 'iat';
+    public static $claimKeyTokenId      = 'jti';
+    public static $claimKeyExpiration   = 'exp';
+    public static $claimKeySubject      = 'sub';
+    public static $claimKeyAudience     = 'aud';
+    public static $claimKeyIssuer       = 'iss';
 
     private $_claimsSet;
     private $_secretKey;
 
     public function __construct($claims = array()) {
         $configuredSecretKey = \xd_utilities\getConfiguration('json_web_token', 'secret_key');
-        $this->$_secretKey = new Key($configuredSecretKey, $this->signingAlgorithm);
-        $this->$_claimsSet = $claims;
+        $this->_secretKey = $configuredSecretKey; //new Key($configuredSecretKey, self::$signingAlgorithm);
+        $this->_claimsSet = $claims;
     }
 
     public function encode() {
 
         $xdmodURL   = \xd_utilities\getConfiguration('general', 'site_address');
-        $issuedAt   = new \DateTimeImmutable()->getTimestamp();
+        $issuedAt   = new \DateTimeImmutable();
         $expire     = $issuedAt->modify('+6 minutes')->getTimestamp();
 
         $claims = [
-            JsonWebToken::claimKeyIssuedAtTime  => $issuedAt->getTimestamp(),
-            JsonWebToken::claimKeyTokenId       => $base64_encode(random_bytes(16)),
-            JsonWebToken::claimKeyExpiration    => $expire,
-            JsonWebToken::claimKeyAudience      => $xdmodURL,
-            JsonWebToken::claimKeyIssuer        => $xdmodURL
+            self::claimKeyIssuedAtTime  => $issuedAt->getTimestamp(),
+            self::claimKeyTokenId       => base64_encode(random_bytes(16)),
+            self::claimKeyExpiration    => $expire,
+            self::claimKeyAudience      => $xdmodURL,
+            self::claimKeyIssuer        => $xdmodURL
         ];
 
         $this->addClaims($claims);
 
         return JWT::encode(
-            $this->$_claimsSet,
-            $this->$_secretKey,
-            $this->$signingAlgorithm
+            $this->_claimsSet,
+            $this->_secretKey,
+            self::$signingAlgorithm
         );
     }
 
@@ -68,13 +66,16 @@ class JsonWebToken
      * Add one or more claim to the claims set
      */
     public function addClaims($claims) {
-        array_push($this->_claimsSet, $claims);
+        foreach ($claims as $key => $value) {
+            $this->_claimsSet[$key] = $value;
+        }
     }
 
     public function getClaim($claimKey) {
-        if (array_key_exists($claimKey, $this->$_claimsSet)) {
-            return $this->$_claimsSet[$claimKey];
+        if (array_key_exists($claimKey, $this->_claimsSet)) {
+            return $this->_claimsSet[$claimKey];
         } else {
             throw new Exception('JSON Web Token Claim does not exist.');
         }
     }
+}
