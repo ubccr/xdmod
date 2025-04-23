a5146010dc5981dccd3a9d4f7352ae459dc425e7
Andrew Stoltman <aestoltm@buffalo.edu>
2025-04-23 14:30:54 -0400

    Added timezone conversion to Plotly JS charts. Should have been included in original conversion.

--- html/gui/js/PlotlyChartWrapper.js
+++ html/gui/js/PlotlyChartWrapper.js
@@ -85,6 +85,23 @@ XDMoD.utils.createChart = function (chartOptions, extraHandlers) {
             }
             return Math.sign(trace1.traceorder - trace2.traceorder);
         });
+
+        // Format timezone -- Plotly does not support timezones
+        // Therefore, we need to utilize moment JS.
+        let axis = baseChartOptions.layout.swapXY ? 'y' : 'x';
+        baseChartOptions.data.forEach((series) => {
+            if (series.type !== 'pie') {
+                series[axis].forEach((elem, index, seriesArr) => {
+                    seriesArr[index] = moment.tz(elem, CCR.xdmod.timezone).format();
+                });
+            }
+        });
+
+        // Format timezone for the tickvals also
+        axis = axis + 'axis';
+        baseChartOptions.layout[axis].tickvals.forEach((tick, index, tickvals) => {
+            tickvals[index] = moment.tz(tick, CCR.xdmod.timezone).format();
+        });
     }
 
     const chart = Plotly.newPlot(baseChartOptions.renderTo, baseChartOptions.data, baseChartOptions.layout, configs);
