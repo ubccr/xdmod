a5bb0df71e415ce2218b2bda00786fc459d9ea57
Aaron Weeden <aaronwee@buffalo.edu>
2025-07-08 13:59:18 -0400

    Add more error handling for encoding/decoding.

--- classes/Models/Services/JsonWebToken.php
+++ classes/Models/Services/JsonWebToken.php
@@ -3,12 +3,23 @@
 namespace Models\Services;
 
 use DateTimeImmutable;
+use DomainException;
+use Exception;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use InvalidArgumentException;
 
 class JsonWebToken
 {
     const SIGNING_ALGORITHM = 'RS256';
+    const KEYS_DIR = (
+        CONFIG_DIR
+        . DIRECTORY_SEPARATOR
+        . 'keys'
+        . DIRECTORY_SEPARATOR
+    );
+    const XDMOD_PRIVATE_KEY_FILE = KEYS_DIR . 'xdmod-private.pem';
+    const JUPYTERHUB_PUBLIC_KEY_FILE = KEYS_DIR . 'jupyterhub-public.pem';
 
     /**
      * @param string $subject the 'sub' property of the JWT to encode.
@@ -16,23 +27,32 @@ class JsonWebToken
      *               time of the JWT.
      */
     public static function encode($subject) {
-        $xdmodPrivateKey = file_get_contents(
-            CONFIG_DIR
-            . DIRECTORY_SEPARATOR
-            . 'keys'
-            . DIRECTORY_SEPARATOR
-            . 'xdmod-private.pem'
-        );
+        $xdmodPrivateKey = file_get_contents(XDMOD_PRIVATE_KEY_FILE);
+        if (false === $xdmodPrivateKey) {
+            throw new Exception(
+                'This XDMoD portal is missing a private key at `'
+                . XDMOD_PRIVATE_KEY_FILE
+                . '` for signing JSON Web Tokens.'
+            );
+        }
         $issuedAt = new DateTimeImmutable();
         $expiration = $issuedAt->modify('+30 seconds')->getTimestamp();
-        $jwt = JWT::encode(
-            [
-                'exp' => $expiration,
-                'sub' => $subject
-            ],
-            $xdmodPrivateKey,
-            self::SIGNING_ALGORITHM
-        );
+        try {
+            $jwt = JWT::encode(
+                [
+                    'exp' => $expiration,
+                    'sub' => $subject
+                ],
+                $xdmodPrivateKey,
+                self::SIGNING_ALGORITHM
+            );
+        } catch (DomainException $e) {
+            throw new Exception(
+                'Error signing the JSON Web Token using `'
+                . XDMOD_PRIVATE_KEY_FILE
+                . '`.'
+            );
+        }
         return [$jwt, $expiration];
     }
 
@@ -41,14 +61,24 @@ class JsonWebToken
      * @return \stdClass the claims in the JWT.
      */
     public static function decode($jwt) {
-        $jupyterhubPublicKey = file_get_contents(
-            CONFIG_DIR
-            . DIRECTORY_SEPARATOR
-            . 'keys'
-            . DIRECTORY_SEPARATOR
-            . 'jupyterhub-public.pem'
-        );
-        $secretKey = new Key($jupyterhubPublicKey, self::SIGNING_ALGORITHM);
-        return JWT::decode($jwt, $secretKey);
+        $jupyterhubPublicKey = file_get_contents(JUPYTERHUB_PUBLIC_KEY_FILE);
+        if (false === $jupyterhubPublicKey) {
+            throw new Exception(
+                'This XDMoD portal is missing a public key at `'
+                . JUPYTERHUB_PUBLIC_KEY_FILE
+                . '` for decoding JSON Web Tokens.'
+            );
+        }
+        try {
+            $secretKey = new Key($jupyterhubPublicKey, self::SIGNING_ALGORITHM);
+            $claims = JWT::decode($jwt, $secretKey);
+        } catch (InvalidArgumentException $e) {
+            throw new Exception(
+                'The public key file at `'
+                . JUPYTERHUB_PUBLIC_KEY_FILE
+                . '` is empty.'
+            );
+        }
+        return $claims;
     }
 }
