e690069e10841fcb39bbb9ef3a36df9602359ea4
Andrew Stoltman <aestoltm@buffalo.edu>
2025-07-16 16:42:38 -0400

    Reformat comments

--- tests/ci/scripts/metric-saved-charts-test-setup.sh
+++ tests/ci/scripts/metric-saved-charts-test-setup.sh
@@ -9,35 +9,26 @@ BUILDENV="ME-export-saved-charts-venv"
 
 set -e
 
-# Switch to the repo root
 pushd $XDMOD_SOURCE_DIR >/dev/null || exit 1
 
-# Convenient for local testing
 if [ -z $BUILDENV ]; then
     mkdir $BUILDENV
 fi
 
-# Setup python venv
 python3 -m venv $BUILDENV
 source $BUILDENV/bin/activate
 
-# Install script deps
 pip3 install -q --upgrade pip
 pip3 install -q requests python-dotenv
 
-# Load credentials into .env
 echo "XDMOD_USERNAME="${CD_USENAME}"" > $XDMOD_SOURCE_DIR/docs/assets/scripts/.env
 echo "XDMOD_PASSWORD="${CD_PASSWORD}"" >> $XDMOD_SOURCE_DIR/docs/assets/scripts/.env
 
-# Fix the url
+# Fix the url, udate export dir, and disable SSL check for localhost
 sed -i "s|site_address = ''|site_address = '${XDMOD_URL}'|g" $XDMOD_SOURCE_DIR/docs/assets/scripts/export-metric-explorer-charts.py
-
-# Change output dir to tmp
 sed -i "s|export_dir = '.'|export_dir = '/tmp'|g" $XDMOD_SOURCE_DIR/docs/assets/scripts/export-metric-explorer-charts.py
-
 sed -i '/session = requests.Session()/a session.verify = False' $XDMOD_SOURCE_DIR/docs/assets/scripts/export-metric-explorer-charts.py
 
-# Call script to export ME saved chart
 chmod +x $XDMOD_SOURCE_DIR/docs/assets/scripts/export-metric-explorer-charts.py
 $XDMOD_SOURCE_DIR/docs/assets/scripts/export-metric-explorer-charts.py
 
