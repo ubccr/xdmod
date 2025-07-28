c94bcdc8540f58dc9fcb3af077fa74071e72b1a7
Ryan Rathsam <ryanrath@buffalo.edu>
2025-07-28 14:30:34 -0400

    Changing how we wait for mask to be gone
    These changes update the way we wait for the mask to not be present from
    waiting for it to not be visible to waiting for the locator to return 0
    ( i.e. that it's not in the dom ).
    
    I also included an update from 1.51.1 to 1.54.1 for playwright

--- tests/playwright/lib/metricExplorer.page.ts
+++ tests/playwright/lib/metricExplorer.page.ts
@@ -80,6 +80,7 @@ class MetricExplorer extends BasePage{
         await this.page.click(this.newChart.modalDialog.ok());
         await expect(this.page.locator(this.newChart.modalDialog.box)).toBeHidden();
         await expect(this.page.locator(this.newChart.modalDialog.noDataMessage)).toBeVisible();
+        await expect(this.page.locator(this.mask)).toHaveCount(0);
     }
 
     /**
@@ -105,6 +106,7 @@ class MetricExplorer extends BasePage{
         await this.endDateLocator.click();
         await this.page.fill(this.endDate, end);
         await this.page.click(this.toolbar.buttonByName('Refresh'));
+        await expect(this.page.locator(this.mask)).toHaveCount(0);
     }
 
     /**
