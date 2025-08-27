5682c10f4615a8feb5af701040f308a1befe1634
Greg Dean <gmdean@buffalo.edu>
2025-08-27 12:08:06 -0400

    adding STRAIGHT_JOIN to metrics explorer/usage tab queries to force join order since that order seems to be the quickest and some times mysql rewrites it to a different order

--- classes/DataWarehouse/Query/Query.php
+++ classes/DataWarehouse/Query/Query.php
@@ -728,7 +728,7 @@ SQL;
         $select_order_by = $this->getSelectOrderBy();
 
         $format = <<<SQL
-SELECT%s
+SELECT STRAIGHT_JOIN%s
   %s
 FROM
   %s%s
@@ -769,7 +769,7 @@ SQL;
 SELECT
   COUNT(*) AS row_count
 FROM (
-  SELECT
+  SELECT STRAIGHT_JOIN
   %s AS total
   FROM
     %s
