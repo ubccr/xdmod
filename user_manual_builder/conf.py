bf94d2c551a2637cd2755560bb2103819f33b8cb
Joseph White <jpwhite4@buffalo.edu>
2025-10-03 10:51:51 -0400

    Update dependency versions. Fix build warnings
    Update the dependencies to the latest compatible version.
    
    Fix cross ref error in the Allocations Tab (seen as a warning
    at build time).

--- user_manual_builder/conf.py
+++ user_manual_builder/conf.py
@@ -21,7 +21,7 @@ release = ''
 extensions = ['sphinx_rtd_theme', 'only']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'sphinx_venv']
 if 'Open' in tags:
     exclude_patterns.extend(['Compliance_Tab.rst', 'Allocations_Tab.rst'])
 
