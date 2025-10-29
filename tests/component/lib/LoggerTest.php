13a70e82d5ce9bed29c432fe71bd69c4ff09627f
Joseph White <jpwhite4@buffalo.edu>
2025-10-29 11:35:59 -0400

    Fix formatting issues with the logger.
    This fixes two bugs in the output format for the logger:
    1) The line based formatter had incorrect spacing between the message
       and context information (when present)
    2) The line based formatter was ommiting the stack trace when
       printing exceptions
    3) The database handler was not configured to output the context information
    
    I've added some component tests to verify the correct output. The reference
    values for the output were generated on the old code.
    
    Known issues:
    
    The serialization of exceptions is different in this version of monolog vs
    the XDMoD 10.5 version. The old code would print exceptions like:
    
    ```
    2025-10-29 15:46:44 [error]  (exceptiontest: Exception: Test Line Exception in /root/xdmod/tests/component/lib/LoggerTest.php:16
    Stack trace:
    ```
    
    and the new code does it like this:
    
    ```
    2025-10-29 15:17:02 [error] (exceptiontest: [object] (Exception(code: 0): Test Line Exception at /root/xdmod/tests/component/lib/LoggerTest.php:16)
    [stacktrace]
    )
    ```
    
    I'm not going to make the code use the old exception serialization format at this time
    I think it only impacts people reading the logs and the new format provides more information
    than the old one.

---
+++ tests/component/lib/LoggerTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace ComponentTests;
+
+use CCR\Log;
+use CCR\DB;
+
+class LoggerTest extends BaseTest
+{
+    public function provideFileOutput()
+    {
+        return array(
+            array('debug', 'message field', array('other' => 1.2), '/\[debug\] message field \(other: 1.2\)$/'),
+            array('info', 'single line string', array(), '/\[info\] single line string$/'),
+            array('warning', '', array('other' => 'comp123'), '/\[warning\] \(other: comp123\)$/'),
+            array('error', '', array('exceptiontest' => new \Exception('Test Line Exception')), '/\[error\] \(exceptiontest: .*' . str_replace('/', '\\/', __FILE__) . ':' . __LINE__ . '\)\W\[stacktrace\]/')
+        );
+    }
+
+    /**
+     * @dataProvider provideFileOutput
+     * @param string $message to send to the logger
+     * @param array $context to send to the logger
+     * @param string $expectedRegex to match the file output content
+     */
+    public function testFileOutput($level, $message, $context, $expectedRegex)
+    {
+        $conf = array(
+            'db' => false,
+            'console' => false,
+            'mail' => false,
+            'file' => tempnam(sys_get_temp_dir(), 'component_logger_test_'),
+            'fileLogLevel' => Log::DEBUG
+        );
+
+        $logger = Log::factory('log-test', $conf);
+        $logger->$level($message, $context);
+
+        $output = file_get_contents($conf['file']);
+        $this->assertMatchesRegularExpression($expectedRegex, $output);
+    }
+
+    public function provideDbOutput()
+    {
+        return array(
+            array('debug', 'message field', array('other' => 1.2), 7, '{"message":"message field","other":1.2}'),
+            array('info', 'single line string', array(), 6, '{"message":"single line string"}'),
+            array('warning', '', array('other' => 'comp123'), 4, '{"other":"comp123"}')
+        );
+    }
+
+    /**
+     * @dataProvider provideDbOutput
+     * @param string $message to send to the logger
+     * @param array $context to send to the logger
+     * @param string $expectedPriority expected numerical priority value in the database
+     * @param string $expectedJsonString expected json content string
+     */
+    public function testDbOutput($level, $message, $context, $expectedPriority, $expectedJsonString)
+    {
+        $conf = array(
+            'console' => false,
+            'mail' => false,
+            'file' => false,
+            'db' => true,
+            'dbLogLevel' => Log::DEBUG
+        );
+
+        $db = DB::factory('logger');
+        $initial_vals = $db->query("SELECT MAX(id) AS start_id FROM mod_logger.log_table");
+
+        $logger = Log::factory('log-test', $conf);
+        $logger->$level($message, $context);
+
+        $logoutput = $db->query("SELECT priority, message FROM mod_logger.log_table WHERE ident = 'log-test' AND id > :start_id ORDER BY id ASC", $initial_vals[0]);
+	    $this->assertJsonStringEqualsJsonString($expectedJsonString, $logoutput[0]['message']);
+	    $this->assertEquals($expectedPriority, $logoutput[0]['priority']);
+    }
+
+    public function testDbExceptionFormat()
+    {
+        $ident = 'testDbExceptionFormat';
+
+        $conf = array(
+            'console' => false,
+            'mail' => false,
+            'file' => false,
+            'db' => true,
+            'dbLogLevel' => Log::DEBUG
+        );
+
+        $db = DB::factory('logger');
+        $initial_vals = $db->query("SELECT MAX(id) AS start_id FROM mod_logger.log_table");
+
+        $logger = Log::factory($ident, $conf);
+
+        $logger->error('', array('other' => new \Exception('Test Exception'))); $exline = __LINE__;
+
+        $logoutput = $db->query("SELECT priority, message FROM mod_logger.log_table WHERE ident = '" . $ident . "' AND id > :start_id ORDER BY id ASC", $initial_vals[0]);
+
+        $this->assertEquals('3', $logoutput[0]['priority']);
+        $exceptionSerialization = json_decode($logoutput[0]['message'], true);
+
+        $this->assertArrayNotHasKey('message', $exceptionSerialization);
+        $this->assertArrayHasKey('other', $exceptionSerialization);
+        $this->assertArrayHasKey('class', $exceptionSerialization['other']);
+        $this->assertArrayHasKey('trace', $exceptionSerialization['other']);
+
+        $this->assertEquals('Exception', $exceptionSerialization['other']['class']);
+        $this->assertEquals('Test Exception', $exceptionSerialization['other']['message']);
+        $this->assertEquals( __FILE__ . ":$exline", $exceptionSerialization['other']['file']);
+    }
+
+    public function testCombinedOutput()
+    {
+        $conf = array(
+            'db' => true,
+            'dbLogLevel' => Log::DEBUG,
+            'console' => false,
+            'mail' => false,
+            'file' => tempnam(sys_get_temp_dir(), 'component_combined_test_'),
+            'fileLogLevel' => Log::DEBUG
+        );
+
+        $logger = Log::factory('combined-test', $conf);
+
+        $db = DB::factory('logger');
+        $initial_vals = $db->query("SELECT MAX(id) AS start_id FROM mod_logger.log_table");
+
+        $logger->debug('message portion', array('context' => 'portion'));
+
+        $output = file_get_contents($conf['file']);
+        $this->assertStringEndsWith("[debug] message portion (context: portion)\n", $output);
+
+        $logoutput = $db->query("SELECT priority, message FROM mod_logger.log_table WHERE ident = 'combined-test' AND id > :start_id ORDER BY id ASC", $initial_vals[0]);
+
+        $this->assertEquals('{"message":"message portion","context":"portion"}', $logoutput[0]['message']);
+        $this->assertEquals('7', $logoutput[0]['priority']);
+    }
+}
