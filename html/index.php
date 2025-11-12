8d791219338a4343941b73f18a9d0ff4a326e3f5
Ryan Rathsam <ryanrath@buffalo.edu>
2025-11-12 11:04:23 -0500

    These headers are no longer necessary
    Or at least, they do not cause issues when running our normal suite of
    automated tests or when logging in via SSO locally.

--- html/index.php
+++ html/index.php
@@ -5,12 +5,6 @@ use Access\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-# These are only here temporarily,
-# put together which headers / values are set based on config options.
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: *");
-header("Allow: *");
 
 // Configurable constants ---------------------------
 $orgConfig = \Configuration\XdmodConfiguration::assocArrayFactory(
