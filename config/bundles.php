9c05e7a09af4a5e3bd83af07b58859338c425fbc
ryanrath <ryanrath@buffalo.edu>
2025-12-04 15:05:28 -0500

    Removing Doctrine as it's unused (#2131)

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
