855c3b37cf66e3b3969ab49635f9b02b2c2c32ca
Conner Saeli <connersa@buffalo.edu>
2026-06-05 00:09:17 -0400

    .

--- src/Security/Authenticators/RESTAuthenticatorSuccessHandler.php
+++ src/Security/Authenticators/RESTAuthenticatorSuccessHandler.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 
+use XDUser;
+
 class RESTAuthenticatorSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
     public function onAuthenticationSuccess(Request $request, TokenInterface $token): ?Response
