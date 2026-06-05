9d3f39a2417f76e778411ab2cddf336e1511ca0a
Conner Saeli <connersa@buffalo.edu>
2026-06-04 23:53:05 -0400

    ....

---
+++ src/Security/Authenticators/RESTAuthenticatorSuccessHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CCR\Security\Authenticators;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+
+class RESTAuthenticatorSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): ?Response
+    {
+        $user = $token->getUser();
+        $xdUser = XDUser::getUserByUserName($user->getUserIdentifier());
+        $response = new JsonResponse([
+            'success' => true,
+            'results' => [
+                'token' => $xdUser->getToken(),
+                'name' => $xdUser->getFormalName()
+            ]
+        ]);
+
+        return $response;
+    }
+}
+
