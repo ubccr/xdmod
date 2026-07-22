58672303f4834e80d97b9a2652f4174436369cfc
ryanrath <ryanrath@buffalo.edu>
2026-07-22 14:43:02 -0400

    (re)Moving bin/console into xdmod-admin (#2254)
    * (re)Moving bin/console into xdmod-admin
    
    This commit removes Symfony's `bin/console` script and moves that
    functionality into our `xdmod-admin` script. I've added some "happy path" tests to ensure things are working as we would expect, hopefully when we encounter some failure examples we can update these tests to account for them.

--- classes/OpenXdmod/Migration/DotEnvConfigMigration.php
+++ classes/OpenXdmod/Migration/DotEnvConfigMigration.php
@@ -14,7 +14,7 @@ class DotEnvConfigMigration extends Migration
         if (!file_exists($dotEnvPath)) {
             // .env doesn't need anything in it, but it does need to exist.
             file_put_contents(BASE_DIR . '/.env', '');
-            SymfonyCommandHelper::dumpDotEnv();
         }
+        (new SymfonyCommandHelper())->dumpDotEnv();
     }
 }
--- src/Helper/SymfonyCommandHelper.php
+++ src/Helper/SymfonyCommandHelper.php
@@ -2,10 +2,17 @@
 
 namespace CCR\Helper;
 
+use CCR\Helper\Exception\NonZeroStatusCodeException;
 use CCR\Kernel;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Exception\ExceptionInterface;
+use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\Input;
+use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\Output;
 use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Dotenv\Exception\FormatException;
 use Symfony\Component\Dotenv\Exception\PathException;
@@ -17,48 +24,33 @@ use Symfony\Component\Dotenv\Exception\PathException;
  * NOTE: Once we have all of our existing code migrated to `src` then we can change `executeCommand` from a static
  * function to a regular function and use Dependency Injection to add it to the classes that need it.
  */
-class SymfonyCommandHelper
+readonly class SymfonyCommandHelper
 {
+
+    public function __construct(private ?LoggerInterface $logger = null)
+    {
+    }
+
     /**
      * Execute the provided Symfony $command.
      *
-     * @param string $command the Symfony command to run.
-     * @param array $options for the Symfony command.
+     * @param string|array $command the Symfony command to run.
      * @throws \LogicException if the command is empty.
      * @throws \Exception if an error is encountered while running the specified command.
      * @throws \RuntimeException if a non-zero exit code is returned by the Symfony Command.
      */
-    public static function executeCommand(string $command, array $options = [], string $env = 'prod', bool $debug = false): void
+    public function executeCommand(string|array $command, string $env = 'prod', bool $debug = false): string
     {
-        list($statusCode, $output) = self::doExecuteCommand($command, $options, $env, $debug);
-        if ($statusCode !== 0) {
-            throw new \RuntimeException("Error Running Symfony Command $command\n$output");
-        }
-    }
+        $this->logger?->debug('Executing Symfony String Command', ['command' => var_export($command, true)]);
 
-    /**
-     * Execute the provided Symfony console command.
-     *
-     * @param string $command
-     * @param array $options
-     * @param string $env
-     * @param bool $debug
-     * @throws \LogicException if the command is empty.
-     * @throws \RuntimeException if the environment file cannot be loaded or
-     *  if a non-zero exit code is returned by the Symfony console command
-     */
-    private static function doExecuteCommand(string $command, array $options, string $env, bool $debug): array
-    {
         if (empty($command)) {
             throw new \LogicException('Command must not be empty.');
         }
 
-        try {
-            $envPath = BASE_DIR . '/.env';
-            (new Dotenv())->bootEnv($envPath);
-        } catch(FormatException | PathException $e) {
-            throw new \RuntimeException('Unable to load environment file', $e->getCode(), $e);
-        }
+        // Boot up the environment for Symfony to use when executing the provided command.
+        $envPath = BASE_DIR . '/.env';
+        (new Dotenv())->bootEnv($envPath);
+
 
         // Setup our Kernel / Application
         $kernel = new Kernel($env, $debug);
@@ -67,26 +59,44 @@ class SymfonyCommandHelper
         // we set this so that it doesn't `exit` whatever php script is calling this function.
         $application->setAutoExit(false);
 
-        // Set the Symfony command to execute.
-        array_unshift($options, $command);
-
-        $input = new ArrayInput($options);
-        $output = new BufferedOutput();
-        try {
-            $statusCode = $application->run($input, $output);
-            return [$statusCode, $output->fetch()];
-        } catch(\Exception $e) {
-            throw new \RuntimeException("Error while running Symfony Command", $e->getCode(), $e);
+        // Determine what type of Input to use based on the type of $command.
+        if (is_array($command)) {
+            $input = new ArgvInput($command);
+        } else {
+            $input = new StringInput($command);
         }
+        $output = new BufferedOutput();
+
+        // Attempt to execute the Symfony command.
+        $statusCode = $application->run($input, $output);
+
+        $this->logger?->debug('Symfony Command Executed', ['status_code' => $statusCode]);
+
+        // Make sure to snag the output now just in case the application returned a non-zero statusCode so we can
+        // include it with the exception.
+        $commandOutput = $output->fetch();
+        if ($statusCode !== 0) {
+            throw new NonZeroStatusCodeException($commandOutput, "Error Running Symfony Command");
+        }
+
+        return $commandOutput;
     }
 
-    public static function dumpDotEnv(): void
+    /**
+     * A helper function that first executes cache:clear followed by dotenv:dump. This ensures that we have an up to
+     * date `.env.local.php` file.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function dumpDotEnv(): void
     {
         // Make sure to clear the cache before dumping the dotenv so we start clean.
-        SymfonyCommandHelper::executeCommand('cache:clear');
+        $this->executeCommand('cache:clear');
 
         // Dump dotenv data so we don't read .env each time in prod.
-        // Note: this means that if you want to start debugging stuff you'll need to delete the generated .env.
-        SymfonyCommandHelper::executeCommand('dotenv:dump');
+        // Note: this means that if you want to start debugging stuff you'll need to delete or modify the generated
+        // .env.local.php.
+        $this->executeCommand('dotenv:dump');
     }
 }
