8b3e876471273ef228fecdccc865fe010602f192
Andrew Stoltman <60742416+aestoltm@users.noreply.github.com>
2026-07-22 15:13:43 -0400

    Simplify chart exporting (#2127) (#2272)
    * Simplify the export process for XDMoD. Also fixes a bug with timezones not being considered for image export
    * Port PR to xdmod11.0

--- html/gui/js/PlotlyChartWrapper.js
+++ html/gui/js/PlotlyChartWrapper.js
@@ -20,7 +20,8 @@ XDMoD.utils.createChart = function (chartOptions, extraHandlers) {
         displayModeBar: false,
         doubleClick: 'reset',
         doubleClickDelay: 500,
-        showAxisRangeEntryBoxes: false
+        showAxisRangeEntryBoxes: false,
+        staticPlot: chartOptions.isExport
     };
     XDMoD.utils.deepExtend(baseChartOptions, chartOptions);
     const isEmpty = (!baseChartOptions.data) || (baseChartOptions.data && baseChartOptions.data.length === 0);
@@ -61,6 +62,7 @@ XDMoD.utils.createChart = function (chartOptions, extraHandlers) {
 
         if (baseChartOptions.data[0].type === 'pie') {
             baseChartOptions.layout.pieChart = true;
+            baseChartOptions.layout.margin.t += 30;
         }
 
         if (!baseChartOptions.credits && baseChartOptions.credits === false) {
@@ -133,7 +135,7 @@ XDMoD.utils.createChart = function (chartOptions, extraHandlers) {
             return;
         }
 
-        const update = relayoutChart(chartDiv, baseChartOptions.layout.height, true);
+        const update = relayoutChart(chartDiv, baseChartOptions.layout.height, true, baseChartOptions.isExport);
         Plotly.relayout(baseChartOptions.renderTo, update);
     });
 
